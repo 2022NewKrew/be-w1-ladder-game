b08a337cee8850e253c5ca6f31c29929b4a3d788
pride.chicken <pride.chicken@kakaocorp.com>
2021-12-27 16:27:50 +0900

    [feature] 사다리 만들기 & 랜덤값 받기
    - 사다리를 만들어서 return하는 함수 만듦
    - 사다리에 다리를 놓을지 말지 결저하는 함수 만듦
    - 랜덤 함수는 0, 1을 랜덤하게 뽑아 0이 나오면 다리를 놓고, 1이 나오면 다리를 놓지 않음

--- src/ladder.java
+++ src/ladder.java
@@ -1,10 +1,39 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class ladder {
     public static void main(String[] args) throws IOException{
         int[] twoParameters = getConsoleTwoParameter();
+        ArrayList<ArrayList<Character>> ladder = getLadder(twoParameters);
+    }
+
+    private static ArrayList<ArrayList<Character>> getLadder(int[] twoParameters) {
+        ArrayList<ArrayList<Character>> ladder = new ArrayList<>();
+        int column = twoParameters[1];
+        int row = 2*twoParameters[0]-1;
+
+        for(int i=0; i<column; i++ ){
+            ArrayList<Character> line = new ArrayList<>();
+            for(int j=0; j<row; j++){
+                line.add(getLadderElement(j));
+            }
+            ladder.add(line);
+        }
+        return ladder;
+    }
+
+    private static Character getLadderElement(int j) {
+        if(j%2==1){
+            return 'ㅣ';
+        }
+        boolean isRandomZero = new Random().nextInt(2) == 0;
+        if(isRandomZero){
+            return '-';
+        }
+        return ' ';
     }
 
     private static int[] getConsoleTwoParameter() throws IOException {
