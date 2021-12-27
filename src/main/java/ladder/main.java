4b05bd529b6f203ac40635dc1ecef7c043a39eca
trevi.kim <trevi.kim@kakaocorp.com>
2021-12-27 15:58:28 +0900

    - col, row 입력 받아 ladder 생성하는 로직 추가 - 한 줄당 하나씩 짝수인 경우 '-' 추가하는 로직 추가 - ladder print 하는 로직 추가

---
+++ src/main/java/ladder/main.java
@@ -0,0 +1,55 @@
+package ladder;
+
+import java.util.Scanner;
+import java.lang.Math;
+
+public class main {
+    public static void main(String[] args) {
+
+        char[][] arr;
+
+        Scanner sc = new Scanner(System.in);
+        int N = getScanner(sc, "참여할 사람은 몇명인가요?");
+        int col = N * 2 - 1;
+        int row = getScanner(sc,"최대 사다리 높이는 몇 개인가요?");
+
+        arr = new char[row][col];
+
+        initLadder(arr, col, row);
+        printLadder(arr, col, row);
+
+    }
+
+    private static void initLadder(char[][] arr, int col, int row) {
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                if (j % 2 == 0)
+                    arr[i][j] = '|';
+                else
+                    arr[i][j] = ' ';
+            }
+            arr[i][(int) (Math.random() * (col / 2)) * 2 + 1] = '-';
+        }
+    }
+
+    private static int getScanner(Scanner sc, String str) {
+        System.out.println(str);
+        int N = sc.nextInt();
+        return N;
+    }
+
+
+
+    private static void printLadder(char[][] arr, int col, int row) {
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                System.out.print(arr[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
