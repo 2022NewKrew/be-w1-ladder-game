ea4b8f98ffed42ef40b4d1c851765048bc961f3a
eroica.b <eroica.b@kakaocorp.com>
2021-12-27 16:10:58 +0900

    Day1_Make Ladder

--- Week1_Ladder/src/main/java/LadderMaker/Day1.java
+++ Week1_Ladder/src/main/java/LadderMaker/Day1.java
@@ -14,25 +14,40 @@ public class Day1 {
         person = getInput("참여할 사람은 몇 명인가요?");
         ladder = getInput("최대 사다리 높이는 몇 개인가요?");
 
-        makeLadderWithNoArray(person, ladder);
+        makeLadderWithNoArray(person, ladder); // ladder with no array
         System.out.println("-------");
-        makeLadderWith2DArrayList(person, ladder);
+        makeLadderWith2DArrayList(person, ladder); // ladder with 2D ArrayList
         System.out.println("-------");
-        makeLadderWithSingleArrayList(person, ladder);
+        makeLadderWithSingleArrayList(person, ladder); // ladder with Single ArrayList and StringBuilder
         System.out.println("-------");
-        makeLadderWith2DArray(person, ladder);
+        makeLadderWith2DArray(person, ladder); // ladder with 2D Array
     }
 
+    /**
+     * introduce and get Input
+     * @param description
+     * @return
+     */
     private int getInput(String description){
         System.out.println(description);
         return new Scanner(System.in).nextInt();
     }
 
+    /**
+     * return random Character
+      * @param j
+     * @return
+     */
     private char randomCharacter(int j){
         if((j & 1) == 0) return '|';
         else return ladderList[random.nextInt(ladderList.length)];
     }
 
+    /**
+     * Make and Draw ladder with no array as using StringBuilder
+     * @param person
+     * @param ladder
+     */
     private void makeLadderWithNoArray(int person, int ladder){
         StringBuilder sb = new StringBuilder(person << 1);
 
@@ -43,6 +58,11 @@ public class Day1 {
         }
     }
 
+    /**
+     * Draw ladder using 2D ArrayList
+     * @param person
+     * @param ladder
+     */
     private void makeLadderWith2DArrayList(int person, int ladder){
         ArrayList<ArrayList<Character>> arrayList = make2DArrayList(person << 1, ladder);
         arrayList.stream()
@@ -50,6 +70,12 @@ public class Day1 {
                                                         .forEach(System.out::print));
     }
 
+    /**
+     * Make ladder using 2D ArrayList
+     * @param personTwice
+     * @param ladder
+     * @return
+     */
     private ArrayList<ArrayList<Character>> make2DArrayList(int personTwice, int ladder){
         ArrayList<ArrayList<Character>> arrayList = new ArrayList<ArrayList<Character>>(ladder);
         for(int i = 0; i < ladder; i++){
@@ -62,12 +88,23 @@ public class Day1 {
         return arrayList;
     }
 
+    /**
+     * Draw ladder using Single ArrayList and StringBuilder
+     * @param person
+     * @param ladder
+     */
     private void makeLadderWithSingleArrayList(int person, int ladder){
         ArrayList<StringBuilder> arrayList = makeSingleArrayList(person << 1, ladder);
         arrayList.stream()
                 .forEach(System.out::println);
     }
 
+    /**
+     * Make ladder using Single ArrayList and StringBuilder
+     * @param personTwice
+     * @param ladder
+     * @return
+     */
     private ArrayList<StringBuilder> makeSingleArrayList(int personTwice, int ladder){
         ArrayList<StringBuilder> arrayList = new ArrayList<StringBuilder>(ladder);
         for(int i = 0; i < ladder; i++){
@@ -78,6 +115,11 @@ public class Day1 {
         return arrayList;
     }
 
+    /**
+     * Draw ladder using 2D Array
+     * @param person
+     * @param ladder
+     */
     private void makeLadderWith2DArray(int person, int ladder){
         char[][] array = make2DArray(person << 1, ladder);
         for(int i = 0; i < ladder; i++)
@@ -85,6 +127,12 @@ public class Day1 {
                 System.out.print(array[i][j]);
     }
 
+    /**
+     * Make ladder using 2D Array
+     * @param personTwice
+     * @param ladder
+     * @return
+     */
     private char[][] make2DArray(int personTwice, int ladder){
         char[][] array = new char[ladder][personTwice];
         for(int i = 0; i < ladder; i++){
