8a4d523c294526113f40824ea2ee8720c539ceb0
pride.chicken <pride.chicken@kakaocorp.com>
2021-12-27 16:07:32 +0900

    [feature] 인풋값 받기
    - 두개의 인자를 받아 저장하는 함수 만들기
    - getConsoleTwoParameter 함수 만듦
    - 두 개의 인자를 받아 크기 2의 배열을 return 해줌

--- src/ladder.java
+++ src/ladder.java
@@ -1,5 +1,21 @@
-public class ladder {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class ladder {
+    public static void main(String[] args) throws IOException{
+        int[] twoParameters = getConsoleTwoParameter();
+    }
+
+    private static int[] getConsoleTwoParameter() throws IOException {
+        int [] twoParameters = new int[2];
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("참여할 사람은 몇 명인가요 ?");
+        twoParameters[0] = Integer.parseInt(bufferedReader.readLine());
+
+        System.out.println("최대 사다리 높이는 몇 개인가요 ?");
+        twoParameters[1] = Integer.parseInt(bufferedReader.readLine());
+        return twoParameters;
     }
 }
