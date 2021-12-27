82ba987c390a434ac949cbcea9405668782022cd
jindo.min <jindo.min@kakaocorp.com>
2021-12-27 15:41:05 +0900

    [refactor] javadoc 형식에 맞춰 주석 변경

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,11 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    // 사다리 출력 메서드
+    /**
+     * 사다리를 화면에 출력하는 메서드
+     *
+     * @param ladder 사다리 배열을 입력 받는다
+     */
     static void printLadder(char[][] ladder) {
         for (int i = 0; i < ladder.length; i++) {
             for (int j = 0; j < ladder[0].length; j++)
@@ -13,7 +17,13 @@ public class Main {
         }
     }
 
-    // 사다리 생성 메서드
+    /**
+     * 사람 수와 사다리 높이를 인자로 받아 사다리를 생성하는 메서드
+     *
+     * @param number 사람 수
+     * @param height 사다리 높이
+     * @return 사다리 배열
+     */
     static char[][] makeLadder(int number, int height) {
         char[][] ladder = new char[height][2 * number -1];
 
