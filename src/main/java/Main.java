1419813e4a9518ac792d4e431d75b73774522ddb
jindo.min <jindo.min@kakaocorp.com>
2021-12-27 23:19:18 +0900

    [refactor] 입력, 입력값 검증 부분 분리

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,13 +4,14 @@ public class Main {
     static int numberOfPlayer = 0;
     static int height = 0;
     static char[][] ladders;
+
     /**
      * 사다리를 화면에 출력하는 메서드
      */
     static void printLadder() {
-        for (int i = 0; i < ladders.length; i++) {
+        for (char[] ladder : ladders) {
             for (int j = 0; j < ladders[0].length; j++)
-                System.out.printf("%c", ladders[i][j]);
+                System.out.printf("%c", ladder[j]);
             System.out.println();
         }
     }
@@ -35,25 +36,30 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) {
+    static void validateInput(int input) {
+        if (input <= 0)
+            throw new InputMismatchException();
+    }
+
+    static void input() throws InputMismatchException {
         Scanner in = new Scanner(System.in);
 
+        System.out.println("참여할 사람은 몇 명인가요?");
+        numberOfPlayer = in.nextInt();
+        validateInput(numberOfPlayer);
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        height = in.nextInt();
+        validateInput(height);
+    }
+
+    public static void main(String[] args) {
         try {
-            System.out.println("참여할 사람은 몇 명인가요?");
-            numberOfPlayer = in.nextInt();
-            System.out.println("최대 사다리 높이는 몇 개인가요?");
-            height = in.nextInt();
+            input();
         } catch (InputMismatchException e) {
-            System.out.println("정수를 입력해주세요");
+            System.out.println("0보다 큰 정수를 입력해주세요");
             e.printStackTrace();
             return;
         }
-
-        if (numberOfPlayer <= 0 || height <= 0) {
-            System.out.println("0보다 큰 정수를 입력해주세요");
-            return;
-        }
-
         makeLadder();
         printLadder();
     }
