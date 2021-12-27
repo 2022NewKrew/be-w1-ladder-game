623d447b3206e7c267fa1e6d7d6bd0e45b23ac65
binary.yun <binary.yun@kakaocorp.com>
2021-12-27 18:15:50 +0900

    feat: 새로운 사다리를 생성해서 출력

--- src/main/java/LadderGame.java
+++ src/main/java/LadderGame.java
@@ -6,9 +6,12 @@ public class LadderGame {
         Scanner in = new Scanner(System.in);
 
         System.out.println("참여할 사람은 몇 명인가요?");
-        String people = in.nextLine();
+        int people = Integer.valueOf(in.nextLine());
 
         System.out.println("최대 사다리 높이는 몇 개인가요?");
-        String heightLadder = in.nextLine();
+        int heightLadder = Integer.valueOf(in.nextLine());
+
+        Ladder ladder = new Ladder(people, heightLadder);
+        System.out.println(ladder);
     }
 }
