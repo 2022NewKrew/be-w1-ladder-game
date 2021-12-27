557d0c1dfbba92fb6b9470114a9dab133f44d347
champ.ion <champ.ion@kakaocorp.com>
2021-12-27 17:27:18 +0900

    ladder game step1

---
+++ src/Ladder.java
@@ -0,0 +1,37 @@
+import java.lang.Math;
+import java.util.Arrays;
+
+public class Ladder {
+    private int width;
+    private int height;
+    private boolean[][] line;
+
+    public Ladder(int numPeople, int ladderHeight) {
+        this.width = numPeople - 1;
+        this.height = ladderHeight;
+        makeLines();
+    }
+
+    private void makeLines() {
+        line = new boolean[height][width];
+
+        for (int h = 0; h < height; h++) {
+            for (int w = 0; w < width; w++) {
+                line[h][w] = Math.random() > 0.5;
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        String ladder = "";
+        for (int h = 0; h < height; h++) {
+            ladder += "|";
+            for (int w = 0; w < width; w++) {
+                ladder += (line[h][w] == true ? "-" : " ") + "|";
+            }
+            ladder += "\n";
+        }
+        return ladder;
+    }
+}
