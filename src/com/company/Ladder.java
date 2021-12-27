ab1d4469644762a9fc96d812bc0387c61e4e3170
worldbright <worldbright0@gmail.com>
2021-12-27 14:25:14 +0900

    fix Ladder Class's printLadder method

--- src/com/company/Ladder.java
+++ src/com/company/Ladder.java
@@ -10,6 +10,7 @@ public class Ladder {
     public Ladder(int numPeople, int height) {
         this.numPeople = numPeople;
         this.height = height;
+        makeNewLadder();
     }
 
     private void makeNewLadder() {
@@ -28,11 +29,11 @@ public class Ladder {
     public void printLadder() {
         for(int y = 0; y < height; y++) {
             for(int x = 0; x < numPeople; x++) {
-                System.out.println("|");
+                System.out.print("|");
                 if(ladder[y][x])
-                    System.out.println("-");
+                    System.out.print("-");
                 else
-                    System.out.println(" ");
+                    System.out.print(" ");
             }
             System.out.println("|");
         }
