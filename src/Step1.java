02fd9889960ab56f3866a5efec794e4ac72a8bf6
rubam.kong <rubam.kong@kakaocorp.com>
2021-12-27 14:32:59 +0900

    ladder-game impl: step1

---
+++ src/Step1.java
@@ -0,0 +1,10 @@
+public class Step1 {
+
+    public static void main(String[] args) {
+
+        Ladder ladder = new Ladder(3,5);
+        ladder.makeLadder();
+        ladder.printLadder();
+    }
+
+}
