040dbecb89005db1a23e007118dae5596a72dfe9
imeansu <gcnml10@naver.com>
2021-12-27 17:43:51 +0900

    [step1] divide createLadder and printLadder

--- src/main/java/LadderGame.java
+++ src/main/java/LadderGame.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -15,10 +17,11 @@ public class LadderGame {
         int height = heightAndPeople[1];
 
         // 사다리 만들기
-        StringBuilder ladder = createLadder(people, height);
+        String[][] ladder = createLadder(people, height);
 
         // 출력
-        System.out.println(ladder);
+        StringBuilder ladderStringBuilder = ladderToString(ladder);
+        System.out.println(ladderStringBuilder);
     }
 
     private static int[] inputHeightAndPeople(){
@@ -34,16 +37,26 @@ public class LadderGame {
     }
 
     // 랜덤 Boolean 반환
-   private static boolean randomBoolean(Random rd){
+    private static boolean randomBoolean(Random rd){
         return rd.nextBoolean();
     }
 
-    private static StringBuilder createLadder(int people, int height){
-        StringBuilder sb = new StringBuilder();
+    private static String[][] createLadder(int people, int height){
+        String[][] ladder = new String[height][(people-1)*2 +1];
         for (int i = 0; i < height; i++){
             for (int j = 0 ; j < (people-1)*2 + 1; j++) {
-                if (j % 2 == 0) sb.append(STRINGS[0]);
-                else sb.append(randomBoolean(rd) ? STRINGS[1] : STRINGS[2]);
+                if (j % 2 == 0) ladder[i][j] = STRINGS[0];
+                else ladder[i][j] = randomBoolean(rd) ? STRINGS[1] : STRINGS[2];
+            }
+        }
+        return ladder;
+    }
+
+    private static StringBuilder ladderToString(String[][] array){
+        StringBuilder sb = new StringBuilder();
+        for(String[] innerArray : array){
+            for(String st : innerArray){
+                sb.append(st);
             }
             sb.append('\n');
         }
