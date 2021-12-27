a8beeecd24939e42fde6afce3284d183cb1c25fb
wes.kim <wes.kim@kakaocorp.com>
2021-12-27 16:28:58 +0900

    walk 메서드 바디 줄이기

--- src/main/java/com/kakaocorp/ladder/iteration/HorizontalFirstWalker.java
+++ src/main/java/com/kakaocorp/ladder/iteration/HorizontalFirstWalker.java
@@ -4,23 +4,19 @@ import com.kakaocorp.ladder.model.Ladder;
 import com.kakaocorp.ladder.model.Node;
 import com.kakaocorp.ladder.model.Stick;
 
-import java.util.List;
-
 public class HorizontalFirstWalker implements MultipleNodesWalker {
 
     @Override
     public void walk(Ladder ladder, Callback callback) {
         for (int i = 0; i < ladder.getHeight(); i++) {
-            List<Stick> sticks = ladder.getSticks();
             Node left = null;
-            Node right = null;
-            for (Stick stick : sticks) {
+            Node right;
+            for (Stick stick : ladder.getSticks()) {
                 right = stick.getNodes().get(i);
                 callback.process(left, right);
                 left = right;
-                right = null;
             }
-            callback.process(left, right);
+            callback.process(left, null);
         }
     }
 }
