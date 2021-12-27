53a4baad80adce19e77fc08f7a5c3f0ceb48dcf1
jb.jang <jb.jang@kakaocorp.com>
2021-12-27 18:51:41 +0900

    feat: 사다리 게임 1단계 구현

---
+++ src/main/java/Ladder.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Ladder {
+
+    int number;
+    int height;
+    ArrayList<String> ladderResult = new ArrayList<>();
+
+    public Ladder(int number, int height) {
+        this.number = number;
+        this.height = height;
+    }
+
+    public void setLadder() {
+        Random random = new Random();
+        for(int i=0; i<height; i++) {
+            StringBuilder sb = new StringBuilder();
+            sb.append("|");
+            for(int j=0; j<number-1; j++) {
+                if (random.nextInt(2) == 1) {
+                    sb.append("-");
+                } else {
+                    sb.append(" ");
+                }
+                sb.append("|");
+            }
+            ladderResult.add(sb.toString());
+        }
+    }
+
+    public void printLadder() {
+        for(int i=0; i<height; i++) {
+            System.out.println(ladderResult.get(i));
+        }
+    }
+}
