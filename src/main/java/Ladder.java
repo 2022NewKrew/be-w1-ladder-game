302310c77e22f7a09b122def17d67f4a9c2140b7
punsoo <sonu.sohn@kakaocorp.com@gmail.com>
2021-12-27 17:30:55 +0900

    Ladder 구현

--- src/main/java/Ladder.java
+++ src/main/java/Ladder.java
@@ -1,7 +1,35 @@
 package main.java;
 
-public class Ladder {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.Random;
 
+public class Ladder {
+    private final static String DOWN = "|";
+    private final static String RIGHT = "|-";
+
+    public static void main(String[] args) {
+        ResultView.printResult(generateLadder(InputView.enterPlayers(), InputView.enterHeight()));
+    }
+
+    private static ArrayList<ArrayList<String>> generateLadder(int playerNum, int height) {
+        ArrayList<ArrayList<String>> ladder = new ArrayList<>();
+        for(int i = 0; i < height; i++) {
+            ladder.add(generateLine(playerNum));
+        }
+        return ladder;
+    }
+
+    private static ArrayList<String> generateLine(int playerNum) {
+        ArrayList<String> line = new ArrayList<>();
+        for(int i = 1; i < playerNum; i++) {
+            line.add(getPoint());
+        }
+        line.add(DOWN);
+        return line;
+    }
+
+    private static String getPoint() {
+        Random rand = new Random();
+        return rand.nextBoolean() ? RIGHT : DOWN;
     }
 }
