3b42db071ddb303945618660ce14ba4db6c3a572
frank.burger <frank.burger@kakaocorp.com>
2021-12-27 15:34:27 +0900

    Add Ladder and LadderGame class
    LadderGame class is the driver class.
    
    Each class has its breif to-do's written as comment.

---
+++ src/Ladder.java
@@ -0,0 +1,6 @@
+public class Ladder {
+    // 1. Constructor to initialize ladder parameters(width(number of players), height)
+    // 2. Method to (re)generate ladder
+    // 3. toString() method
+    // 4. Fields to save ladder state
+}
---
+++ src/LadderGame.java
@@ -0,0 +1,7 @@
+public class LadderGame {
+    public static void main(String[] args) {
+        // 1. Prompt and read input(number of players, ladder height)
+        // 2. Generate ladder
+        // 3. Show result
+    }
+}
