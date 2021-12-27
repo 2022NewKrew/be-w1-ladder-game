df4939df4e3e5c2d9cd78e38faa9c9f2ae6eb2df
hungryjayy <juwonson2068@naver.com>
2021-12-27 16:39:28 +0900

    refactor: simplify each methods

--- src/com/laddergame/LadderGameApplication.java
+++ src/com/laddergame/LadderGameApplication.java
@@ -1,34 +1,15 @@
 package com.laddergame;
 
 import java.util.ArrayList;
-import java.util.Random;
-import java.util.Scanner;
+
+import static com.laddergame.View.*;
 
 public class LadderGameApplication {
 
     public static void main(String[] args) {
-        System.out.println("참여할 사람은 몇 명인가요?");
-        Scanner scanner = new Scanner(System.in);
-        int participantNumber = scanner.nextInt();
-        System.out.println(participantNumber);
-
-        System.out.println("최대 사다리 높이는 몇인가요?");
-        int ladderHeight = scanner.nextInt();
-        System.out.println(ladderHeight);
-        ArrayList<String> result = new ArrayList<>(5);
-
-        for(int i = 0; i < ladderHeight; i++) {
-            StringBuilder sb = new StringBuilder();
-            for(int j = 0; j < participantNumber - 1; j++) {
-                sb.append("|");
-                int randomNumber = new Random().nextInt(10);
-                String singleResult = randomNumber < 10 / 2 ? "-" : " ";
-                sb.append(singleResult);
-            }
-            sb.append("|");
-            result.add(sb.toString());
-        }
-
-        result.forEach(System.out::println);
+        int participantNumber = inputParticipantNumber();
+        int ladderHeight = inputLadderHeight();
+        ArrayList<ArrayList<Boolean>> result = new LadderGame(participantNumber, ladderHeight).startGame();
+        outputLadderGameResult(result);
     }
 }
