ff58d63e74497cba52ba4a6605414f0c35a4ca5d
geatrigger-kim <geatrigger.kim@kakaocorp.com>
2021-12-27 14:53:09 +0900

    LadderGame level1 prototype

---
+++ src/main/java/com/gradle/geatrigger/LadderGame.java
@@ -0,0 +1,34 @@
+package com.gradle.geatrigger;
+
+public class LadderGame {
+    public static void main(String[] args) {
+        int cnt, height;
+        System.out.println("참여할 사람은 몇 명인가요?");
+        cnt = 3;
+        System.out.println("참여할 사람은 몇 명인가요?");
+        height = 5;
+        DrawLadder(cnt, height);
+    }
+
+    private static void DrawLadder(int cnt, int height) {
+        for(int i = 0; i < height; i++) {
+            for(int j = 0; j < cnt - 1; j++) {
+                DrawVertical();
+                DrawHorizon();
+            }
+            DrawVertical();
+            System.out.println();
+        }
+    }
+
+    private static void DrawHorizon() {
+        if (Math.random() > 0.5)
+            System.out.print("-");
+        else
+            System.out.print(" ");
+    }
+
+    private static void DrawVertical() {
+        System.out.print("|");
+    }
+}
