2e43f571a9c99dfa62a651a0bea25faeab3d14fd
kakao <kakao@pires76-pc.local>
2021-12-27 15:54:43 +0900

    Ladder 객체화
    ladder 를 객체로 만들어 활용하기 위해 변경

---
+++ src/main/java/Ladder.java
@@ -0,0 +1,50 @@
+import java.util.Random;
+
+public class Ladder {
+    private int height;
+    private int width;
+    private char[][] ladder;
+
+    public Ladder(int height, int numOfPeople) {
+        this.height = height;
+        this.width = numOfPeople * 2 - 1;
+        this.ladder = new char[this.height][this.width];
+    }
+
+    public void initialize() {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (j % 2 == 0) {
+                    ladder[i][j] = '|';
+                } else {
+                    ladder[i][j] = ' ';
+                }
+            }
+        }
+    }
+
+    public void makeRandom() {
+        Random random = new Random();
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 1; j < width; j += 2) {
+                if (j - 2 >= 0 && ladder[i][j - 2] == '-') {
+                    continue;
+                }
+
+                if (random.nextBoolean()) {
+                    ladder[i][j] = '-';
+                }
+            }
+        }
+    }
+
+    public void print() {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                System.out.print(ladder[i][j]);
+            }
+            System.out.print('\n');
+        }
+    }
+}
