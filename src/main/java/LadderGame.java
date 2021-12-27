aaaecadf7e1d135e4258291473ce4928369f0b9f
binary.yun <binary.yun@kakaocorp.com>
2021-12-27 16:56:13 +0900

    feat: 기본 사다리게임 입출력 구현

---
+++ src/main/java/LadderGame.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class LadderGame {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("참여할 사람은 몇 명인가요?");
+        String people = in.nextLine();
+
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        String heightLadder = in.nextLine();
+    }
+}
