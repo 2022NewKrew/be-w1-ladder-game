2a0cdae92b8ece4bbb7f0ae5473f6487818707fa
YoonRyeol <yunyul3@gmail.com>
2021-12-27 14:00:49 +0900

    2021-12-27 | YR | 1 단계 파일 구조 변경

--- src/main/java/LadderGame.java
+++ src/main/java/LadderGame.java
@@ -1,34 +1,52 @@
-import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
 public class LadderGame {
+    char[][] output;
+    int N;
+    int H;
 
-    UserInterface userInterface;
-    LadderService ladderService;
-    PlayerService playerService;
-
-    private LadderGame() {
-        this.userInterface = new UserInterface();
-        this.ladderService = new LadderService();
-        this.playerService = new PlayerService();
+    private void getInput(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("참여할 사람은 몇 명 인가요");
+        N = scanner.nextInt();
+        System.out.println("최대 사다리 높이는 몇 개 인가요?");
+        H = scanner.nextInt();
     }
 
-    public static void main(String[] args) {
-        LadderGame ladderGame = new LadderGame();
-        ladderGame.initData();
-        ladderGame.printData();
+    private void initOutput(){
+        output = new char[H][N-1];
     }
 
-    private void initData() {
-        Input<ArrayList<String>> playersInput = userInterface.readPlayers();
-        Input<Integer> heightInput = userInterface.readHeight();
-        ladderService.makeLadder(heightInput.getValue(), playersInput.getValue().size());
-        playerService.makePlayerList(playersInput);
+    private void makeLadder(){
+        Random rand = new Random();
+        for(int i=0; i<output.length; i++){
+            for(int j=0; j<output[i].length; j++){
+                if(rand.nextBoolean()){
+                    output[i][j] = ' ';
+                }else{
+                    output[i][j] = '-';
+                }
+            }
+        }
     }
 
-    private void printData() {
-        userInterface.printPlayers(playerService.getPlayerList());
-        userInterface.printLadder(ladderService.getLadder());
+    private void printOutput(){
+        for(int i=0; i<output.length; i++){
+            for(int j=0; j<output[i].length; j++){
+                System.out.print('|');
+                System.out.print(output[i][j]);
+            }
+            System.out.println('|');
+        }
     }
 
+    public static void main(String[] args){
+        LadderGame lg = new LadderGame();
+        lg.getInput();
+        lg.initOutput();
+        lg.makeLadder();
+        lg.printOutput();
+    }
 
 }
