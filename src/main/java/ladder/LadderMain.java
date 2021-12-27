a2337ae9676fd1cc7b1f0da6713a071b42e14011
flip1945 <coghdus1016@naver.com>
2021-12-27 18:43:07 +0900

    feat: 사다리게임 1단계 구현

--- src/main/java/ladder/LadderMain.java
+++ src/main/java/ladder/LadderMain.java
@@ -1,7 +1,22 @@
 package ladder;
 
+import ladder.provider.LadderProvider;
+
+import java.util.Scanner;
+
 public class LadderMain {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner scanner = new Scanner(System.in);
+        int numberOfPlayers;
+        int maxLadderHeight;
+
+        System.out.println("참여할 사람은 몇 명인가요?");
+        numberOfPlayers = scanner.nextInt();
+
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        maxLadderHeight = scanner.nextInt();
+
+        LadderProvider ladderProvider = new LadderProvider(maxLadderHeight, numberOfPlayers);
+        System.out.println(ladderProvider.createLadder());
     }
 }
