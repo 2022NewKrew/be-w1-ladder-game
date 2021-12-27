c2442c276b4a8c2c0822315f273ec586ecd59081
jake.0208 <jake.0208@kakaocorp.com>
2021-12-27 20:59:29 +0900

    update ladder-game

--- src/main/java/Ladder.java
+++ src/main/java/Ladder.java
@@ -1,24 +1,45 @@
+import java.util.Random;
+
 public class Ladder {
-    int participants;
-    int height;
+    private final int participants;
+    private final int height;
+    private final boolean[][] ladder;
 
     public Ladder(int participants, int height){
         this.participants = participants;
         this.height  = height;
+        ladder = new boolean[height][participants-1];
+
+        makeLadder();
     }
 
-    public void print(){
+    private void makeLadder() {
+        Random random = new Random();
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < participants - 1; j++) {
+                ladder[i][j] = random.nextBoolean();
+            }
+        }
+    }
+
+    public void printLadder(){
+        String path = "|";
+
         for(int i=0; i<height; i++){
             for(int j = 0; j<participants-1; j++){
-                System.out.print("|");
-                if (Math.random() <= 0.5) {
-                    System.out.print("-");
-                } else {
-                    System.out.print(" ");
-                }
+                System.out.print(path);
+                System.out.print(isLadder(i,j));
             }
-            System.out.println("|");
+            System.out.println(path);
         }
+    }
+
+    private String isLadder(int i, int j){
+        String path = "-";
+        String space = " ";
+
+        return ladder[i][j]? path : space;
     }
 }
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,13 +3,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("게임을 시작합니다.");
+
         System.out.println("참여할 사람은 몇 명인가요?");
         int participants = sc.nextInt();
+
         System.out.println("최대 사다리 높이는 몇 개인가요?");
         int height = sc.nextInt();
 
         Ladder ladder = new Ladder(participants, height);
-        ladder.print();
+        ladder.printLadder();
     }
 }
 
