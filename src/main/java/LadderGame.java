753efd6edbc4013cee5790fe7dc69cf89bc56427
chance.kk <chance.kk@kakaocorp.com>
2021-12-28 02:06:08 +0900

    Create Ladder, Modify LadderGame

--- src/main/java/LadderGame.java
+++ src/main/java/LadderGame.java
@@ -1,16 +1,13 @@
 import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.Random;
 
 public class LadderGame {
     static int numberOfGamers;
     static int heightOfLadder;
-    static ArrayList<ArrayList<String>> ladder = new ArrayList<ArrayList<String>>();
 
     public static void main(String[] args) {
         input();
-        setLadder();
-        printLadder();
+        Ladder ladder = new Ladder(numberOfGamers, heightOfLadder);
+        ladder.printLadder();
     }
 
     public static void input() {
@@ -20,31 +17,6 @@ public class LadderGame {
         System.out.println("최대 사다리 높이는 몇 개인가요?");
         heightOfLadder = scanner.nextInt();
     }
-
-    public static void setLadder() {
-        for(int i = 0 ; i < heightOfLadder; i++) {
-            ArrayList<String> row = setLadderRow();
-            ladder.add(row);
-        }
-    }
-
-    public static ArrayList<String> setLadderRow() {
-        Random rd = new Random();
-        ArrayList<String> row = new ArrayList<String>();
-        row.add("|");
-        for(int j = 0 ; j < numberOfGamers-1 ; j++) {
-            row.add(rd.nextBoolean() ? "-" : " ");
-            row.add("|");
-        }
-        return row;
-    }
-
-    public static void printLadder() {
-        for(int i = 0 ; i < heightOfLadder; i++) {
-            for(int j = 0 ; j < ladder.get(i).size(); j++) {
-                System.out.print(ladder.get(i).get(j));
-            }
-            System.out.println();
-        }
-    }
 }
+
+
