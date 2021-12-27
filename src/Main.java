41880adf5a007b97cd8b269894da670ac0be0e49
charlie.p1 <charlie.p1@kakaocorp.com>
2021-12-27 17:29:02 +0900

    Update Main for step 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
-public class Main {
-    public static void main(String[] args) {
+import java.security.SecureRandom;
+import java.util.Scanner;
 
+public class Main {
+    private static final byte[] SEED = {26, -69, 76, -110, -94, 80, 126, -68, 100, 1, 68, -32, 5, 95, -11, -58, 123, -86, 126, 47};
+
+    public static void main(String[] args) {
+        LadderGame ladderGame = initLadderGameByInput();
+        System.out.println(ladderGame.buildLadder());
+    }
+
+    private static LadderGame initLadderGameByInput() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("참여할 사람은 몇 명인가요?");
+        int numberOfPlayers = scanner.nextInt();
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        int heightOfLadder = scanner.nextInt();
+        System.out.println();
+
+        return new LadderGame(numberOfPlayers, heightOfLadder, new SecureRandom(SEED));
     }
 }
