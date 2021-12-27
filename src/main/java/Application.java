0c64dde16bb8748dc334800fb59f90ac9968b5a2
sukstar76 <47517483+sukstar76@users.noreply.github.com>
2021-12-27 22:06:26 +0900

    랜덤 사다리 구현 (#33)
    * git ignore 추가
    
    * ladder 기능 구현
    
    input 메소드
    배열 생성 및 랜덤화 메소드
    ladder print 메소드
    
    * Ladder 객체화
    
    ladder 를 객체로 만들어 활용하기 위해 변경
    
    Co-authored-by: pires.76 <pires.76@kakaocorp.com>
    Co-authored-by: kakao <kakao@pires76-pc.local>

---
+++ src/main/java/Application.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Application {
+    public static int numOfPeople;
+    public static int heightOfLadder;
+
+    public static void runApp() {
+        input();
+
+        Ladder ladder = new Ladder(heightOfLadder, numOfPeople);
+        ladder.initialize();
+        ladder.makeRandom();
+        ladder.print();
+    }
+
+    public static void input() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("참여할 사람은 몇 명인가요?");
+        numOfPeople = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        heightOfLadder = Integer.parseInt(scanner.nextLine());
+    }
+
+    public static void main(String[] args) {
+        runApp();
+    }
+}
