c8067ff71cccf900a75e53568ae0eaf28a44c1a8
pug.gg <kakaocommerce@pug-gg.local>
2021-12-27 15:35:03 +0900

    step-1: 사다리 게임 1단계 구현

---
+++ src/main/java/ladder/Main.java
@@ -0,0 +1,20 @@
+package ladder;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("참여할 사람은 몇 명인가요?");
+        int participants = sc.nextInt();
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        int height = sc.nextInt();
+
+        Ladder ladder = new Ladder(participants, height);
+        ladder.print();
+    }
+
+}
