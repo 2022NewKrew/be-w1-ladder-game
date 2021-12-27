35490f08277f3c2e51b119924f7e2f28d7d4e05a
tony.ahn <tony.ahn@kakaocorp.com>
2021-12-27 18:11:17 +0900

    사다리 게임 2단계 구현

--- src/Ladder.java
+++ src/Ladder.java
@@ -1,44 +1,56 @@
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Ladder {
-    public static ArrayList<String> makeLadder(int people, int height) {
-        ArrayList<String> sadari = new ArrayList<String>();
-        StringBuilder sb = new StringBuilder();
-        double randNum;
-        for(int i=0;i<height;i++) {
-            sb.setLength(0);
-            for(int j=1;j<people;j++) {
-                sb.append('|');
-                // 0 ~ 1 사이의 난수를 가지고 0.5보다 크다면 공백, 0.5보다 작다면 -를 넣는다
-                randNum = Math.random();
-                if(randNum >= 0.5)
-                    sb.append(' ');
-                else
-                    sb.append('-');
-            }
-            sb.append('|');
-            sadari.add(sb.toString());
-        }
-        return sadari;
+
+    private ArrayList<String> ladder;
+    private int people;
+    private int height;
+    private StringBuilder sb;
+
+    public Ladder() {
+        ladder = new ArrayList<String>();
+        people = 2;
+        height = 5;
+        sb = new StringBuilder();
+        makeLadder();
     }
 
-    public static void printLadder(ArrayList<String> sadari) {
-        for(String s : sadari) {
+    public Ladder(int p, int h) {
+        ladder = new ArrayList<String>();
+        people = p;
+        height = h;
+        sb = new StringBuilder();
+        makeLadder();
+    }
+
+    public char getRandomBridge() {
+        double randNum;
+        randNum = Math.random();
+        if(randNum >= 0.5)
+            return ' ';
+        return '-';
+    }
+
+    private String makeLayer() {
+        sb.setLength(0);
+        for(int i=1;i<people;i++) {
+            sb.append('|');
+            sb.append(getRandomBridge());
+        }
+        sb.append('|');
+        return sb.toString();
+    }
+
+    public void makeLadder() {
+        for(int i=0;i<height;i++) {
+            ladder.add(makeLayer());
+        }
+    }
+
+    public void printLadder() {
+        for(String s : ladder) {
             System.out.println(s);
         }
     }
 
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int people, height;
-
-        System.out.println("참여할 사람은 몇 명인가요?");
-        people = s.nextInt();
-        System.out.println("최대 사다리 높이는 몇 개인가요?");
-        height = s.nextInt();
-
-        ArrayList<String> sadari = makeLadder(people, height);
-        printLadder(sadari);
-    }
 }
---
+++ src/LadderGame.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class LadderGame {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int people, height;
+
+        System.out.println("참여할 사람은 몇 명인가요?");
+        people = s.nextInt();
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        height = s.nextInt();
+
+        Ladder ladder = new Ladder(people, height);
+        ladder.printLadder();
+    }
+}
