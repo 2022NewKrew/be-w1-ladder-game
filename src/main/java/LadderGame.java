5d0facb395fb7f230844d0d71cfa36f057d1e815
YoonRyeol <yunyul3@gmail.com>
2021-12-27 14:00:49 +0900

    2021-12-27 | YR | 1 단계 파일 구조 변경

--- src/main/java/LadderGame.java
+++ src/main/java/LadderGame.java
@@ -1,9 +1,52 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class LadderGame {
-    public static void main(String[] args) {
-        UserInterface userInterface = new UserInterface(new Scanner(System.in));
-        userInterface.makeLadder();
-        userInterface.printLadder();
+    char[][] output;
+    int N;
+    int H;
+
+    private void getInput(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("참여할 사람은 몇 명 인가요");
+        N = scanner.nextInt();
+        System.out.println("최대 사다리 높이는 몇 개 인가요?");
+        H = scanner.nextInt();
     }
+
+    private void initOutput(){
+        output = new char[H][N-1];
+    }
+
+    private void makeLadder(){
+        Random rand = new Random();
+        for(int i=0; i<output.length; i++){
+            for(int j=0; j<output[i].length; j++){
+                if(rand.nextBoolean()){
+                    output[i][j] = ' ';
+                }else{
+                    output[i][j] = '-';
+                }
+            }
+        }
+    }
+
+    private void printOutput(){
+        for(int i=0; i<output.length; i++){
+            for(int j=0; j<output[i].length; j++){
+                System.out.print('|');
+                System.out.print(output[i][j]);
+            }
+            System.out.println('|');
+        }
+    }
+
+    public static void main(String[] args){
+        LadderGame lg = new LadderGame();
+        lg.getInput();
+        lg.initOutput();
+        lg.makeLadder();
+        lg.printOutput();
+    }
+
 }
