7918574a043e8d4ff9e446e09833a524c073c3dc
frank.burger <frank.burger@kakaocorp.com>
2021-12-27 18:36:39 +0900

    Add initial implementation of LadderGame and Ladder
    Ladder's internal state is the same as how it looks, which is not good but
    is enough to meet step-1's requirement.
    
    The generation process thus also works on raw string.
    
    The generation method is named so, because the method is naive and we will
    probably need another generation method very soon.

--- src/Ladder.java
+++ src/Ladder.java
@@ -1,6 +1,35 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
 public class Ladder {
-    // 1. Constructor to initialize ladder parameters(width(number of players), height)
-    // 2. Method to (re)generate ladder
-    // 3. toString() method
     // 4. Fields to save ladder state
+    private int width, height;
+    private ArrayList<String> stringRepr;
+
+
+    // 1. Constructor to initialize ladder parameters(width(number of players), height)
+    public Ladder(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+
+    // 2. Method to (re)generate ladder
+    public void generateSimpleRandom() {
+        Random rand = new Random();
+
+        this.stringRepr = new ArrayList<>();
+        for (int lineNo = 0; lineNo < this.height; lineNo++) {
+            this.stringRepr.add(Stream.generate(() -> rand.nextBoolean() ? "-" : " ").limit(this.width - 1).collect(Collectors.joining("|", "|", "|")));
+        }
+    }
+
+
+    // 3. toString() method
+    public String toString() {
+        return String.join("\n", this.stringRepr);
+    }
 }
