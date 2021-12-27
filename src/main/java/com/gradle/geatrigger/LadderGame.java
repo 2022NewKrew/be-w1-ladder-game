832754a2bd321799439df9ba962ae7d04f650fdc
geatrigger-kim <geatrigger.kim@kakaocorp.com>
2021-12-27 14:59:15 +0900

    Add Scanner and Fix 2nd question

--- src/main/java/com/gradle/geatrigger/LadderGame.java
+++ src/main/java/com/gradle/geatrigger/LadderGame.java
@@ -1,12 +1,15 @@
 package com.gradle.geatrigger;
 
+import java.util.Scanner;
+
 public class LadderGame {
     public static void main(String[] args) {
         int cnt, height;
+        Scanner sc = new Scanner(System.in);
         System.out.println("참여할 사람은 몇 명인가요?");
-        cnt = 3;
-        System.out.println("참여할 사람은 몇 명인가요?");
-        height = 5;
+        cnt = sc.nextInt();
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        height = sc.nextInt();
         DrawLadder(cnt, height);
     }
 
