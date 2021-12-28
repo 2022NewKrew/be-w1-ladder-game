8dd88a79dab3720b219107f1cdc7f696790e572b
kai.sa <kai.sa@kakaocorp.com>
2021-12-28 15:45:59 +0900

    step2

--- src/main/java/Ladder.java
+++ src/main/java/Ladder.java
@@ -9,34 +9,42 @@ public class Ladder {
     public Ladder(int personNum, int ladderHeight) {
         this.personNum = personNum;
         this.ladderHeight = ladderHeight;
-        this.ladder = new boolean[personNum-1][ladderHeight];
-        setLadder(personNum,ladderHeight);
+        this.ladder = new boolean[ladderHeight][personNum-1];
+        makeLadder(personNum, ladderHeight);
     }
 
-    void setLadder(int personNum, int ladderHeight){
-        Random random = new Random();
-        for(int i=0; i<personNum-1; i++){
-            for(int j=0; j<ladderHeight; j++){
-                if(random.nextBoolean()){
-                    ladder[i][j] = true;
-                }
-            }
+    void makeLadder(int personNum, int ladderHeight) {
+        for (int i = 0; i < ladderHeight; i++) {
+            makeLine(personNum,ladder[i]);
         }
     }
 
-    void printLadder(){
-        for(int i=0; i<ladderHeight; i++){
+    void makeLine(int personNum, boolean[] line){
+        Random random = new Random();
+        for(int i=0; i<personNum - 1; i++){
+            line[i] = random.nextBoolean();
+        }
+    }
+
+    void printLadder() {
+        for (int i = 0; i < ladderHeight; i++) {
             System.out.print("|");
-            for(int j=0; j<personNum-1; j++){
-                if(ladder[j][i]){
-                    System.out.print("-|");
-                }
-                else{
-                    System.out.print(" |");
-                }
-            }
+            printLine(i);
             System.out.println("");
         }
+    }
+
+    void printLine(int h){
+        for (int i = 0; i < personNum - 1; i++) {
+            System.out.print(printUnit(ladder[h][i]));
+        }
+    }
+
+    String printUnit(boolean b){
+        if(b){
+            return "-|";
+        }
+        return " |";
     }
 
 }
