43b28cfe6a714977ecd3348e7bf599b08d36731e
pride.chicken <pride.chicken@kakaocorp.com>
2021-12-28 14:41:30 +0900

    [refactor] toString 오버라이드
    - show 함수로 보여주던 것을 toString을 이용해 출력시킴
    - 오버라이드 과정에서 string으로 보여주도록 변경

--- src/ladder.java
+++ src/ladder.java
@@ -41,11 +41,15 @@ public class Ladder {
         return new Random().nextInt(100) >= percent;
     }
 
-        System.out.println("참여할 사람은 몇 명인가요 ?");
-        twoParameters[0] = Integer.parseInt(bufferedReader.readLine());
-
-        System.out.println("최대 사다리 높이는 몇 개인가요 ?");
-        twoParameters[1] = Integer.parseInt(bufferedReader.readLine());
-        return twoParameters;
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        for(ArrayList<Character> line : ladder){
+            for(Character element : line){
+                result.append(element);
+            }
+            result.append("\n");
+        }
+        return result.toString();
     }
 }
