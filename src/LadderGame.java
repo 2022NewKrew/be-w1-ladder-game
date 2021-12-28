1d6416434b58036ae54d2052941e1b95b85f9797
jaden <jaden.dev@kakaocorp.com>
2021-12-28 19:00:59 +0900

    Step2 : 사다리게임 입력 및 출력 구현 (indent 단계 줄이기, else문 제거하기) - reformat을 통한 코드 정리

--- src/LadderGame.java
+++ src/LadderGame.java
@@ -21,25 +21,26 @@ public class LadderGame {
     // else 문을 제거하기 위해선 if ~ 후 return하는 방법도 있지만, 문자 고유의 아스키코드가 있다는 점에 착안했습니다.
     // 변화하는 c값(column값)에 따라 적절한 아스키코드값을 가질 수 있다면, if문을 아예 쓰지 않아도 되리라 판단하고 메서드를 작성했습니다.
     private char ladderElement(int c) {
-        int ascii = LADDER - (c%2) * (LADDER - CONNECTION + (CONNECTION-SPACE) * (int)(Math.random()*2));
-        return (char)ascii;
+        int ascii = LADDER - (c % 2) * (LADDER - CONNECTION + (CONNECTION - SPACE) * (int) (Math.random() * 2));
+        return (char) ascii;
     }
+
     private StringBuilder singleRow() {
         StringBuilder row = new StringBuilder();
-        for (int c=0; c<2*people-1; c++) {
+        for (int c = 0; c < 2 * people - 1; c++) {
             row.append(ladderElement(c));
         }
         return row;
     }
 
     public void makeLadder() {
-        for (int r=0; r<height; r++) {
+        for (int r = 0; r < height; r++) {
             ladder.add(singleRow());
         }
     }
 
     public void printLadder() {
-        for (int r=0; r<height; r++) {
+        for (int r = 0; r < height; r++) {
             System.out.println(ladder.get(r));
         }
     }
