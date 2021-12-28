ff769cf4fcb3275ad494a121d37837d5afd31560
HyunJi <hyunjilim98@gmail.com>
2021-12-28 17:35:14 +0900

    입력 클래스 분리

--- src/Application.java
+++ src/Application.java
@@ -1,29 +1,11 @@
 import ladder.Ladder;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class Application {
     public static void main(String[] args) throws IOException {
-        ApplicationInput input = getInput();
-        Ladder ladder = new Ladder(input.people, input.height);
-        System.out.println(ladder);
-    }
-
-    private static ApplicationInput getInput() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         ApplicationInput input = new ApplicationInput();
-
-        System.out.println("참여할 사람은 몇 명인가요?");
-        input.people = Integer.parseInt(br.readLine());
-        System.out.println("최대 사다리 높이는 몇 개인가요?");
-        input.height = Integer.parseInt(br.readLine());
-        return input;
-    }
-
-    static class ApplicationInput {
-        private int people;
-        private int height;
+        Ladder ladder = new Ladder(input.getPeople(), input.getHeight());
+        System.out.println(ladder);
     }
 }
---
+++ src/ApplicationInput.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ApplicationInput {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    private final int people;
+    private final int height;
+
+    public ApplicationInput() throws IOException {
+        people = inputIntValue("참여할 사람은 몇 명인가요?");
+        height = inputIntValue("최대 사다리 높이는 몇 개인가요?");
+    }
+
+    private int inputIntValue(String expression) throws IOException {
+        System.out.println(expression);
+        return Integer.parseInt(br.readLine());
+    }
+
+    public int getPeople() {
+        return people;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
