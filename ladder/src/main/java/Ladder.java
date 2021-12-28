0c6231a18f151b22d946d1f6d0575984c09c2f3b
roger.lee <2wjdwo97@gmail.com>
2021-12-28 12:17:21 +0900

    *Add duplicate check function
    change List<List<boolean>> to Boolean[][]
    Implement isValid() function

--- ladder/src/main/java/Ladder.java
+++ ladder/src/main/java/Ladder.java
@@ -1,37 +1,39 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Ladder{
-    private final List<List<Boolean>> ladderShape;
+    // 연결되는 곳 true, 아닌 곳 false 로 저장
+    private final Boolean[][] ladderShape;
     private final int numOfParticipants;
     private final int numOfLadderHeight;
 
     public Ladder(int numOfParticipants, int numOfLadderHeight){
         this.numOfParticipants = numOfParticipants;
         this.numOfLadderHeight = numOfLadderHeight;
-        ladderShape = new ArrayList<>();
+        ladderShape = new Boolean[numOfLadderHeight][numOfParticipants - 1];
     }
 
     public void build(){
         for (int row = 0; row < numOfLadderHeight; row++)
-            ladderShape.add(createRow());
+            createRow(row);
     }
 
-    private List<Boolean> createRow(){
-        List<Boolean> newRow = new ArrayList<>();
+    private void createRow(int row){
         Random random = new Random();
-
         for (int bar = 0; bar < numOfParticipants - 1; bar++)
-            newRow.add(random.nextBoolean());
+            ladderShape[row][bar] = isValid(row, bar) && random.nextBoolean();
+    }
 
-        return newRow;
+    private boolean isValid(int row, int bar){
+        if (bar == 0)
+            return true;
+        return !ladderShape[row][bar - 1];
     }
 
     public void printShape(){
-        ladderShape.forEach(
+        Arrays.stream(ladderShape).forEach(
                 row -> {
-                    row.stream()
+                    Arrays.stream(row)
                             .map(this::mapBar)
                             .forEach(bar -> System.out.print("|" + bar));
                     System.out.println("|");
@@ -39,6 +41,6 @@ public class Ladder{
     }
 
     private String mapBar(Boolean barExists){
-        return barExists? " ": "-";
+        return barExists? "-": " ";
     }
 }
