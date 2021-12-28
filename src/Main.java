925db7f1e34453e0c0dc0d7a993e4b296682b26d
jayden-bin <jayden.bin@kakaocorp.com>
2021-12-28 20:49:51 +0900

    Review 받은 수정사항들 수정
    - main함수내에 있는 startLadderGame메소드 static화

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        LadderGame game = new LadderGame();
-        game.startLadderGame();
+        LadderGame.startLadderGame();
     }
 }
