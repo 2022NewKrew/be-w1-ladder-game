6786a1ef4dd9a63faf443c5af3e85c992cd17c36
Gunyoung-Kim <gun025bba@naver.com>
2021-12-28 11:10:08 +0900

    메소드 명 변경: GameStarter의 일부 메소드 직관적인 이름으로 변경

--- src/main/java/com/gunyoung/one/boot/GameStarter.java
+++ src/main/java/com/gunyoung/one/boot/GameStarter.java
@@ -8,6 +8,7 @@ public class GameStarter {
      * for Singleton
      */
     private static GameStarter INSTANCE;
+
     private GameStarter() {}
 
     public static GameStarter getInstance() {
@@ -22,22 +23,22 @@ public class GameStarter {
      * 게임을 시작한다
      */
     public void start() {
-        setDataForGame();
+        inputDataForLadder();
         drawLadder();
     }
 
-    private void setDataForGame() {
-        int numOfUser = takeNumberOfUser();
-        int ladderHeight = takeLadderHeight();
+    private void inputDataForLadder() {
+        int numOfUser = inputNumberOfUser();
+        int ladderHeight = inputLadderHeight();
         Ladder.init(numOfUser, ladderHeight);
     }
 
-    private int takeNumberOfUser() {
+    private int inputNumberOfUser() {
         System.out.println("참여할 사람은 몇 명인가요?");
         return scanner.nextInt();
     }
 
-    private int takeLadderHeight() {
+    private int inputLadderHeight() {
         System.out.println("최대 사다리 높이는 몇 개인가요?");
         return scanner.nextInt();
     }
