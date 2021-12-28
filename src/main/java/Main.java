e2c04c60ac1989d1903a3bc421e12d062472f2c6
everett.lee <kakaocommerce@kakaocommerceui-MacBookPro-3.local>
2021-12-28 12:53:53 +0900

    Step2 구현: depth1 들여쓰기, else 제거

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,15 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
     private static Setting setting = new Setting();
+    private static Scanner scanner = new Scanner(System.in);
+    private static Random random = new Random();
 
     private static boolean[][] ladderGame;
 
-    private static Scanner scanner = new Scanner(System.in);
-
     private static int height;
-
     private static int width;
 
     public static void main(String[] args) throws Exception {
@@ -38,56 +38,75 @@ public class Main {
         if(isNegativeValue(width)) {
             width = 0;
         }
-
         ladderGame = new boolean[height][width];
     }
 
     public static void initializeGameSetting() throws Exception{
+
         try {
-            System.out.println("참여할 사람은 몇 명인가요?");
-            int numberOfMan = scanner.nextInt();
+            int numberOfMan = getUserInputInt("참여할 사람은 몇 명인가요?");
             if(isNegativeValue(numberOfMan)) {
                 throw new Exception("양수를 입력해주세요.");
             }
 
-            System.out.println("최대 사다리 높이는 몇 개인가요?");
-            int maxHeightOfLadder = scanner.nextInt();
+            int maxHeightOfLadder = getUserInputInt("최대 사다리 높이는 몇 개인가요?");
             if(isNegativeValue(maxHeightOfLadder)) {
                 throw new Exception("양수를 입력해주세요.");
             }
 
             setting.setNumberOfMan(numberOfMan);
-            setting.setNumberOfMan(maxHeightOfLadder);
+            setting.setMaxHeightOfLadder(maxHeightOfLadder);
         } catch (Exception ex) {
             throw ex;
         }
+
     }
 
-    public static boolean isNegativeValue(int num) {
-        return num < 0;
+    public static int getUserInputInt(String message) {
+        System.out.println(message);
+        return scanner.nextInt();
     }
 
     public static void generateLadder() {
         for(int row = 0; row < height; row++) {
-            for(int col = 0; col < width; col++) {
-                int randomValue = (int) (Math.random() * 2);
-                ladderGame[row][col] = (randomValue == 1) ? true : false;
-            }
+            generateLadderRow(row);
+        }
+    }
+
+    public static void generateLadderRow(int rowIndex) {
+        for(int col = 0; col < width; col++) {
+            ladderGame[rowIndex][col] = random.nextBoolean();
         }
     }
 
     public static void printLadder() {
         for(int row = 0; row < height; row++) {
-            for(int col = 0; col < width; col++) {
-                System.out.print("|");
-                if(ladderGame[row][col]) {
-                    System.out.print("-");
-                } else {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println("|");
+            printLadderRow(row);
+            System.out.println();
         }
+    }
+
+    public static void printLadderRow(int rowIndex) {
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for(int col = 0; col < width; col++) {
+            stringBuilder.append("|");
+
+            String ladder = existsLadder(rowIndex, col) ? "-" : " ";
+            stringBuilder.append(ladder);
+        }
+        stringBuilder.append("|");
+
+        System.out.print(stringBuilder);
+    }
+
+    public static boolean existsLadder(int rowIndex, int colIndex) {
+        return ladderGame[rowIndex][colIndex];
+    }
+
+    public static boolean isNegativeValue(int num) {
+        return num < 0;
     }
 
 }
