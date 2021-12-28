08d1c4b611ca359032fab2d23cc164853d14b8fd
UpperLeaf <tkdduq8260@gmail.com>
2021-12-28 10:11:22 +0900

    Step-2 구현

--- src/main/java/LadderRenderer.java
+++ src/main/java/LadderRenderer.java
@@ -1,26 +1,27 @@
 public class LadderRenderer {
 
     public void render(Ladder ladder) {
-        int lines = ladder.getPlayerNum();
-        int maxLadderHeight = ladder.getMaxLadderHeight();
-
-        for(int currentHeight = 0; currentHeight < maxLadderHeight; currentHeight++) {
-            for(int currentLine = 0; currentLine < lines; currentLine++) {
-                printLadder(ladder, currentLine, currentHeight);
-            }
-            System.out.println();
+        for(int currentHeight = 0; currentHeight < ladder.getMaxLadderHeight(); currentHeight++) {
+            printHeight(ladder, currentHeight);
         }
     }
 
-    private void printLadder(Ladder ladder, int currentLine, int height) {
+    private void printHeight(Ladder ladder, int height) {
+        for(int currentLine = 0; currentLine < ladder.getPlayerNum(); currentLine++) {
+            printLine(ladder, currentLine, height);
+        }
+        System.out.println();
+    }
+
+    private void printLine(Ladder ladder, int line, int height) {
         System.out.print("|");
-        if(currentLine == ladder.getPlayerNum() - 1){
+        if(line == ladder.getPlayerNum() - 1){
             return;
         }
-        if (!ladder.isConnected(currentLine, currentLine+ 1, height)) {
+        if (!ladder.isConnected(line, line + 1, height)) {
             System.out.print(" ");
-        } else {
-            System.out.print("-");
+            return;
         }
+        System.out.print("-");
     }
 }
