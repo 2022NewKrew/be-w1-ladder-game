b266288c1057edbd2bb9ef319c8826c1943cf8ee
geatrigger-kim <geatrigger.kim@kakaocorp.com>
2021-12-28 10:47:30 +0900

    Make 2d array ladder before draw ladder

--- src/main/java/com/gradle/geatrigger/LadderGame.java
+++ src/main/java/com/gradle/geatrigger/LadderGame.java
@@ -3,31 +3,45 @@ package com.gradle.geatrigger;
 import java.util.Scanner;
 
 public class LadderGame {
+    static int[][] ladder;
+    static int cnt, height;
     public static void main(String[] args) {
-        int cnt, height;
         Scanner sc = new Scanner(System.in);
         System.out.println("참여할 사람은 몇 명인가요?");
         cnt = sc.nextInt();
         System.out.println("최대 사다리 높이는 몇 개인가요?");
         height = sc.nextInt();
-        DrawLadder(cnt, height);
+        ladder = new int[height][cnt];
+        MakeLadder();
+        DrawLadder();
     }
 
-    private static void DrawLadder(int cnt, int height) {
+    private static void MakeLadder() {
         for(int i = 0; i < height; i++) {
             for(int j = 0; j < cnt - 1; j++) {
-                DrawVertical();
-                DrawHorizon();
+                if (Math.random() > 0.5)
+                    ladder[i][j] = 1;
+                else
+                    ladder[i][j] = 0;
             }
-            DrawVertical();
+            ladder[i][cnt - 1] = 0;
+        }
+    }
+
+    private static void DrawLadder() {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < cnt; j++) {
+                DrawVertical();
+                DrawHorizon(ladder[i][j]);
+            }
             System.out.println();
         }
     }
 
-    private static void DrawHorizon() {
-        if (Math.random() > 0.5)
+    private static void DrawHorizon(int cnt) {
+        for (int i = 0; i < cnt; i++)
             System.out.print("-");
-        else
+        if (cnt == 0)
             System.out.print(" ");
     }
 
