caf63f122da8a02fac293d53e72503f826d44b79
moko.ko <moko.ko@kakaocorp.com>
2021-12-28 11:33:59 +0900

    Ladder 코드 수정
    사다리 인스턴스 생성시 "|" 제외

---
+++ ladder-game/src/main/java/Ladder.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Ladder {
+
+    static private final Random randomGenerator = new Random();
+    private final List<String> shape = new ArrayList<>();
+
+    public Ladder(int numberOfPerson, int ladderHeight) throws IllegalArgumentException {
+        checkNumberOfPerson(numberOfPerson);
+        checkLadderHeight(ladderHeight);
+        for (int i = 0; i < ladderHeight; i++) {
+            shape.add(generateLadderLine(numberOfPerson));
+        }
+    }
+
+    private void checkNumberOfPerson(int numberOfPerson) throws IllegalArgumentException {
+        if (numberOfPerson < 1) {
+            throw new IllegalArgumentException("참여하는 사람의 수는 양의 정수여야 합니다.");
+        }
+    }
+
+    private void checkLadderHeight(int ladderHeight) throws IllegalArgumentException {
+        if (ladderHeight < 1) {
+            throw new IllegalArgumentException("사다리의 높이는 양의 정수여야 합니다.");
+        }
+    }
+
+    private String generateLadderLine(int numberOfPerson) {
+        StringBuilder generatedLine = new StringBuilder();
+        for (int i = 0; i < numberOfPerson; i++) {
+            generatedLine.append(generateStep());
+        }
+        return generatedLine.toString();
+    }
+
+    private char generateStep() {
+        if (randomGenerator.nextBoolean()) {
+            return '-';
+        }
+        return ' ';
+    }
+
+    public List<String> getShape() {
+        return shape;
+    }
+}
