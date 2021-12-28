ffd861d1bc434fbeaf5dac381f19d919f5105572
edward.baek <edward.baek@kakaocorp.com>
2021-12-28 18:22:39 +0900

    refactor : 패키지 구조 변경

---
+++ ladder-project/src/test/java/domain/ladder/LadderTest.java
@@ -0,0 +1,19 @@
+package domain.ladder;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class LadderTest {
+
+    @Test
+    void generateRandomLineTest() {
+        //given
+        int column = 3;
+        int height = 5;
+        Ladder ladder = new Ladder(column, height);
+
+        //then
+        assertDoesNotThrow(() -> new AssertionError());
+    }
+}
