da450abf70fc206c0073ffa03ec3b69e934b91e9
charlie.p1 <charlie.p1@kakaocorp.com>
2021-12-28 15:32:29 +0900

    Add LadderMap class

---
+++ app/src/main/java/com/chanminkim/w1/LadderMap.java
@@ -0,0 +1,42 @@
+package com.chanminkim.w1;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class LadderMap {
+    private final Random random;
+    private final LadderState[][] map;
+
+    public LadderMap(Integer numberOfPlayers, Integer heightOfLadder, Random random) {
+        this.random = random;
+        int widthOfLadder = numberOfPlayers * 2 - 1;
+        this.map = buildMap(new LadderState[heightOfLadder][widthOfLadder]);
+    }
+
+    private LadderState[][] buildMap(LadderState[][] map) {
+        return Arrays.stream(map)
+                .map(this::buildLine)
+                .toArray(LadderState[][]::new);
+    }
+
+    private LadderState[] buildLine(LadderState[] line) {
+        for (int i = 1; i < line.length - 1; i += 2) {
+            line[i - 1] = LadderState.VERTICAL;
+            boolean isOccurredBefore = i > 1 && line[i - 2].equals(LadderState.HORIZONTAL);
+            line[i] = buildPiece(isOccurredBefore);
+        }
+        line[line.length - 1] = LadderState.VERTICAL;
+        return line;
+    }
+
+    private LadderState buildPiece(boolean isOccurredBefore) {
+        if (isOccurredBefore) {
+            return LadderState.EMPTY;
+        }
+        return random.nextBoolean() ? LadderState.HORIZONTAL : LadderState.EMPTY;
+    }
+
+    public LadderState[][] getMap() {
+        return map;
+    }
+}
