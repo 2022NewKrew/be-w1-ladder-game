507c3ea6d9bef2530746e147f319773bd8a5f5a9
jake.0208 <jake.0208@kakaocorp.com>
2021-12-28 13:25:40 +0900

    게임 InputManager 생성

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,30 +2,12 @@ import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
-    private static final Scanner sc = new Scanner(System.in);
-
     public static void main(String[] args) {
-        System.out.println("참여할 사람은 몇 명인가요?");
-        int numPeople = inputNumber();
+        InputManager inputManager = new InputManager();
+        StartInfo startInfo = inputManager.getStartInfo();
 
-        System.out.println("최대 사다리 높이는 몇 개인가요?");
-        int maxHeight = inputNumber();
-
-        Ladder ladder = new Ladder(numPeople, maxHeight);
+        Ladder ladder = new Ladder(startInfo.getNumPeople(), startInfo.getMaxHeight());
         ladder.printLadder();
-    }
-    static int inputNumber(){
-        try{
-            int value = Integer.parseInt(sc.nextLine());
-            if(value <=0){
-                throw new NumberFormatException();
-            }
-            return value;
-        }
-        catch(NumberFormatException e){
-            System.out.println("양의 정수를 입력해주세요.");
-            return inputNumber();
-        }
     }
 }
 
