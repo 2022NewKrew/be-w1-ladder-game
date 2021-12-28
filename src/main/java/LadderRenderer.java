cafe2cb760091cf4d125eb63757b88261945b19e
ben.ny <ben.ny@kakaocorp.com>
2021-12-28 17:17:10 +0900

    StringBuilder를 사용해서 print 줄이기

--- src/main/java/LadderRenderer.java
+++ src/main/java/LadderRenderer.java
@@ -2,16 +2,22 @@ import java.util.ArrayList;
 
 public class LadderRenderer {
     public void render(Ladder ladder){
+        StringBuilder ladderString = new StringBuilder();
         int heightOfLadder = ladder.getInfo().getHeightOfLadder();
+
         for(int layer = 0; layer < heightOfLadder; layer++)
-            renderOneLine(ladder.getInfo().getNumOfPeople(), ladder.getConnections(), layer);
+            ladderString.append(renderOneLine(ladder.getInfo().getNumOfPeople(), ladder.getConnections(), layer));
+        System.out.println(ladderString);
     }
 
-    public void renderOneLine(int numOfPeople, ArrayList<ArrayList<Boolean>> connections, int layer){
+    public StringBuilder renderOneLine(int numOfPeople, ArrayList<ArrayList<Boolean>> connections, int layer){
+        StringBuilder ladderStringOneLine = new StringBuilder();
+
         for(int person = 0; person < numOfPeople-1; person++){
-            System.out.print('|');
-            System.out.print(connections.get(layer).get(person) ? "-" : " ");
+            ladderStringOneLine.append('|');
+            ladderStringOneLine.append(connections.get(layer).get(person) ? "-" : " ");
         }
-        System.out.println("|");
+        ladderStringOneLine.append("|\n");
+        return ladderStringOneLine;
     }
 }
