2347f6694a8dadfb88732879cfc5ff66471c00b6
edward.baek <edward.baek@kakaocorp.com>
2021-12-28 16:35:38 +0900

    refactor : main 메서드 수정
    10라인 넘어가지 않도록 수정
    try with resource를 이용하여 자동 메모리 해체하도록 수정

--- ladder-project/src/main/java/Main.java
+++ ladder-project/src/main/java/Main.java
@@ -5,20 +5,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int participantNumber;
-        int height;
-        Scanner sc = new Scanner(System.in);
+        try (Scanner sc = new Scanner(System.in)) {
+            int participantNumber = inputFromScreen("참여할 사람은 몇 명인가요? (1 명 이상 입력해주세요)", sc);
+            int height = inputFromScreen("최대 사다리 높이는 몇 개인가요?", sc);
 
-        System.out.println("참여할 사람은 몇 명인가요? (1 명 이상 입력해주세요)");
-        participantNumber = sc.nextInt();
-        System.out.println("최대 사다리 높이는 몇 개인가요?");
-        height = sc.nextInt();
+            Participant participant = new Participant(participantNumber);
+            LadderGame game = new LadderGame(participant, height);
 
-        Participant participant = new Participant(participantNumber);
-        LadderGame game = new LadderGame(participant, height);
+            game.play();
+        }
+    }
 
-        game.play();
-
-        sc.close();
+    //이 메서드가 (입력/출력) 2가지 일을 한다고 봐야할지 하나의 일을 한다고 봐야할지 잘 모르겠습니다..
+    public static int inputFromScreen(String message, Scanner scanner) {
+        System.out.println(message);
+        return scanner.nextInt();
     }
 }
