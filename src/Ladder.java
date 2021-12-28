e20bdf5344630c78d87f54ab66b92b519a4e870f
jayden-bin <jayden.bin@kakaocorp.com>
2021-12-28 11:57:14 +0900

    complete step2

--- src/Ladder.java
+++ src/Ladder.java
@@ -4,42 +4,38 @@ import java.util.Scanner;
 
 public class Ladder {
 
-    private ArrayList<ArrayList<Boolean>> ladders;
+    private final ArrayList<ArrayList<Boolean>> ladders = new ArrayList<>();
+    private final Random random = new Random();
+    private int width, height;
 
-    public static void main(String[] args) {
+    void inputData() {
         Scanner sc = new Scanner(System.in);
         System.out.println("참여할 사람은 몇 명인가요?");
-        int n = sc.nextInt();
+        width = sc.nextInt();
         System.out.println("최대 사다리 높이는 몇 개인가요?");
-        int m = sc.nextInt();
-        Ladder ladder = new Ladder();
-        ladder.makeLadders(n, m);
-        ladder.printLadders();
+        height = sc.nextInt();
+        sc.close();
     }
 
-    void makeLadders(int n, int m) {
-        Random random = new Random();
-        ladders = new ArrayList<>();
-        for (int i = 0; i < m; i++) {
-            ArrayList<Boolean> line = new ArrayList<>();
-            for (int j = 0; j < n - 1; j++) {
-                line.add(random.nextBoolean());
-            }
-            ladders.add(line);
+    void makeLadders() {
+        for (int i = 0; i < height; i++) {
+            setLaddersData();
         }
+    }
+
+    void setLaddersData() {
+        ArrayList<Boolean> line = new ArrayList<>();
+        for (int j = 0; j < width - 1; j++) {
+            line.add(random.nextBoolean());
+        }
+        ladders.add(line);
     }
 
     void printLadders() {
-        for (ArrayList<Boolean> lines : ladders) {
-            System.out.print("|");
-            for (boolean isLine : lines) {
-                if (isLine)
-                    System.out.print("-");
-                else
-                    System.out.print(" ");
-                System.out.print("|");
-            }
-            System.out.println();
+        for (ArrayList<Boolean> ladder : ladders) {
+            ladder.forEach(it -> System.out.print(it ? "|-" : "| "));
+            System.out.println("|");
         }
     }
+
 }
