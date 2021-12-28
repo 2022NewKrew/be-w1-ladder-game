f245b743eb36fbde7d37c0a7d7e0b5131e568bee
carrot.ez <kakaocommerce@kakaocommerceui-MacBookPro.local>
2021-12-28 18:20:03 +0900

    simplify line connection

--- src/line/Line.java
+++ src/line/Line.java
@@ -18,11 +18,23 @@ public class Line {
     }
 
     public void connect(Line targetLine, int position) {
+        if (isConnectable(position) && targetLine.isConnectable(position)) {
+            this.setConnection(targetLine, position);
+            targetLine.setConnection(this, position);
+        }
+    }
+
+    public void setConnection(Line targetLine, int position) {
         connections.get(position).connect(targetLine);
     }
 
     public boolean isConnectable(int position) {
         return !connections.get(position).isConnected();
+    }
+
+    public boolean isConnectedTo(Line targetLine, int position) {
+        Line connectedLine = getConnectedLine(position);
+        return connectedLine != null && connectedLine.equals(targetLine);
     }
 
     public Line getConnectedLine(int position) {
