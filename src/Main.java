09ffa8f88a8d3fd4c0c4b211c97fa6a4a3fd8333
carrot.ez <kakaocommerce@kakaocommerceui-MacBookPro.local>
2021-12-29 01:36:47 +0900

    refactor input methods

--- src/Main.java
+++ src/Main.java
@@ -7,18 +7,18 @@ public class Main {
     private static final Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        String[] players = inputPlayers("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요.) (최대 5글자)");
-        int ladderHeight = inputLadderHeight("최대 사다리 높이는 몇 개인가요?");
+        String[] players = inputPlayers();
+        int ladderHeight = inputLadderHeight();
 
         new LadderGame(players, ladderHeight).start();
     }
 
-    private static int inputLadderHeight(String msg) {
-        System.out.println(msg);
+    private static int inputLadderHeight() {
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
         return sc.nextInt();
     }
-    private static String[] inputPlayers(String msg) {
-        System.out.println(msg);
+    private static String[] inputPlayers() {
+        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요.) (최대 5글자)");
         return sc.nextLine().split("\\s*[,]\\s*");
     }
 }
