8f082978f69a56fa23c58f5bf44a4de3bea27360
edward.baek <edward.baek@kakaocorp.com>
2021-12-28 18:31:12 +0900

    refactor : initLadder 메서드를 내부에서 호출하도록 변경

--- ladder-project/src/main/java/domain/ladder/Ladder.java
+++ ladder-project/src/main/java/domain/ladder/Ladder.java
@@ -6,14 +6,17 @@ public class Ladder {
 
     private final int line;
     private final int[][] ladder;
+    private final LadderShape ladderShape;
 
     public Ladder(int column, int height) {
         this.ladder = new int[height][column - 1];
         this.line = generateRandomLine();
         assert this.line > -1;
+        initLadder();
+        this.ladderShape = LadderShape.create(ladder);
     }
 
-    public void initLadder() {
+    private void initLadder() {
         for (int i = 0; i < line; i++) {
             int rowPosition = RandomUtil.generateSpecificRange(ladder.length);
             int columnPosition = RandomUtil.generateSpecificRange(ladder[0].length);
@@ -21,28 +24,8 @@ public class Ladder {
         }
     }
 
-    public String generateLadderShape() {
-        StringBuilder sb = new StringBuilder();
-        for (int[] ladderLine : ladder) {
-            sb.append(generateLadderLine(ladderLine));
-            sb.append("\n");
-        }
-        return sb.toString();
-    }
-
-    private StringBuilder generateLadderLine(int[] line) {
-        StringBuilder sb = new StringBuilder();
-        for (int block : line) {
-            sb.append(LadderComponent.LADDER_ROW_LINE.getValue());
-            if (block == 0) {
-                sb.append(LadderComponent.LADDER_EMPTY_COLUMN.getValue());
-            }
-            if (block == 1) {
-                sb.append(LadderComponent.LADDER_COLUMN_LINE.getValue());
-            }
-        }
-        sb.append(LadderComponent.LADDER_ROW_LINE.getValue());
-        return sb;
+    public String getLadderShape() {
+        return ladderShape.getShape();
     }
 
     private int generateRandomLine() {
