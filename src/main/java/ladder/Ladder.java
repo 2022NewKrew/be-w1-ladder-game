b2bd01126ebfdfc1cbc49a996d6811d6bb281416
devwihtpug <devwithpug@kakao.com>
2021-12-28 10:38:15 +0900

    step-2: else문 제거 & StringBuilder 사용

--- src/main/java/ladder/Ladder.java
+++ src/main/java/ladder/Ladder.java
@@ -3,7 +3,6 @@ package ladder;
 public class Ladder {
 
     // TODO - 1. 들여쓰기를 1단계로 줄일 것
-    // TODO - 2. else 를 사용하지 말 것
 
     private final int participants;
     private final int height;
@@ -33,9 +32,8 @@ public class Ladder {
     private boolean isValidConnection(int x, int y) {
         if (x == 0) {
             return getRandomBoolean();
-        } else {
-            return !isAlreadyConnected(x, y) && getRandomBoolean();
         }
+        return !isAlreadyConnected(x, y) && getRandomBoolean();
     }
 
     private boolean isAlreadyConnected(int x, int y) {
@@ -47,17 +45,15 @@ public class Ladder {
     }
 
     public void print() {
+        StringBuilder sb = new StringBuilder();
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < participants - 1; x++) {
-                System.out.print("|");
-                if (bridges[y][x]) {
-                    System.out.print("-");
-                } else {
-                    System.out.print(" ");
-                }
+                sb.append("|");
+                sb.append((bridges[y][x]) ? "-" : " ");
             }
-            System.out.println("|");
+            sb.append("|\n");
         }
+        System.out.print(sb);
     }
 
 }
