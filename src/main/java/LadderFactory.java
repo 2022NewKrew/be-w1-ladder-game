ae479eae424bae5e2fcc7c8d91032060bede28b8
GoonLee <auteur.kor@gmail.com>
2021-12-28 15:54:19 +0900

    Apply code review: create some class to handle input

--- src/main/java/LadderFactory.java
+++ src/main/java/LadderFactory.java
@@ -1,50 +1,10 @@
-import java.util.InputMismatchException;
-import java.util.NoSuchElementException;
-import java.util.Scanner;
-
 public class LadderFactory {
 
-    private int peopleSize;
-    private int height;
-
     public LadderFactory() {
-        Scanner scanner = new Scanner(System.in);
 
-        this.setupPeopleNum(scanner);
-        this.setupHeight(scanner);
     }
 
-    public Ladder buildLadder() {
-        return new Ladder(this.peopleSize, this.height);
-    }
-
-    private void setupPeopleNum(Scanner scanner) {
-        System.out.println("참여할 사람은 몇 명 인가요?");
-
-        try{
-            this.peopleSize = scanner.nextInt();
-            this.validatePositiveInteger(this.peopleSize);
-        } catch (NoSuchElementException | IllegalStateException exception) {
-            System.err.println("입력을 처리할 수 없습니다.");
-            System.exit(1);
-        }
-    }
-
-    private void setupHeight(Scanner scanner) {
-        System.out.println("최대 사다리 높이는 몇 개 인가요?");
-
-        try{
-            this.height = scanner.nextInt();
-            this.validatePositiveInteger(this.height);
-        } catch (NoSuchElementException | IllegalStateException exception) {
-            System.err.println("입력을 처리할 수 없습니다.");
-            System.exit(1);
-        }
-    }
-
-    private void validatePositiveInteger(int value) throws InputMismatchException {
-        if (value <= 0) {
-            throw new InputMismatchException();
-        }
+    public Ladder buildLadder(int peopleSize, int height) {
+        return new Ladder(peopleSize, height);
     }
 }
--- src/main/java/LadderGame.java
+++ src/main/java/LadderGame.java
@@ -1,7 +1,12 @@
 public class LadderGame {
     public static void main(String[] args) {
+        int peopleSize, height;
+
+        peopleSize = InputHandler.getPeopleSize();
+        height = InputHandler.getHeight();
+
         LadderFactory ladderFactory = new LadderFactory();
-        Ladder ladder = ladderFactory.buildLadder();
+        Ladder ladder = ladderFactory.buildLadder(peopleSize, height);
         ladder.printLadder();
     }
 }
