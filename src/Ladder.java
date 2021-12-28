2266d86211b08ffbc651be2f60fbd8c235c1f9d1
vince.joe <seonghyeoncho96@gmail.com>
2021-12-28 11:20:32 +0900

    클래스로 변경

---
+++ src/Ladder.java
@@ -0,0 +1,47 @@
+import java.util.Random;
+
+public class Ladder {
+    private final int numOfPeople;
+    private final int height;
+    private final char[][] ladder;
+
+    Ladder(int numOfPeople, int height) {
+        this.numOfPeople = numOfPeople;
+        this.height = height;
+        this.ladder = new char[height][getWidth()];
+
+        construct();
+    }
+
+    private int getWidth() {
+        return numOfPeople * 2 - 1;
+    }
+
+    private void construct() {
+        Random random = new Random();
+
+        for (int i=0; i<height; ++i) {
+            for (int j=0; j<getWidth(); ++j) {
+                if (j%2 == 0) {
+                    ladder[i][j] = '|';
+                } else {
+                    ladder[i][j] = (random.nextBoolean() ? '-' : ' ');
+                }
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        for (char[] row: ladder) {
+            for (char c: row) {
+                sb.append(c);
+            }
+            sb.append('\n');
+        }
+
+        return sb.toString();
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,8 @@ public class Main {
         int count = _getIntFromScanner(scanner, "참여할 사람은 몇 명인가요? ");
         int height = _getIntFromScanner(scanner, "사다리 높이는 몇개인가요? ");
 
-        char[][] ladder = _generateLadder(count, height);
-        _fillLadder(ladder);
-        _printLadder(ladder);
+        Ladder ladder = new Ladder(count, height);
+        System.out.println(ladder);
     }
 
     /**
@@ -32,42 +31,5 @@ public class Main {
         } while (input <= 0);
 
         return input;
-    }
-
-    /**
-     * @param count 사람 수
-     * @param height 사다리 높이
-     * @return 빈 사다리
-     */
-    private static char[][] _generateLadder(int count, int height) {
-        int width = 2 * count - 1;
-        return new char[height][width];
-    }
-
-    /**
-     * {@code ladder} 내부를 랜덤하게 채움
-     */
-    private static void _fillLadder(char[][] ladder) {
-        for (int i=0; i<ladder.length; ++i) {
-            for (int j=0; j<ladder[0].length; ++j) {
-                if (j % 2 == 0) {
-                    ladder[i][j] = '|';
-                } else {
-                    ladder[i][j] = (Math.random() < 0.5 ? '-' : ' ');
-                }
-            }
-        }
-    }
-
-    /**
-     * {@code ladder} 출력
-     */
-    private static void _printLadder(char[][] ladder) {
-        for (char[] chars : ladder) {
-            for (char c : chars) {
-                System.out.print(c);
-            }
-            System.out.println();
-        }
     }
 }
