b01b028d55272309e331f16e76403beb51dbe05e
easter.egg <easter.egg@kakaocorp.com>
2021-12-28 10:44:01 +0900

    2일차 ladderGame 수정 ( 들여쓰기 제한 / 함수 분할 )

--- LadderGame/src/LadderGame.java
+++ LadderGame/src/LadderGame.java
@@ -9,17 +9,17 @@ public class LadderGame {
     private BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
     private BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    private int width, height;
+    private int userCnt, ladderHeight;
     private List<List<Boolean>> ladder;
 
     // 입력함수
-    public void inputWidth () throws Exception {
+    private void inputWidth () throws Exception {
         System.out.println("참여할 사람은 몇 명인가요?");
-        this.width = Integer.parseInt(bf.readLine());
+        this.userCnt = Integer.parseInt(bf.readLine());
     }
-    public void inputHeight() throws Exception {
+    private void inputHeight() throws Exception {
         System.out.println("최대 사다리 높이는 몇 개인가요?");
-        this.height = Integer.parseInt(bf.readLine());
+        this.ladderHeight = Integer.parseInt(bf.readLine());
     }
 
     // 사다리 생성
@@ -28,11 +28,14 @@ public class LadderGame {
         this.inputHeight();
 
         this.ladder = new ArrayList<>(); // 새 사다리
-        for(int h=0; h<this.height; h++) {
-            List<Boolean> row = new ArrayList<>(); // 한 줄
-            for(int w=1; w<this.width; w++) row.add((int)(Math.random() * 2) != 0); // 연결점 생성
-            ladder.add(row);
+        for(int h = 0; h<this.ladderHeight; h++) { // 한 줄씩 사다리를 생성
+            ladder.add(makeRow());
         }
+    }
+    private List<Boolean> makeRow() { // 한 줄의 사다리 생성
+        List<Boolean> row = new ArrayList<>();
+        for(int w = 1; w<this.userCnt; w++) row.add((int)(Math.random() * 2) != 0); // 연결점 생성
+        return row;
     }
 
     // 사다리 출력
@@ -41,18 +44,22 @@ public class LadderGame {
 
         StringBuilder sb = new StringBuilder();
         for(List<Boolean> row: this.ladder){ // 높이
-            sb.append('|'); // 첫 막대기
-            for(Boolean bridge: row){ // 너비
-                // 중간다리 판단
-                sb.append(bridge ? '-' : ' ');
-                sb.append('|');
-            }
-            sb.append('\n'); // 줄바꿈
+            printRow(sb, row); // 한 줄을 출력
         }
         bw.write(sb.toString());
         bw.flush();
     }
+    private void printRow(StringBuilder sb, List<Boolean> row) { // 한 줄 출력
+        sb.append('|'); // 첫 막대기
+        for(Boolean bridge: row){ // 너비
+            // 중간다리 판단
+            sb.append(bridge ? '-' : ' ');
+            sb.append('|');
+        }
+        sb.append('\n'); // 줄바꿈
+    }
 
+    // 메인
     public static void main(String[] args) throws Exception {
         LadderGame lg = new LadderGame();
         lg.printLadder();
