8e36d84f624a8bd3b98dcd56720ed70b87c8e9d7
binary.yun <binary.yun@kakaocorp.com>
2021-12-28 18:51:21 +0900

    refactor: people의 변수명을 직관적으로 변경

--- src/main/java/LadderGame.java
+++ src/main/java/LadderGame.java
@@ -5,12 +5,12 @@ public class LadderGame {
 
     public static void main(String[] args) {
         System.out.println("참여할 사람은 몇 명인가요?");
-        int people = inputNumber();
+        int numOfPeople = inputNumber();
 
         System.out.println("최대 사다리 높이는 몇 개인가요?");
         int heightLadder = inputNumber();
 
-        Ladder ladder = new Ladder(people, heightLadder);
+        Ladder ladder = new Ladder(numOfPeople, heightLadder);
         System.out.println(ladder);
     }
 
