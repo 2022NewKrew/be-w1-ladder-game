b857083f5c43c428ceb5fa6167654bccb37331bf
miya.ong <miya.ong@kakaocorp.com>
2021-12-28 15:22:20 +0900

    refactor LadderGame class

--- src/main/java/LadderGame.java
+++ src/main/java/LadderGame.java
@@ -5,24 +5,32 @@ public class LadderGame {
     private Ladder ladder = null;
 
     public void initGame() {
-        Scanner sc = new Scanner(System.in);
-        int participantsNum, maxHeight;
         try {
-            System.out.println("참여할 사람은 몇 명인가요?");
-            participantsNum = sc.nextInt();
-            System.out.println("최대 사다리 높이는 몇 개인가요?");
-            maxHeight = sc.nextInt();
-            if (participantsNum <= 0 || maxHeight <= 0)
-                throw new InputMismatchException();
-            ladder = new Ladder(participantsNum, maxHeight);
+            ladder = createLadder();
         } catch (InputMismatchException e) {
-            System.err.println("양의 정수를 입력하셈");
+            System.err.println("사다리 정보가 잘못되었음");
         }
     }
 
     public void showStatus() {
         try {
             ladder.printLadder();
-        } catch (NullPointerException e) {}
+        } catch (NullPointerException e) {
+            System.err.println("사다리가 없음");
+        }
+    }
+
+    private Ladder createLadder() {
+        Scanner sc = new Scanner(System.in);
+        int participantsNum, maxHeight;
+
+        System.out.println("참여할 사람은 몇 명인가요?");
+        participantsNum = sc.nextInt();
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        maxHeight = sc.nextInt();
+        if (participantsNum <= 0 || maxHeight <= 0)
+            throw new InputMismatchException();
+
+        return new Ladder(participantsNum, maxHeight);
     }
 }
