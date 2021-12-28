8f9598788d28970b58d3cbf021347f4d44300bb9
YoonRyeol <yunyul3@gmail.com>
2021-12-28 13:51:47 +0900

    2021-12-28 | YR | IntInput 클래스 추가 (2일차 구현 완료)

---
+++ src/main/java/IntInput.java
@@ -0,0 +1,11 @@
+public class IntInput {
+    int value;
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
--- src/main/java/LadderGame.java
+++ src/main/java/LadderGame.java
@@ -1,8 +1,7 @@
-import java.util.Random;
 import java.util.Scanner;
 
 public class LadderGame {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         UserInterface userInterface = new UserInterface(new Scanner(System.in));
         userInterface.makeLadder();
         userInterface.printLadder();
