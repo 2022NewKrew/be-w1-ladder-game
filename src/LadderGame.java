b1ca31a345a40ce1cb9878d67d474812ea586dbf
jaden <jaden.dev@kakaocorp.com>
2021-12-28 19:29:30 +0900

    Step2 : 사다리게임 입력 및 출력 구현 (indent 단계 줄이기, else문 제거하기) - 코드 리뷰 후 수정사항 반영 - 사다리에 들어갈 element를 계산하는 과정을 가독성있게 과정별로 나누어 수정 - private 메서드들을 클래스 뒤편으로 배치 - 다소 추상적이었던 변수명을 명확하게 변경

--- src/LadderGame.java
+++ src/LadderGame.java
@@ -3,7 +3,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class LadderGame {
-    int people;
+    int width;
     int height;
     List<StringBuilder> ladder = new ArrayList<>();
     private final int LADDER = 124;
@@ -13,24 +13,10 @@ public class LadderGame {
     LadderGame() {
         Scanner sc = new Scanner(System.in);
         System.out.println("참여할 사람은 몇 명인가요?");
-        people = sc.nextInt();
+        int people = sc.nextInt();
+        width = 2 * people - 1;
         System.out.println("최대 사다리 높이는 몇 개인가요?");
         height = sc.nextInt();
-    }
-
-    // else 문을 제거하기 위해선 if ~ 후 return하는 방법도 있지만, 문자 고유의 아스키코드가 있다는 점에 착안했습니다.
-    // 변화하는 c값(column값)에 따라 적절한 아스키코드값을 가질 수 있다면, if문을 아예 쓰지 않아도 되리라 판단하고 메서드를 작성했습니다.
-    private char ladderElement(int c) {
-        int ascii = LADDER - (c % 2) * (LADDER - CONNECTION + (CONNECTION - SPACE) * (int) (Math.random() * 2));
-        return (char) ascii;
-    }
-
-    private StringBuilder singleRow() {
-        StringBuilder row = new StringBuilder();
-        for (int c = 0; c < 2 * people - 1; c++) {
-            row.append(ladderElement(c));
-        }
-        return row;
     }
 
     public void makeLadder() {
@@ -44,4 +30,32 @@ public class LadderGame {
             System.out.println(ladder.get(r));
         }
     }
+
+    // else 문을 제거하기 위해선 if ~ 후 return하는 방법도 있지만, 문자 고유의 아스키코드가 있다는 점에 착안했습니다.
+    // 변화하는 c값(column값)에 따라 적절한 아스키코드값을 가질 수 있다면, if문을 아예 쓰지 않아도 되리라 판단하고 메서드를 작성했습니다.
+    private char ladderElement(int column) {
+        // elemet의 값은 세로로 이어진 사다리 LADDER로 초기화
+        int element = LADDER;
+        // 0 또는 1의 값을 갖는 랜덤 숫자
+        int randomNumber = (int) (Math.random() * 2);
+        // 랜덤 숫자에 따라 CONNECTION 인지, SPACE 인지를 결정할 숫자
+        int isConnection = (CONNECTION - SPACE) * randomNumber;
+        // CONNECTION인지 SPACE인지 결정한 후, LADDER 값과의 차이를 보정
+        isConnection += LADDER - CONNECTION;
+        // 만약 column이 짝수라면 0, 홀수라면 isConnection 이 되는 값
+        int isLadder = (column % 2) * isConnection;
+        // 기존 element(=LADDER)에서 값을 빼주어 적절한 element ascii값을 얻음
+        element -= isLadder;
+        return (char) element;
+    }
+
+    private StringBuilder singleRow() {
+        StringBuilder row = new StringBuilder();
+        for (int c = 0; c < width; c++) {
+            row.append(ladderElement(c));
+        }
+        return row;
+    }
+
+
 }
