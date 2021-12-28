be6d801a70befbb5ca5e3cf586d7455cdd6c3ef7
edward.baek <edward.baek@kakaocorp.com>
2021-12-28 18:27:10 +0900

    feat : LadderShape 클래스 생성
    Ladder 클래스에서 형태를 생성한는 것을 따로 클래스로 분리하기 위해서 생성

---
+++ ladder-project/src/test/java/domain/ladder/LadderShapeTest.java
@@ -0,0 +1,23 @@
+package domain.ladder;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LadderShapeTest {
+
+    @Test
+    void createTest() {
+        //given
+        int[][] ladder = new int[2][2];
+        ladder[0][1] = 1;
+        ladder[1][0] = 1;
+        String expected = ("| |-|\n|-| |\n");
+
+        //when
+        LadderShape result = LadderShape.create(ladder);
+
+        //then
+        assertEquals(expected, result.getShape());
+    }
+}
