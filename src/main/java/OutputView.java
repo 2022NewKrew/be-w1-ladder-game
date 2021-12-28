c875a0d94c8e1d8e1275c08f55551423a7be72a6
meg.j <meg.j@kakaocorp.com>
2021-12-28 18:45:35 +0900

    step2 완료

--- src/main/java/OutputView.java
+++ src/main/java/OutputView.java
@@ -2,10 +2,21 @@ public class OutputView {
 
     public static void printLadder(String[][] ladder) {
         for(String[] row : ladder) {
-            for(String el : row) {
-                System.out.print(el);
-            }
-            System.out.println();
+            printLadderLine(row);
         }
+    }
+
+    private static void printLadderLine(String[] row) {
+        for (int i=0 ; i<row.length*2+1; i++) {
+            System.out.print(getElement(row, i));
+        }
+        System.out.println();
+    }
+
+    private static String getElement(String[] row , int i) {
+        if (i%2==1) {
+            return row[i/2];
+        }
+        return "|";
     }
 }
