dd4224c5fe8b0923f6ac30a41b720f1fe2230b17
theo.94 <theo.94@kakaocorp.com>
2021-12-28 11:47:42 +0900

    Refactor : 사다리 게임 2단계 구현

--- mission1/src/main/java/LadderGame.java
+++ mission1/src/main/java/LadderGame.java
@@ -1,17 +1,36 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class LadderGame {
     public static void main(String[] args) {
         System.out.println("사다리 게임을 시작합니다.");
-        Scanner scanner = new Scanner(System.in);
 
         System.out.println("참여할 사람은 몇 명인가요?");
-        int numOfPlayer = Integer.parseInt(scanner.nextLine());
+        int numOfPlayer = getInput();
 
         System.out.println("최대 사다리 높이는 몇 개인가요?");
-        int maxHeight = Integer.parseInt(scanner.nextLine());
+        int maxHeight = getInput();
 
         LadderGamePlayer player = new LadderGamePlayer(maxHeight, numOfPlayer);
         player.play();
+    }
+
+    static int getInput() {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            int result = scanner.nextInt();
+            return checkInput(result) ? result : getInput();
+        }
+        catch (InputMismatchException e) {
+            return getInput();
+        }
+    }
+
+    static boolean checkInput(int value) {
+        if (value > 0)
+            return true;
+        System.out.println("0보다 큰 정수를 입력해주세요.");
+        return false;
     }
 }
--- mission1/src/main/java/LadderGamePlayer.java
+++ mission1/src/main/java/LadderGamePlayer.java
@@ -1,49 +1,55 @@
 import java.util.Random;
 
 public class LadderGamePlayer {
-    private int maxHeight;
-    private int numOfRow;
+    private final int maxHeight;
+    private final int numOfRow;
+    private char[][] ladder;
 
     public LadderGamePlayer(int maxHeight, int numOfRow) {
         this.maxHeight = maxHeight;
         this.numOfRow = numOfRow;
+        this.ladder = new char[maxHeight][numOfRow * 2 - 1];
     }
 
     public void play(){
-        // 사다리 틀 만들기
-        char[][] ladder = makeLadder(maxHeight, numOfRow * 2 - 1);
-
-        // 사다리 채우기
-        fillLadder(ladder);
+        // 사다리 작성
+        makeLadder(ladder);
 
         // 사다리 출력
+        printLadder(ladder);
+    }
+
+    private void makeLadder(char[][] ladder){
+        for (char[] row : ladder) {
+            // 한 줄씩 채우기
+            fillRow(row);
+        }
+    }
+
+    private void fillRow(char[] row){
+        // 세로 줄
+        for(int i=0; i<row.length; i+=2)
+            row[i] = '|';
+
+        // 가로 줄
+        for(int i= 0; i< row.length; i++){
+            row[i] = getRandom();
+        }
+    }
+
+    private char getRandom() {
+        Random random = new Random();
+        char result = ' ';
+
+        if(random.nextBoolean())
+            result = '-';
+
+        return result;
+    }
+
+    private void printLadder(char[][] ladder) {
         for(int i=0; i<maxHeight; i++){
             System.out.println(ladder[i]);
-        }
-    }
-
-    public char[][] makeLadder(int height, int width){
-        char[][] ladder = new char[height][width];
-
-        // 세로 줄만 채운다
-        for(int i=0; i<height; i++){
-            for(int j=0; j<width; j+=2){
-                ladder[i][j] = '|';
-            }
-        }
-        return ladder;
-    }
-
-    public void fillLadder(char[][] ladder){
-        Random random = new Random();
-
-        for(int i= 0; i< ladder.length; i++){
-            for(int j= 1; j< ladder[0].length; j+=2){
-                if(random.nextBoolean())
-                    ladder[i][j] = '-';
-                else
-                    ladder[i][j] = ' ';
-            }
         }
     }
 }
