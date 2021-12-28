45789def48a938b5b902a4dce2ecfc29e6e3e1e6
moko.ko <moko.ko@kakaocorp.com>
2021-12-28 13:28:19 +0900

    LadderGame 싱글톤 패턴으로 변경
    LadderGame 은 하나의 인스턴스만 있으면 충분하기 때문에
    싱글톤 패턴으로 변경

--- ladder-game/src/main/java/Application.java
+++ ladder-game/src/main/java/Application.java
@@ -1,7 +1,7 @@
 public class Application {
     static public void main(String[] args) {
         try {
-            LadderGame ladderGame = new LadderGame(UserInput.getNumberOfPeople(), UserInput.getLadderHeight());
+            LadderGame ladderGame = LadderGame.getInstance(UserInput.getNumberOfPeople(), UserInput.getLadderHeight());
             ladderGame.printLadder();
         } catch (IllegalArgumentException illegalArgumentException) {
             System.out.println(illegalArgumentException.getMessage());
