9ad9a69fab160178d14801ab181720c199d9fda1
SaeGon-Heo <her84562@gmail.com>
2021-12-28 17:38:49 +0900

    사다리 게임 2단계 구현 요구사항 만족 및 최대한 객체지향적으로 코드 정리

--- src/main/java/org/cs/finn/laddergame/Main.java
+++ src/main/java/org/cs/finn/laddergame/Main.java
@@ -4,10 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         LadderGame ladderGame = new LadderGame();
-
-        ladderGame.requestParameter();
-        ladderGame.createLadder();
-        ladderGame.printLadder();
-        ladderGame.closeGame();
+        ladderGame.run();
     }
 }
