f44a9118b573c89d7bcfc338ddd4eec1371ec320
anika.dovob <anika.dovob@kakaocorp.com>
2021-12-28 16:59:34 +0900

    LadderGame의 maxHeight 변수명을 ladderHeight로 변경

--- src/main/java/laddergame/LadderGame.java
+++ src/main/java/laddergame/LadderGame.java
@@ -2,13 +2,13 @@ package laddergame;
 
 public class LadderGame {
     private final int numberOfGamer;
-    private final int maxHeight;
+    private final int ladderHeight;
     private final Ladder ladder;
 
     public LadderGame() {
         this.numberOfGamer = InputView.inputNumberOfGamer();
-        this.maxHeight = InputView.inputLadderHeight();
-        this.ladder = new Ladder(numberOfGamer, maxHeight);
+        this.ladderHeight = InputView.inputLadderHeight();
+        this.ladder = new Ladder(numberOfGamer, ladderHeight);
     }
 
     public void printLadder() {
