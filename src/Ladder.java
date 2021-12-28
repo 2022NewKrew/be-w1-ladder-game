7f269f3a132e83c1da97b6b1ec4493ae3fd4c40b
jayden-bin <jayden.bin@kakaocorp.com>
2021-12-28 18:12:30 +0900

    apply reviews

--- src/Ladder.java
+++ src/Ladder.java
@@ -1,38 +1,38 @@
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Ladder {
 
-    private final ArrayList<ArrayList<Boolean>> ladders = new ArrayList<>();
+    private final List<List<Boolean>> ladders = new ArrayList<>();
     private final Random random = new Random();
     private int width, height;
 
-    void inputData() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("참여할 사람은 몇 명인가요?");
-        width = sc.nextInt();
-        System.out.println("최대 사다리 높이는 몇 개인가요?");
-        height = sc.nextInt();
-        sc.close();
+     Ladder(int width,int height){
+        this.width = width;
+        this.height = height;
+        makeLadders();
+        printLadders();
     }
 
-    void makeLadders() {
+
+    private void makeLadders() {
         for (int i = 0; i < height; i++) {
             setLaddersData();
         }
     }
 
-    void setLaddersData() {
-        ArrayList<Boolean> line = new ArrayList<>();
+    private void setLaddersData() {
+        List<Boolean> line = new ArrayList<>();
         for (int j = 0; j < width - 1; j++) {
             line.add(random.nextBoolean());
         }
         ladders.add(line);
     }
 
-    void printLadders() {
-        for (ArrayList<Boolean> ladder : ladders) {
+    private void printLadders() {
+        for (List<Boolean> ladder : ladders) {
             ladder.forEach(it -> System.out.print(it ? "|-" : "| "));
             System.out.println("|");
         }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        Ladder ladder = new Ladder();
-        ladder.inputData();
-        ladder.makeLadders();
-        ladder.printLadders();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("참여할 사람은 몇 명인가요?");
+        int width = sc.nextInt();
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        int height = sc.nextInt();
+        sc.close();
+        Ladder ladder = new Ladder(width,height);
+
     }
 }
