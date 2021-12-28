88e1ff96082f5b2f3f466ff576de27f659d608c3
SaeGon-Heo <her84562@gmail.com>
2021-12-28 14:32:20 +0900

    Rebuild code structure

---
+++ src/main/java/org/cs/finn/laddergame/domain/LadderData.java
@@ -0,0 +1,38 @@
+package org.cs.finn.laddergame.domain;
+
+import java.security.SecureRandom;
+import java.util.ArrayList;
+
+public class LadderData {
+    private final ArrayList<String> bridges = new ArrayList<>();
+    private final SecureRandom sRand = new SecureRandom();
+
+    public void build(final InputParameter iPara) {
+        bridges.clear();
+
+        final int member = iPara.getMember();
+        final int bound = 1 << (member - 1);
+        final int height = iPara.getLadderHeight();
+
+        for (int i = 0; i < height; i++) {
+            // bound를 추가로 더해서
+            // 사다리를 놓을 수 있는 최대 개수 + 1 위치에 1을 항상 추가하여
+            // Zero-fill 처리하고 가장 위쪽 1을 잘라낸 뒤 저장한다
+            final String bridge = Integer.toBinaryString(bound + sRand.nextInt(bound));
+            bridges.add(bridge.substring(1));
+        }
+    }
+
+    public void print() {
+        for (String bridge : bridges) {
+            printOneHeight(bridge);
+        }
+    }
+
+    private void printOneHeight(final String bridge) {
+        for (char ch : bridge.toCharArray()) {
+            System.out.print("|" + (ch == '1' ? "-" : " "));
+        }
+        System.out.println("|");
+    }
+}
