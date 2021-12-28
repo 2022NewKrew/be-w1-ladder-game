b68dced01b0ae4675cf3ddb25e915b02bfafc1c4
theo.94 <theo.94@kakaocorp.com>
2021-12-28 20:58:16 +0900

    Refactor : 피드백을 반영한 사다리 게임 2단계 구현
    - 사다리 작성과 렌더링 클래스 분리
    - 메인 클래스와 입력값을 받는 클래스 분리
    - 가로줄 출력 오류 수정
    - 기타 코드 스타일 수정

--- mission1/src/main/java/LadderGame.java
+++ mission1/src/main/java/LadderGame.java
@@ -1,36 +1,9 @@
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
 public class LadderGame {
     public static void main(String[] args) {
         System.out.println("사다리 게임을 시작합니다.");
 
-        System.out.println("참여할 사람은 몇 명인가요?");
-        int numOfPlayer = getInput();
-
-        System.out.println("최대 사다리 높이는 몇 개인가요?");
-        int maxHeight = getInput();
-
-        LadderGamePlayer player = new LadderGamePlayer(maxHeight, numOfPlayer);
-        player.play();
-    }
-
-    static int getInput() {
-        Scanner scanner = new Scanner(System.in);
-
-        try {
-            int result = scanner.nextInt();
-            return checkInput(result) ? result : getInput();
-        }
-        catch (InputMismatchException e) {
-            return getInput();
-        }
-    }
-
-    static boolean checkInput(int value) {
-        if (value > 0)
-            return true;
-        System.out.println("0보다 큰 정수를 입력해주세요.");
-        return false;
+        LadderInputManager ladderInputManager = new LadderInputManager();
+        LadderMaker ladderMaker = new LadderMaker(ladderInputManager);
+        LadderRenderer.render(ladderMaker.getLadder());
     }
 }
