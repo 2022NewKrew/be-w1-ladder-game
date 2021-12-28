8e3aa8ef960d04d9384006bf89c2e652b6a3adac
YoonRyeol <yunyul3@gmail.com>
2021-12-28 16:38:12 +0900

    2021-12-28 | YR | 문자열 입력, 조건 구현 완료

--- src/main/java/LstSizeStrCond.java
+++ src/main/java/LstSizeStrCond.java
@@ -1,15 +1,13 @@
 import java.util.ArrayList;
 
-public class LstSizeStrCond implements StrCond {
+public class LstSizeStrCond implements StrCond{
     int limit;
-
-    public LstSizeStrCond(int limit) {
+    public LstSizeStrCond(int limit){
         this.limit = limit;
     }
-
     @Override
     public boolean isValid(ArrayList<String> strLst, String str) {
-        if (strLst.size() <= limit) return false;
+        if(strLst.size() <= limit) return false;
         return true;
     }
 }
--- src/main/java/StrInputCondition.java
+++ src/main/java/StrInputCondition.java
@@ -1,38 +1,38 @@
 import java.util.ArrayList;
 
-public class StrInputCondition implements InputCondition {
+public class StrInputCondition implements InputCondition{
 
     private String query;
     ArrayList<StrCond> condLst;
     private String errorMsg;
 
-    public StrInputCondition(String query, ArrayList<StrCond> condLst, String errorMsg) {
+    public StrInputCondition(String query, ArrayList<StrCond> condLst, String errorMsg){
         this.query = query;
         this.condLst = condLst;
         this.errorMsg = errorMsg;
     }
 
     @Override
-    public boolean isValid(Object input) {
-        ArrayList<String> words = (ArrayList<String>) input;
-        for (String word : words)
-            if (!checkConds(condLst, words, word)) return false;
+    public boolean isValid(Input input) {
+        ArrayList<String> inputs = (ArrayList<String>) input.getValue();
+        for(String word : inputs)
+            if(!checkConds(condLst, inputs, word)) return false;
         return true;
     }
 
-    private boolean checkConds(ArrayList<StrCond> condLst, ArrayList<String> words, String word) {
-        for (StrCond cond : condLst)
-            if (!cond.isValid(words, word)) return false;
+    private boolean checkConds(ArrayList<StrCond> condLst, ArrayList<String> words, String word){
+        for(StrCond cond : condLst)
+            if(!cond.isValid(words, word)) return false;
         return true;
     }
 
     @Override
     public String getQuery() {
-        return query;
+        return null;
     }
 
     @Override
     public String getErrorMsg() {
-        return errorMsg;
+        return null;
     }
 }
