8ad9b76690238529b96470ea3222329f98620061
bb0711 <bb0711@kaist.ac.kr>
2021-12-28 10:34:16 +0900

    done day1 - review updated

--- src/Ladder.java
+++ src/Ladder.java
@@ -5,6 +5,7 @@ public final class Ladder {
     private final int n_people;
     private final int max_height;
     private char[][] arr; // 크기를 나중에 할당해서 그런지 final을 설정할 수가 없네요ㅠㅠ 다른 방법이 있을까요..?
+    Random random = new Random();
 
     //constructor
     public Ladder(int n, int m) {
@@ -30,7 +31,7 @@ public final class Ladder {
     public void buildSteps() {
         for (int i=0; i<this.arr.length; i++) {
             for (int j=1; j<this.arr[0].length; j+=2) {
-                Random random = new Random();
+
                 float random_float = random.nextFloat();
                 if (random_float <0.5) { // make each step by 50% probability
                     this.arr[i][j]='-';
