64994a1303db986dc7b16b47eebf5cd852cf7eb0
punsoo <sonu.sohn@kakaocorp.com@gmail.com>
2021-12-28 18:33:09 +0900

    refactor:stream.forEach() 제거

--- src/main/java/ResultView.java
+++ src/main/java/ResultView.java
@@ -7,14 +7,10 @@ public class ResultView {
     }
     public static void printLadder(ArrayList<ArrayList<String>> ladder) {
         System.out.println();
-        ladder
-                .stream()
-                .forEach(ResultView::printLine);
+        ladder.forEach(ResultView::printLine);
     }
     private static void printLine(ArrayList<String> line) {
-        line
-                .stream()
-                .forEach(System.out::print);
+        line.forEach(System.out::print);
         System.out.println();
     }
 }
