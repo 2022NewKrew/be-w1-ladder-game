fddfbd74dfb3dda26732042d519d292388681fd8
charlie.p1 <charlie.p1@kakaocorp.com>
2021-12-28 16:14:17 +0900

    Update LadderGame to use LadderGame and LadderConverter

--- app/src/main/java/com/chanminkim/w1/LadderGame.java
+++ app/src/main/java/com/chanminkim/w1/LadderGame.java
@@ -3,42 +3,13 @@ package com.chanminkim.w1;
 import java.util.Random;
 
 public class LadderGame {
-    private final Integer numberOfPlayers;
-    private final Integer heightOfLadder;
-    private final Random random;
+    private final LadderMap ladderMap;
 
     public LadderGame(Integer numberOfPlayers, Integer heightOfLadder, Random random) {
-        this.numberOfPlayers = numberOfPlayers;
-        this.heightOfLadder = heightOfLadder;
-        this.random = random;
+        this.ladderMap = new LadderMap(numberOfPlayers, heightOfLadder, random);
     }
 
     public String buildLadder() {
-        StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < heightOfLadder; i++) {
-            builder.append(buildLine());
-            builder.append('\n');
-        }
-        return builder.toString();
-    }
-
-    private String buildLine() {
-        StringBuilder builder = new StringBuilder();
-        boolean isOccurredBefore = false;
-        for (int i = 0; i < numberOfPlayers - 1; i++) {
-            builder.append(LadderState.VERTICAL.getValue());
-            String piece = buildPiece(isOccurredBefore);
-            isOccurredBefore = piece.equals(LadderState.HORIZONTAL.getValue());
-            builder.append(piece);
-        }
-        builder.append(LadderState.VERTICAL.getValue());
-        return builder.toString();
-    }
-
-    private String buildPiece(boolean isOccurredBefore) {
-        if (isOccurredBefore) {
-            return LadderState.EMPTY.getValue();
-        }
-        return random.nextBoolean() ? LadderState.HORIZONTAL.getValue() : LadderState.EMPTY.getValue();
+        return LadderConverter.convertToString(ladderMap);
     }
 }
