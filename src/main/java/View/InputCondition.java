b32fb0774d9507bf87f483f00e0d620a4bdfed72
YoonRyeol <yunyul3@gmail.com>
2021-12-28 13:51:47 +0900

    2021-12-28 | YR | IntInput 클래스 추가 (2일차 구현 완료)

--- src/main/java/View/InputCondition.java
+++ src/main/java/View/InputCondition.java
@@ -3,23 +3,22 @@ public class InputCondition {
     private int limit;
     private String errorMsg;
 
-    public InputCondition(String query, int limit, String errorMsg){
+    public InputCondition(String query, int limit, String errorMsg) {
         this.query = query;
         this.limit = limit;
         this.errorMsg = errorMsg;
     }
 
-    public boolean isValid(int input){
-        if(input > limit) return true;
-        System.out.println(getErrorMsg());
+    public boolean isValid(int input) {
+        if (input > limit) return true;
         return false;
     }
 
-    public String getQuery(){
+    public String getQuery() {
         return query;
     }
 
-    private String getErrorMsg(){
+    public String getErrorMsg() {
         return errorMsg;
     }
 
