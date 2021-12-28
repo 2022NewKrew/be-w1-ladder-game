f87bec537d847c96cee9f6fba18865869253fb6e
Kimgeunwook <wook961206@naver.com>
2021-12-28 10:02:00 +0900

    Ladder, Main 분리 완료

--- week1/src/src/Ladder.java
+++ week1/src/src/Ladder.java
@@ -2,11 +2,11 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Ladder {
-    static int[][] map;
-    static int peopleNum;
-    static int height;
+    private int[][] map;
+    private int peopleNum;
+    private int height;
 
-    public static void getInput(){
+    Ladder(){
         Scanner sc = new Scanner(System.in);
         System.out.println("참여할 사람은 몇 명인가요?");
         peopleNum = sc.nextInt();
@@ -14,28 +14,22 @@ public class Ladder {
         height = sc.nextInt();
     }
 
-    public static void setMap(){
-        map = new int[height][peopleNum - 1];
+    public void setMap(){
+        this.map = new int[this.height][this.peopleNum - 1];
         Random random = new Random();
-        for(int i = 0; i < height; i++)
-            for(int j = 0 ; j < peopleNum - 1; j++)
-                map[i][j] = random.nextInt(2);
+        for(int i = 0; i < this.height; i++)
+            for(int j = 0 ; j < this.peopleNum - 1; j++)
+                this.map[i][j] = random.nextInt(2);
     }
 
-    public static void showMap(){
-        for(int i = 0; i < height; i++) {
-            for(int j = 0 ; j < peopleNum - 1; j++) {
+    public void showMap(){
+        for(int i = 0; i < this.height; i++) {
+            for(int j = 0 ; j < this.peopleNum - 1; j++) {
                 System.out.print("|");
-                if(map[i][j] == 1) System.out.print("-");
-                if(map[i][j] == 0) System.out.print(" ");
+                if(this.map[i][j] == 1) System.out.print("-");
+                if(this.map[i][j] == 0) System.out.print(" ");
             }
             System.out.println("|");
         }
-    }
-
-    public static void main(String[] args) {
-        getInput();
-        setMap();
-        showMap();
     }
 }
