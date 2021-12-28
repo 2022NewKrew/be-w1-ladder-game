2cd17e422e1900afcd2c2c1d80276e66dba960d7
jaden <jaden.dev@kakaocorp.com>
2021-12-28 21:45:01 +0900

    Step2 : 사다리게임 입력 및 출력 구현 (indent 단계 줄이기, else문 제거하기) - ladderElement 메서드 수정 - 아스키코드 사용을 하지 않기 떄문에 상수화한 사다리 구성요소들을 char형으로 바꿈 - 상수 변수들의 변수명을 조금더 가독성 좋게 변경 (LADDER -> LUMBER, CONNECTION -> BRIDGE) - column에 값에 따라 if 문을 만족하면 바로 return을 통해 메서드를 빠져나가 else문이 필요없도록 함 - 제거했던 멤버변수 people은 차 후 사용될 수도 있다고 판단해 다시 추가

--- src/LadderGame.java
+++ src/LadderGame.java
@@ -1,19 +1,21 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class LadderGame {
     int width;
     int height;
+    int people;
     List<StringBuilder> ladder = new ArrayList<>();
-    private final int LADDER = 124;
-    private final int CONNECTION = 45;
-    private final int SPACE = 32;
+    private final char LUMBER = '|';
+    private final char BRIDGE = '-';
+    private final char SPACE = ' ';
 
     LadderGame() {
         Scanner sc = new Scanner(System.in);
         System.out.println("참여할 사람은 몇 명인가요?");
-        int people = sc.nextInt();
+        people = sc.nextInt();
         width = 2 * people - 1;
         System.out.println("최대 사다리 높이는 몇 개인가요?");
         height = sc.nextInt();
@@ -31,22 +33,18 @@ public class LadderGame {
         }
     }
 
-    // else 문을 제거하기 위해선 if ~ 후 return하는 방법도 있지만, 문자 고유의 아스키코드가 있다는 점에 착안했습니다.
-    // 변화하는 c값(column값)에 따라 적절한 아스키코드값을 가질 수 있다면, if문을 아예 쓰지 않아도 되리라 판단하고 메서드를 작성했습니다.
+    // column 번호를 매개로 호출되어 적절한 ladder element를 리턴하는 메서드
     private char ladderElement(int column) {
-        // elemet의 값은 세로로 이어진 사다리 LADDER로 초기화
-        int element = LADDER;
-        // 0 또는 1의 값을 갖는 랜덤 숫자
-        int randomNumber = (int) (Math.random() * 2);
-        // 랜덤 숫자에 따라 CONNECTION 인지, SPACE 인지를 결정할 숫자
-        int isConnection = (CONNECTION - SPACE) * randomNumber;
-        // CONNECTION인지 SPACE인지 결정한 후, LADDER 값과의 차이를 보정
-        isConnection += LADDER - CONNECTION;
-        // 만약 column이 짝수라면 0, 홀수라면 isConnection 이 되는 값
-        int isLadder = (column % 2) * isConnection;
-        // 기존 element(=LADDER)에서 값을 빼주어 적절한 element ascii값을 얻음
-        element -= isLadder;
-        return (char) element;
+        // column 값이 짝수라면 사다리 세로 축에 해당하므로 LADDER를 바로 리턴
+        if (column % 2 == 0) {
+            return LUMBER;
+        }
+        // Bridge로 연결된 부위인지 아닌지를 결정할 랜덤 객체
+        Random random = new Random();
+        if (random.nextBoolean()) {
+            return BRIDGE;
+        }
+        return SPACE;
     }
 
     private StringBuilder singleRow() {
@@ -56,6 +54,4 @@ public class LadderGame {
         }
         return row;
     }
-
-
 }
