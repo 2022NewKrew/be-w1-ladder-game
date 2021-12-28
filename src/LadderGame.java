bc3ff534ed7ca6d85c68e35664a70b52eecec9bc
jaden <jaden.dev@kakaocorp.com>
2021-12-28 18:31:58 +0900

    Step2 : 사다리게임 입력 및 출력 구현 - git 충돌 해결을 위한 재커밋 - singleRow 메서드 private화

--- src/LadderGame.java
+++ src/LadderGame.java
@@ -1,12 +1,11 @@
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
 public class LadderGame {
     int people;
     int height;
-    List<StringBuilder> ladder = new ArrayList<>();
+    ArrayList<StringBuilder> ladder = new ArrayList<>();
 
     LadderGame() {
         Scanner sc = new Scanner(System.in);
@@ -16,25 +15,25 @@ public class LadderGame {
         height = sc.nextInt();
     }
 
+    // 아스키코드를 이용해서 사다리 element를 stringbuilder에 추가한다
+    // '|' : 124, ' ' : 32, '-' : 45
+    private StringBuilder singleRow() {
+        StringBuilder row = new StringBuilder();
+        for (int c=0; c<2*people-1; c++) {
+            int ascii = 124 - (c%2) * ((int)(Math.random()*2)*13 + 79);
+            row.append((char)ascii);
+        }
+        return row;
+    }
+
     public void makeLadder() {
-        for (int r = 0; r < height; r++) {
-            StringBuilder row = new StringBuilder();
-            Random random = new Random();
-            for (int c = 0; c < 2 * people - 1; c++) {
-                if (c % 2 == 0) {
-                    row.append('|');
-                } else if (random.nextBoolean()) {
-                    row.append('-');
-                } else {
-                    row.append(' ');
-                }
-            }
-            ladder.add(row);
+        for (int r=0; r<height; r++) {
+            ladder.add(singleRow());
         }
     }
 
     public void printLadder() {
-        for (int r = 0; r < height; r++) {
+        for (int r=0; r<height; r++) {
             System.out.println(ladder.get(r));
         }
     }
