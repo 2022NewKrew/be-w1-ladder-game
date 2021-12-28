ae4f11f72594d85e4e73983216116ed0308ef722
root.directory <root.directory@kakaocorp.com>
2021-12-28 17:20:00 +0900

    Update Ladder1
    - 리뷰 내용 반영
    - random.nextBoolean() 사용
    - scanner.nextInt() 사용
    - 불필요한 삼항 연산자 제거
    - toString() 오버라이딩으로 출력 간소
    - 메소드 외부 노출 최소화
    - 과한 주석 생략

--- week1/src/main/java/Ladder1.java
+++ week1/src/main/java/Ladder1.java
@@ -1,63 +1,61 @@
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Random;
 import java.util.Scanner;
 
 class Ladder {
 
   int col, row;
 
-  public void inputLadderSize() {
+  Ladder() {
+    inputLadderSize();
+  }
+
+  private void inputLadderSize() {
     Scanner scanner = new Scanner(System.in);
 
     System.out.println("참여할 사람은 몇 명인가요?");
-    String col = scanner.nextLine().trim();
+    final int col = scanner.nextInt();
 
     System.out.println("최대 사다리 높이는 몇 개인가요?");
-    String row = scanner.nextLine().trim();
+    final int row = scanner.nextInt();
 
     if (validateLadderSize(col, row)) {
-      this.col = Integer.parseInt(col);
-      this.row = Integer.parseInt(row);
+      this.col = col;
+      this.row = row;
     } else {
       System.out.println("양의 정수만 입력해주세요.");
       inputLadderSize();
     }
   }
 
-  private boolean validateLadderSize(String col, String row) {
-    int colNum, rowNum;
-
-    try {
-      colNum = Integer.parseInt(col);
-      rowNum = Integer.parseInt(row);
-    } catch (NumberFormatException e) {
-      return false;
-    }
-
-    return colNum > 0 & rowNum > 0 ? true : false;
+  private boolean validateLadderSize(int col, int row) {
+    return col > 0 & row > 0;
   }
 
-  public void drawLadder() {
-//    한줄 공백
-    System.out.println();
+  @Override
+  public String toString() {
+    final Random random = new Random();
+    StringBuilder stringBuilder = new StringBuilder();
 
-    for (int i=0; i < this.row; i++) {
-      for (int j=0; j < this.col - 1; j++) {
-        System.out.print(Math.random() > 0.5 ? "| " : "|-");
+//    시각적 구분을 위한 한줄 공백
+    stringBuilder.append("\n");
+
+    for (int i = 0; i < this.row; i++) {
+      for (int j = 0; j < this.col - 1; j++) {
+        stringBuilder.append(random.nextBoolean() ? "| " : "|-");
       }
-      System.out.println("|");
+      stringBuilder.append("|\n");
     }
+
+    return stringBuilder.toString();
   }
 }
 
 public class Ladder1 {
 
   public static void main(String[] args) {
-//    인스턴스 생성
     Ladder ladder = new Ladder();
-
-//    사용자 인풋
-    ladder.inputLadderSize();
-
-//    사다리 출력
-    ladder.drawLadder();
+    System.out.println(ladder);
   }
 }
