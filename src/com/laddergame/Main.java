773241123781cec94e70d1886d049d0732e3b3f8
pi.raspberry <pi.raspberry@kakaocorp.com>
2021-12-28 17:47:40 +0900

    Add Ladder class
    * Ladder 클래스에서 사다리 생성을 처리함.
    * Main 클래스에서 입력을 받아 사다리를 출력함.

--- src/com/laddergame/Main.java
+++ src/com/laddergame/Main.java
@@ -1,6 +1,5 @@
 package com.laddergame;
 
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -11,20 +10,7 @@ public class Main {
         System.out.println("최대 사다리 높이는 몇 개인가요?");
         int ladderNum = in.nextInt();
 
-        for(int i = 0; i < ladderNum; i++) {
-            System.out.println(createLadderLine(peopleNum));
-        }
-    }
-
-    private static String createLadderLine(int n) {
-        StringBuilder sb = new StringBuilder();
-        sb.append('|');
-        Random rand = new Random();
-        for(int i = 0; i < n-1; i++)
-            if(rand.nextBoolean())
-                sb.append(" |");
-            else
-                sb.append("-|");
-        return sb.toString();
+        Ladder ladder = new Ladder(peopleNum, ladderNum);
+        System.out.println(ladder);
     }
 }
