59a466abd0efe01c9c9a807b5e9b964f557883f6
edward.baek <edward.baek@kakaocorp.com>
2021-12-28 16:39:26 +0900

    refactor : RandomUtil 수정
    private 생성자를 만듬으로써 기본 생성자가 생성되는것을 방지
    0~특정범위 까지 생성해주는 메서드 작성

--- ladder-project/src/main/java/util/RandomUtil.java
+++ ladder-project/src/main/java/util/RandomUtil.java
@@ -2,8 +2,17 @@ package util;
 
 public class RandomUtil {
 
+    //기본 생성자가 만들어지는 것을 막는다(인스턴스화 방지용)
+    private RandomUtil() {
+        throw new AssertionError();
+    }
+
     public static int generate() {
         double randomNumber = Math.random();
-        return (int) (randomNumber*100000);
+        return (int) (randomNumber * 100000);
+    }
+
+    public static int generateSpecificRange(int range) {
+        return generate() % range;
     }
 }
