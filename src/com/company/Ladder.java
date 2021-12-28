3276ca2be3f2bebb5e65255ceeabf462d162ccef
worldbright <worldbright0@gmail.com>
2021-12-28 10:47:18 +0900

    step 2 : change Ladder info to String type from boolean type

--- src/com/company/Ladder.java
+++ src/com/company/Ladder.java
@@ -6,36 +6,34 @@ import java.util.Random;
 public class Ladder {
     private int numPeople;
     private int height;
-    private boolean[][] ladder;
+    private String[] ladder;
+    private Random random;
+    private static String[] pattern = {"- ", " "};
     public Ladder(int numPeople, int height) {
         this.numPeople = numPeople;
         this.height = height;
+        random = new Random();
         makeNewLadder();
     }
 
     private void makeNewLadder() {
-        Random random = new Random();
-        ladder = new boolean[height][numPeople-1];
-        for(int y = 0; y < height; y++) {
-            for(int x = 0; x < numPeople-1; x++) {
-                if(random.nextBoolean()) {
-                    ladder[y][x] = true;
-                    x++;
-                }
-            }
-        }
+        ladder = new String[height];
+        for(int y = 0; y < height; y++)
+            ladder[y] = makeNewLine(numPeople-1);
+    }
+
+    private String makeNewLine(int width) {
+        String ret = "";
+        while(ret.length() < width)
+            ret += pattern[random.nextInt(2)];
+        return ret.substring(0, width);
     }
 
     public void printLadder() {
-        for(int y = 0; y < height; y++) {
-            for(int x = 0; x < numPeople-1; x++) {
-                System.out.print("|");
-                if(ladder[y][x])
-                    System.out.print("-");
-                else
-                    System.out.print(" ");
-            }
-            System.out.println("|");
-        }
+        for(int y = 0; y < height; y++)
+            printLine(y);
+    }
+    public void printLine(int y) {
+        System.out.println("|" + String.join("|", ladder[y].split("")) + "|");
     }
 }
