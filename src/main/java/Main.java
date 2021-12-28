29e1eb284b377c60407e5fca229f6c4ec4b485f6
jindo.min <jindo.min@kakaocorp.com>
2021-12-28 19:25:34 +0900

    [refactor] 불필요한 try-catch문 없앰

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,16 +3,9 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        int numberOfPlayer = 0;
-        int height = 0;
+        int numberOfPlayer = Input.inputNumberOfPlayer();
+        int height = Input.inputHeight();
 
-        try {
-            numberOfPlayer = Input.inputNumberOfPlayer();
-            height = Input.inputHeight();
-        } catch (InputMismatchException e) {
-            System.err.println("0보다 큰 정수를 입력해주세요");
-            System.exit(1);
-        }
         LadderBuilder ladderBuilder = new LadderBuilder(numberOfPlayer, height);
         Ladder ladder = ladderBuilder.makeLadder();
         Output.printLadder(ladder.getLadder());
