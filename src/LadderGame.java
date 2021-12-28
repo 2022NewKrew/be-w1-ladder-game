5e37380f0a41b8d007f56faa46e892b4be1be13c
jaden <jaden.dev@kakaocorp.com>
2021-12-28 11:57:25 +0900

    사다리게임 step1 commit

--- src/LadderGame.java
+++ src/LadderGame.java
@@ -1,36 +1,42 @@
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class LadderGame {
-    public static void main(String[] args) {
-        int people;
-        int height;
+    int people;
+    int height;
+    ArrayList<StringBuilder> ladder = new ArrayList<>();
 
+    LadderGame() {
         Scanner sc = new Scanner(System.in);
         System.out.println("참여할 사람은 몇 명인가요?");
         people = sc.nextInt();
         System.out.println("최대 사다리 높이는 몇 개인가요?");
         height = sc.nextInt();
+    }
 
-        char[][] ladder = new char[height][2*people-1];
+    public void makeLadder() {
         for (int r=0; r<height; r++) {
+            StringBuilder row = new StringBuilder();
+            Random random = new Random();
             for (int c=0; c<2*people-1; c++) {
-                if (c % 2 == 0) {
-                    ladder[r][c] = '|';
+                if (c%2 == 0) {
+                    row.append('|');
                 }
-                else if (Math.random() < 0.5) {
-                    ladder[r][c] = '-';
+                else if (random.nextBoolean()) {
+                    row.append('-');
                 }
                 else {
-                    ladder[r][c] = ' ';
+                    row.append(' ');
                 }
             }
+            ladder.add(row);
         }
+    }
 
-
-
-//        System.out.println(people);
-//        System.out.println(height);
-
-
+    public void printLadder() {
+        for (int r=0; r<height; r++) {
+            System.out.println(ladder.get(r));
+        }
     }
 }
---
+++ src/StartGame.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class StartGame {
+    public static void main(String[] args) {
+        LadderGame ladderGame = new LadderGame();
+        ladderGame.makeLadder();
+        ladderGame.printLadder();
+    }
+}
