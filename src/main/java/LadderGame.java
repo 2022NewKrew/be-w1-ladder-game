359b50e7e27a0ddec2f08478aee06fb6a97970bf
chance.kk <chance.kk@kakaocorp.com>
2021-12-28 12:54:28 +0900

    Set Git Ignore

--- src/main/java/LadderGame.java
+++ src/main/java/LadderGame.java
@@ -1,6 +1,6 @@
 import domain.Ladder;
-import view.LadderRenderer;
 
+import view.LadderRenderer;
 public class LadderGame {
     public static void main(String[] args) {
         Ladder ladder = new Ladder();
