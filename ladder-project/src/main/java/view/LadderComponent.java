adfb4c19b08870e21f077963dd29b873f34363e2
edward.baek <edward.baek@kakaocorp.com>
2021-12-29 13:56:50 +0900

    refactor : view 패키지로 이동

---
+++ ladder-project/src/main/java/view/LadderComponent.java
@@ -0,0 +1,17 @@
+package view;
+
+public enum LadderComponent {
+    LADDER_ROW_LINE("|"),
+    LADDER_EMPTY_COLUMN("     "),
+    LADDER_COLUMN_LINE("-----");
+
+    private final String value;
+
+    LadderComponent(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
