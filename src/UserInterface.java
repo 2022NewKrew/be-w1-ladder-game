4237d87a384370fb3db32d20718f680a4f238baf
champ.ion <champ.ion@kakaocorp.com>
2021-12-29 22:14:41 +0900

    Ladder Game STEP3 - Feedback(PR#355) - [Cleanup] Cleaned-up about constant variable - [Refactoring] Added Exceptions for input user names(String) and height   of ladder(Integer)

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,23 +1,53 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class UserInterface {
     private static final int MAX_NUM_STRING = 5;
+    private static final String SEPARATOR = ",";
 
     Scanner scanner = new Scanner(System.in);
 
     public List<String> inputUserInfo() {
-        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
-        String[] users = scanner.nextLine().split(",");
+        try {
+            System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+            String[] users = scanner.nextLine().split(SEPARATOR);
 
-        return Arrays.asList(users);
+            checkUserList(users);
+
+            return Arrays.asList(users);
+        } catch (IllegalArgumentException iae) {
+            System.out.println("이름은 " + MAX_NUM_STRING + "글자를 넘어갈 수 없습니다.");
+            return inputUserInfo();
+        }
+    }
+
+    private void checkUserList(String[] users) throws IllegalArgumentException {
+        for (String user : users) {
+            checkUserName(user);
+        }
+    }
+
+    private void checkUserName(String user) {
+        if (user.length() > MAX_NUM_STRING) throw new IllegalArgumentException();
     }
 
     public int inputLadderHeight() {
-        System.out.println("최대 사다리 높이는 몇 개인가요?");
-        return scanner.nextInt();
+        try {
+            System.out.println("최대 사다리 높이는 몇 개인가요?");
+            return inputPositiveInt();
+        } catch (IllegalArgumentException iae) {
+            System.out.println("사다리 높이는 양의 정수여야 합니다.");
+            return inputLadderHeight();
+        }
+    }
+
+    private int inputPositiveInt() {
+        int value = scanner.nextInt();
+        if (value <= 0) {
+            throw new IllegalArgumentException();
+        }
+        return value;
     }
 
     public void printLadderGame(List<String> userList, Ladder ladder) {
