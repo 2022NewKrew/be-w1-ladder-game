5a85d33343444ceb8b10ded4a5b58152362d7db0
jiyolla <jiyolla0919@gmail.com>
2021-12-29 10:47:47 +0900

    [frank] 1단계 리뷰 반영 구현 (#272)
    * Remove and untrack the .idea folder
    
    This is a suggestion from #137.
    
    * Improve generation performance
    
    Generation method now use a static Random object instead of Instantiating a
    new Random object each time it is called.
    
    * Remove unnecessary comment
    
    Those comment is now implemented as code and the code is quite straightforward.
    
    * Rename the ladder state variable with a more meaningful name
    
    * Rewrite toString method
    
    The method given the name toString has a common practice to respect, and it was
    not in the old code.
    
    The method to return the ladder's shape as string is given a new name, and the
    toString method is rewritten to do what it is supposed to do.
    
    * Add various code improvement
    
    1. Ladder state is declared as List instead of ArrayList for better flexibility
    2. Unnecessary "this." is removed
    3. width, height is made final
    
    Co-authored-by: frank.burger <frank.burger@kakaocorp.com>

--- src/Ladder.java
+++ src/Ladder.java
@@ -1,35 +1,35 @@
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 
 public class Ladder {
-    // 4. Fields to save ladder state
-    private int width, height;
-    private ArrayList<String> stringRepr;
+    private static final Random random = new Random();
+    private List<String> stateAsListOfString;
+    private final int width, height;
 
-
-    // 1. Constructor to initialize ladder parameters(width(number of players), height)
     public Ladder(int width, int height) {
         this.width = width;
         this.height = height;
     }
 
-
-    // 2. Method to (re)generate ladder
     public void generateSimpleRandom() {
-        Random rand = new Random();
-
-        this.stringRepr = new ArrayList<>();
-        for (int lineNo = 0; lineNo < this.height; lineNo++) {
-            this.stringRepr.add(Stream.generate(() -> rand.nextBoolean() ? "-" : " ").limit(this.width - 1).collect(Collectors.joining("|", "|", "|")));
+        stateAsListOfString = new ArrayList<>();
+        for (int lineNo = 0; lineNo < height; lineNo++) {
+            stateAsListOfString.add(Stream.generate(() -> random.nextBoolean() ? "-" : " ").limit(width - 1).collect(Collectors.joining("|", "|", "|")));
         }
     }
 
+    public String toPrettyString() {
+        return String.join(System.lineSeparator(), stateAsListOfString);
+    }
 
-    // 3. toString() method
+    @Override
     public String toString() {
-        return String.join("\n", this.stringRepr);
+        return getClass().getName() + "("
+                + "width=" + width + ", "
+                + "height=" + height + ")";
     }
 }
--- src/LadderGame.java
+++ src/LadderGame.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 
 public class LadderGame {
     public static void main(String[] args) {
-        // 1. Prompt and read input(number of players, ladder height)
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("참여할 사람은 몇 명인가요?");
@@ -12,13 +11,9 @@ public class LadderGame {
         System.out.println("최대 사다리 높이는 몇 개인가요?");
         int heightOfLadder = scanner.nextInt();
 
-
-        // 2. Generate ladder
         Ladder ladder = new Ladder(numOfPlayers, heightOfLadder);
         ladder.generateSimpleRandom();
 
-
-        // 3. Show result
-        System.out.println("\n" + ladder);
+        System.out.println(System.lineSeparator() + ladder.toPrettyString());
     }
 }
