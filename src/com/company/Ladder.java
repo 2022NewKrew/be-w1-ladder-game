2ea45dac6e2c3d248f30d4fe78895940c52828a5
worldbright <worldbright0@gmail.com>
2021-12-29 13:24:10 +0900

    fix reviewed issues
    1. make Main.java ends with newLine
    2. change constants naming convention to UpperCase, underscore
    3. remove unnecessary StringBuilder
    4. detach name padding logic to method
    5. add ladderView String value (최초 1번 사다리 view 구성)
    6. change ArrayList -> List (concrete class 대신 interface 사용)
    )

--- src/com/company/Ladder.java
+++ src/com/company/Ladder.java
@@ -7,57 +7,65 @@ import java.util.Random;
 import java.util.stream.Collectors;
 
 public class Ladder {
-    private final int numPeople;
-    private final int height;
-    private ArrayList<String> names;
-    private ArrayList<String> ladder;
+    private final int NUM_PEOPLE;
+    private final int HEIGHT;
+    private List<String> names;
+    private List<String> ladderInfo;
+    private List<String> ladderView;
     private final Random random;
-    private static final String[] pattern = {"- ", " "};
+    private static final String[] PATTERN = {"- ", " "};
 
     public Ladder(String[] names, int height) {
-        this.names = new ArrayList<String>(Arrays.asList(names));
-        this.ladder = new ArrayList<String>();
-        this.numPeople = names.length;
-        this.height = height;
+        this.names = Arrays.asList(names);
+        this.ladderInfo = new ArrayList<String>();
+        this.ladderView = new ArrayList<String>();
+        this.NUM_PEOPLE = names.length;
+        this.HEIGHT = height;
         random = new Random();
         makeNewLadder();
     }
 
     private void makeNewLadder() {
-        for(int y = 0; y < height; y++) {
-            ladder.add(makeNewLine(numPeople - 1));
+        for(int y = 0; y < HEIGHT; y++) {
+            String newLine = makeNewLine(NUM_PEOPLE - 1);
+            ladderInfo.add(newLine);
+            ladderView.add("  |"
+                    + Arrays.stream(newLine.split(""))
+                    .map(s->s.repeat(5))
+                    .collect(Collectors.joining("|"))
+                    + "|");
         }
     }
 
     private String makeNewLine(int width) {
         StringBuilder ret = new StringBuilder();
         while(ret.length() < width) {
-            ret.append(pattern[random.nextInt(2)]);
+            ret.append(PATTERN[random.nextInt(2)]);
         }
         return ret.substring(0, width);
     }
 
     public void printLadder() {
         printNames();
-        for(int y = 0; y < height; y++) {
+        for(int y = 0; y < HEIGHT; y++) {
             printLine(y);
         }
     }
 
     private void printNames() {
-        for(int i = 0; i < numPeople; i++) {
-            int frontSpace = (5 - names.get(i).length()) / 2;
-            int backSpace = 5 - frontSpace - names.get(i).length() + 1;
-            System.out.print(new StringBuilder(" ".repeat(frontSpace)) + names.get(i) + " ".repeat(backSpace));
+        for(int i = 0; i < NUM_PEOPLE; i++) {
+            System.out.print(getPaddedName(names.get(i), 5) + " ");
         }
         System.out.println();
     }
 
+    private String getPaddedName(String name, int size) {
+        int frontSpace = (size - name.length()) / 2;
+        int backSpace = size - frontSpace - name.length();
+        return " ".repeat(frontSpace) + name + " ".repeat(backSpace);
+    }
+
     private void printLine(int y) {
-        System.out.println(new StringBuilder("  |")
-                                + Arrays.stream(ladder.get(y).split(""))
-                                        .map(s->s.repeat(5))
-                                        .collect(Collectors.joining("|"))
-                                + "|");
+        System.out.println(ladderView.get(y));
     }
 }
