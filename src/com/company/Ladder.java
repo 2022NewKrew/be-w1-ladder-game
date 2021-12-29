ed0adcabba5674e2598a30a72509ce3013913066
worldbright <worldbright0@gmail.com>
2021-12-29 12:13:48 +0900

    Step3: added naming feature

--- src/com/company/Ladder.java
+++ src/com/company/Ladder.java
@@ -1,16 +1,20 @@
 package com.company;
 
+import java.util.Arrays;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 public class Ladder {
     private final int numPeople;
     private final int height;
+    private String[] names;
     private String[] ladder;
     private final Random random;
     private static final String[] pattern = {"- ", " "};
 
-    public Ladder(int numPeople, int height) {
-        this.numPeople = numPeople;
+    public Ladder(String[] names, int height) {
+        this.names = names;
+        this.numPeople = names.length;
         this.height = height;
         random = new Random();
         makeNewLadder();
@@ -18,26 +22,40 @@ public class Ladder {
 
     private void makeNewLadder() {
         ladder = new String[height];
-        for (int y = 0; y < height; y++) {
+        for(int y = 0; y < height; y++) {
             ladder[y] = makeNewLine(numPeople - 1);
         }
     }
 
     private String makeNewLine(int width) {
         StringBuilder ret = new StringBuilder();
-        while (ret.length() < width) {
+        while(ret.length() < width) {
             ret.append(pattern[random.nextInt(2)]);
         }
         return ret.substring(0, width);
     }
 
     public void printLadder() {
-        for (int y = 0; y < height; y++) {
+        printNames();
+        for(int y = 0; y < height; y++) {
             printLine(y);
         }
     }
 
-    public void printLine(int y) {
-        System.out.println("|" + String.join("|", ladder[y].split("")) + "|");
+    private void printNames() {
+        for(int i = 0; i < numPeople; i++) {
+            int frontSpace = (5 - names[i].length()) / 2;
+            int backSpace = 5 - frontSpace - names[i].length() + 1;
+            System.out.print(new StringBuilder(" ".repeat(frontSpace)) + names[i] + " ".repeat(backSpace));
+        }
+        System.out.println();
+    }
+
+    private void printLine(int y) {
+        System.out.println(new StringBuilder("  |")
+                                + Arrays.stream(ladder[y].split(""))
+                                        .map(s->s.repeat(5))
+                                        .collect(Collectors.joining("|"))
+                                + "|");
     }
 }
