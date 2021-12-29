0a3ea69dd2e755d6f01f4be35e69a8b5d1914f31
Hongjeongmin <wjdals0471@naver.com>
2021-12-29 16:32:06 +0900

    하나의 층에대한 String을 생성하는 메소드 구현 / 향상된 for 사용

--- src/main/java/LadderSimpleGame.java
+++ src/main/java/LadderSimpleGame.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class LadderSimpleGame extends LadderGame {
     private boolean[][] bridge;
     private final LadderGameService ladderGameService;
@@ -15,20 +17,30 @@ public class LadderSimpleGame extends LadderGame {
     public String getLadderString() throws Exception {
         StringBuilder stringBuilder = new StringBuilder();
 
-        for (int i = 0; i < this.getLadderHeight(); i++) {
-            for (int j = 0; j < this.getNumberOfParticipants() * 2 - 1; j++) {
-                stringBuilder.append(getBridgeChar(i, j));
-            }
-            stringBuilder.append("\n");
+        for (boolean[] floor : this.bridge) {
+            stringBuilder.append(getFloorString(floor));
         }
 
         return stringBuilder.toString();
     }
 
-    private String getBridgeChar(int row, int col) {
+    private String getFloorString(boolean[] floor) {
+
+        System.out.println(Arrays.toString(floor));
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int col = 0; col < this.getNumberOfParticipants() * 2 - 1; col++) {
+            stringBuilder.append(getBridgeChar(col, floor));
+        }
+
+        stringBuilder.append("\n");
+        return stringBuilder.toString();
+    }
+
+    private String getBridgeChar(int col, boolean[] floor) {
         if (col % 2 == 0) {
             return "|";
-        } else if (this.bridge[row][col / 2]) {
+        } else if (floor[col / 2]) {
             return "-";
         }
 
