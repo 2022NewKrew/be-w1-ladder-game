f9259b9f2f9ff05dbde7e3dab4e3d88cae1f8801
yuchanleeme <yuchanlee.me@gmail.com>
2021-12-29 17:17:59 +0900

    feat: Line 클래스에서 구성요소 관리하게 변경
    - chkLadder은 사다리 좌표 기준으로 양 옆에 이어지는 사다리가 있는지 체크하는 Boolean 리스트
    - ladderComponents는 사다리 구성요소(모양)이 있는 String 리스트

--- src/LadderGame/Ladder.java
+++ src/LadderGame/Ladder.java
@@ -1,48 +1,36 @@
 package LadderGame;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Ladder {
-    private final static String VERTICAL_LINE = "|";
-    private final static String HORIZONTAL_LINE = "-----";
-    private final static String BLANK = "     ";
-
     private final int ladderWidth, ladderHeight;
-    private final Random random;
+    private final Random random = new Random();
 
-    private final ArrayList<Line> hasRightLadder;
+    private final List<Line> ladder;
 
     public Ladder(int ladderWidth, int ladderHeight) {
         this.ladderWidth = ladderWidth;
         this.ladderHeight = ladderHeight;
-        this.random = new Random();
-        this.hasRightLadder = makeHasRightLadder();
+        this.ladder = makeLadder();
     }
 
-    private ArrayList<Line> makeHasRightLadder() {
-        ArrayList<Line> hasRightLadder = new ArrayList<>();
-        for (int ladderRow = 0; ladderRow < ladderHeight; ladderRow++) {
-            Line hasRightLadderRow = new Line(ladderWidth, random);
-            hasRightLadder.add(hasRightLadderRow);
+    private List<Line> makeLadder() {
+        List<Line> ladder = new ArrayList<>();
+        for (int row = 0; row < ladderHeight; row++) {
+            Line ladderRow = new Line(ladderWidth, random);
+            ladder.add(ladderRow);
         }
-        return hasRightLadder;
+        return ladder;
     }
 
     public void printLadder() {
-        hasRightLadder.forEach((this::printLadderRow));
+        ladder.forEach((this::printLadderRow));
     }
 
     private void printLadderRow(Line ladderRow){
-        ladderRow.points.forEach(ladderCol -> {
-            System.out.print(VERTICAL_LINE);
-            System.out.print(printLocatedLadder(ladderCol));
-        });
-        System.out.println(VERTICAL_LINE);
-    }
-
-    private String printLocatedLadder(Boolean chkLadder){
-        if(chkLadder) return HORIZONTAL_LINE;
-        return BLANK;
+        ladderRow.ladderComponents.forEach(System.out::print);
+        System.out.println();
     }
 }
--- src/LadderGame/Line.java
+++ src/LadderGame/Line.java
@@ -1,30 +1,49 @@
 package LadderGame;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Line {
+    private final static String VERTICAL_LINE = "|";
+    private final static String HORIZONTAL_LINE = "-----";
+    private final static String BLANK = "     ";
+
     private final int ladderWidth;
     private final Random random;
-    public ArrayList<Boolean> points;
+    public List<Boolean> chkLadder;
+    public List<String> ladderComponents;
 
     public Line(int ladderWidth, Random randomInstance) {
         this.ladderWidth = ladderWidth;
         this.random = randomInstance;
-        makeHasRightLadderLine();
+        makeLadder();
     }
 
-    private void makeHasRightLadderLine(){
-        points = new ArrayList<>();
-        for (int ladderCol = 0; ladderCol < ladderWidth-1; ladderCol++){
-            points.add(makeValue(ladderCol));
+    private void makeLadder(){
+        chkLadder = new ArrayList<>();
+        ladderComponents = new ArrayList<>();
+
+        for (int col = 0; col < ladderWidth; col++){
+            boolean hasLadder = chkLadderCol(col);
+            chkLadder.add(hasLadder);
+            makeLadderComponents(hasLadder);
         }
     }
 
-    private Boolean makeValue(int col){
-        if(col != 0 && points.get(col-1)){
+    private Boolean chkLadderCol(int col){
+        if(col == ladderWidth-1 || (col != 0 && chkLadder.get(col-1))){
             return false;
         }
         return random.nextBoolean();
+    }
+
+    private void makeLadderComponents(boolean hasLadder){
+        ladderComponents.add(VERTICAL_LINE);
+        if(hasLadder){
+            ladderComponents.add(HORIZONTAL_LINE);
+        }else{
+            ladderComponents.add(BLANK);
+        }
     }
 }
