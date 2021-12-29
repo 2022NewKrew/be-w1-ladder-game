2f6b410b7bf053769b3cc1b7f2486446fcdc9571
GoonLee <auteur.kor@gmail.com>
2021-12-29 20:13:24 +0900

    Remove unused method

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -14,21 +14,6 @@ public class InputView {
         scanner = new Scanner(System.in);
     }
 
-    public int getPeopleSize() {
-        int peopleSize = 0;
-
-        System.out.println("참여할 사람은 몇 명 인가요?");
-        try{
-            peopleSize = scanner.nextInt();
-            validatePositiveInteger(peopleSize);
-        } catch (NoSuchElementException | IllegalStateException exception) {
-            System.err.println("입력을 처리할 수 없습니다.");
-            System.exit(1);
-        }
-
-        return peopleSize;
-    }
-
     public int getHeight() {
         int height = 0;
 
