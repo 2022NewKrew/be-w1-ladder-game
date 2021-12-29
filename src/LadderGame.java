8b673a59270f907396f54915091c32fe9422e46a
eastclo <goldeastcloud@gmail.com>
2021-12-29 11:47:27 +0900

    3단계 구현 - Main입력시 참여자 이름을 받게끔 수정, LadderGame 생성자 변경, display시 참여자 정보 출력

--- src/LadderGame.java
+++ src/LadderGame.java
@@ -1,14 +1,26 @@
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class LadderGame {
 
+    private final List<String> participants;
     private final Ladder ladder;
     private final DisplayLadder renderer;
 
-    LadderGame(int numberOfParticipants, int height) {
-        ladder = new Ladder(numberOfParticipants, height);
+    LadderGame(List<String> participants, int height) {
+        this.participants = List.copyOf(participants);
+        ladder = new Ladder(participants.size(), height);
         renderer = new StandardOutDisplayLadder();
     }
 
     public void displayLadder() {
+        //TODO:바꾸기
+        StringBuilder sb = new StringBuilder();
+        for(String participant : participants) {
+            sb.append(participant);
+            IntStream.range(0, 6-participant.length()).forEach(e -> sb.append(' '));
+        }
+        System.out.println(sb);
         renderer.display(ladder);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,20 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //TODO:입력부 변경
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("참여할 사람은 몇 명인가요?");
-        int numberOfParticipants = scan.nextInt();
+        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요");
+        String[] participants = scan.nextLine().split(",");
 
         System.out.println("최대 사다리 높이는 몇 개 인가요?");
         int ladderHeight = scan.nextInt();
 
-        LadderGame game = new LadderGame(numberOfParticipants, ladderHeight);
+        LadderGame game = new LadderGame(new ArrayList<>(Arrays.asList(participants)), ladderHeight);
         game.displayLadder();
     }
 }
