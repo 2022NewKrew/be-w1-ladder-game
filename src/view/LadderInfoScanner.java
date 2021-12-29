f08a01af620c1b74ad0c7e2cff0fa16c10d77b4f
jayden-bin <jayden.bin@kakaocorp.com>
2021-12-29 17:05:49 +0900

    Review 받은 내용 수정 - delimiter가 상수임을 표시하기 위해 상수명을 대문자로 치환 - 입력 안내 문구에서 사용되는 delimiter 문구에서도 DELIMITER 상수를 사용하도록 수정

--- src/view/LadderInfoScanner.java
+++ src/view/LadderInfoScanner.java
@@ -5,17 +5,18 @@ import data.LadderInfo;
 import java.util.Scanner;
 
 public class LadderInfoScanner {
+    private static final String DELIMITER = ",";
+    private static final String DELIMITER_NAME = "쉼표";
     private static final Scanner scanner = new Scanner(System.in);
-    private static final String delimiter = ",";
 
     private LadderInfoScanner() {
         //instance 생성 제한용 생성자
     }
 
     public static LadderInfo getLadderInfo() {
-        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+        System.out.printf("참여할 사람 이름을 입력하세요. (이름은 %s(%s)로 구분하세요)\n",DELIMITER_NAME,DELIMITER);
         String inputString = scanner.nextLine();
-        String[] participants = inputString.split(delimiter);
+        String[] participants = inputString.split(DELIMITER);
 
         System.out.println("최대 사다리 높이는 몇 개인가요?");
         int height = scanner.nextInt();
