ba627d986d83c577956b39abcc34be1f5a3094ef
myles.nah <myles.nah@kakaocorp.com>
2021-12-29 16:21:09 +0900

    feat: 사다리 게임 3단계 피드백 반영 및 gradle 프로젝트로 변경

---
+++ src/main/java/com/kakao/step3_2/domain/LadderGame.java
@@ -0,0 +1,37 @@
+package com.kakao.step3_2.domain;
+
+import com.kakao.step3_2.view.LadderView;
+
+import java.util.Scanner;
+
+public class LadderGame {
+    private static final Scanner SCAN = new Scanner(System.in);
+
+    private Names names;
+    private int countOfLines;
+
+    public LadderGame() {
+        try {
+            getPeopleNames();
+            getCountOfLines();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        new LadderView(new Ladder(names, countOfLines));
+    }
+
+    private void getPeopleNames() throws Exception {
+        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요. 각 이름은 다섯 글자까지만 사용됩니다.)");
+        this.names = new Names(SCAN.nextLine());
+    }
+
+    private void getCountOfLines() throws Exception {
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        countOfLines = SCAN.nextInt();
+
+        if (countOfLines <= 0) {
+            throw new Exception("올바르지 않은 사다리 높이가 입력되었습니다.");
+        }
+    }
+}
---
+++ src/main/java/com/kakao/step3_2/domain/Line.java
@@ -0,0 +1,43 @@
+package com.kakao.step3_2.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Line {
+    private static final String LADDER_VERT = "|";
+    private static final String LADDER_HORI = "-----";
+    private static final String LADDER_EMP = "     ";
+    private static final Random RAND = new Random();
+
+    private final List<Boolean> points;
+
+    public Line(int countOfPeople) {
+        points = new ArrayList<>();
+
+        for (int i = 0; i < countOfPeople - 1; i++) {
+            points.add(RAND.nextBoolean() && (i == 0 || !points.get(i - 1)));
+        }
+    }
+
+    private String convPointToString(int i) {
+        if (i % 2 == 0) return LADDER_VERT;
+
+        if (points.get((i - 1) / 2)) return LADDER_HORI;
+
+        return LADDER_EMP;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder(LADDER_EMP);
+
+        for (int i = 0, size = points.size(); i < 2 * size + 1; i++) {
+            sb.append(convPointToString(i));
+        }
+
+        sb.append("\n");
+
+        return sb.toString();
+    }
+}
