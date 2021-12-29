3ea1d07cf0318bd09505e5fec81175178206d4a7
jake.0208 <jake.0208@kakaocorp.com>
2021-12-29 13:35:24 +0900

    refactor: InputManager클래스 수정
    * 클래스 내 모든 메소드 static으로 변경 => 그에 따라 메인에서 인스턴스만들지 않고 사용가능
    * getter와 헷갈릴 수 있는 변수명 변경

--- src/main/java/InputManager.java
+++ src/main/java/InputManager.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class InputManager {
     private static final Scanner sc = new Scanner(System.in);
 
-    public StartInfo getStartInfo(){
+    public static StartInfo makeStartInfo(){
         System.out.println("참여할 사람은 몇 명인가요?");
-        int numPeople = getInputNumber();
+        int numPeople = makeInputNumber();
 
         System.out.println("최대 사다리 높이는 몇 개인가요?");
-        int maxHeight = getInputNumber();
+        int maxHeight = makeInputNumber();
 
         return new StartInfo(numPeople, maxHeight);
     }
-    private int getInputNumber(){
+    private static int makeInputNumber(){
         while(true){
             try{
                 int value = Integer.parseInt(sc.nextLine());
@@ -24,7 +24,7 @@ public class InputManager {
             }
         }
     }
-    private int checkIntValue(int value){
+    private static int checkIntValue(int value){
         if(value <=0){
             throw new NumberFormatException();
         }
