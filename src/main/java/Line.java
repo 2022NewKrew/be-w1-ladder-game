4066279fbc96a27b73fee0b841a9458276c4a882
jake.0208 <jake.0208@kakaocorp.com>
2021-12-29 17:55:27 +0900

    feat: ladder 생성 시 중복된 사다리 안나오게함

--- src/main/java/Line.java
+++ src/main/java/Line.java
@@ -7,11 +7,25 @@ public class Line {
 
     public Line(int numPeople){
         for (int i = 0; i < numPeople; i++) {
-            points.add(rand.nextBoolean());
+            addPoints(i);
         }
     }
 
     public ArrayList<Boolean> getPoints() {
         return points;
+    }
+    private void addPoints(int i){
+        if(isNextLadder(i)){
+            points.add(false);
+            return;
+        }
+        points.add(rand.nextBoolean());
+    }
+
+    private boolean isNextLadder(int i){
+        if(i==0){
+            return false;
+        }
+        return points.get(i-1);
     }
 }
