7ab191786f3ad820d1fe7b42617a72db3adca1b7
Mino <50402452+homin025@users.noreply.github.com>
2021-12-29 10:47:17 +0900

    [ladder-game] implementation step-2 (#233)
    Co-authored-by: mino.lee <mino.lee@kakaocorp.com>

---
+++ src/main/java/step_2/Ladder.java
@@ -0,0 +1,9 @@
+package step_2;
+
+public class Ladder {
+    public StringBuilder shape;
+
+    public Ladder(StringBuilder shape) {
+        this.shape = shape;
+    }
+}
---
+++ src/main/java/step_2/LadderBuilder.java
@@ -0,0 +1,37 @@
+package step_2;
+
+import java.util.Random;
+import java.util.stream.IntStream;
+
+public class LadderBuilder {
+    private final Random random = new Random();
+
+    private final StringBuilder ladder;
+
+    public LadderBuilder() {
+        ladder = new StringBuilder();
+    }
+
+    public Ladder buildLadder(int ladderWidth, int ladderHeight) {
+        IntStream.range(0, ladderHeight)
+                .forEach(row -> buildLineOfLadder(ladderWidth));
+        return new Ladder(ladder);
+    }
+
+    private void buildLineOfLadder(int ladderWidth) {
+        ladder.append(LadderCharacter.COLOMN);
+        IntStream.range(0, ladderWidth)
+                .forEach(col -> ladder.append(getLadderConnection()));
+        ladder.append(LadderCharacter.CHANGE_LINE);
+    }
+
+    private String getLadderConnection() {
+        if (getLadderConnectionCondition())
+            return LadderCharacter.CONNECTED;
+        return LadderCharacter.NOT_CONNECTED;
+    }
+
+    private boolean getLadderConnectionCondition() {
+        return random.nextBoolean();
+    }
+}
