52f036902a18de6b7a6cf018e33dbb5e43f587fb
yuchanleeme <yuchanlee.me@gmail.com>
2021-12-29 18:07:11 +0900

    feat: 유효성 검사 추가
    - 참가자 이름 최대 5글자로 제한

--- src/LadderGame/LadderGame.java
+++ src/LadderGame/LadderGame.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class LadderGame {
+    private static final int MAX_LENGTH_OF_NAME = 5;
     private List<String> playerNames;
     private int ladderHeight, playerNum;
 
@@ -21,7 +22,17 @@ public class LadderGame {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉽표(,)로 구분하세요)");
         String allPlayerNames = inputScanner.nextLine();
         playerNames = Arrays.asList(allPlayerNames.split(","));
+        validatePlayerNames();
+
         playerNum = playerNames.size();
+    }
+
+    private void validatePlayerNames(){
+        playerNames.forEach(name -> {
+            if (name.length() > MAX_LENGTH_OF_NAME) {
+                throw new IllegalArgumentException(MAX_LENGTH_OF_NAME + "글자 이상의 이름이 포함되어 있습니다.");
+            }
+        });
     }
 
     private void inputLadderHeight(Scanner inputScanner){
