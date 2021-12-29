7f77c3fedefcf0ca60d3d654c7084e6aaada9d90
jindo.min <jindo.min@kakaocorp.com>
2021-12-29 13:25:19 +0900

    [refactor] Stream 쓰도록 변경

--- src/main/java/Output.java
+++ src/main/java/Output.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Output {
     static final String SIDE_RAIL = "|";
@@ -7,6 +9,7 @@ public class Output {
 
     /**
      * 사다리의 한 줄을 출력하는 메서드
+     *
      * @param line 출력할 한 줄
      */
     static public void printLine(ArrayList<Boolean> line) {
@@ -20,6 +23,7 @@ public class Output {
 
     /**
      * 사다리를 출력하는 메서드
+     *
      * @param lines 출력할 줄 리스트
      */
     static void printLadder(ArrayList<Line> lines) {
@@ -31,29 +35,33 @@ public class Output {
 
     /**
      * 이름 사이의 공백을 출력하는 메서드
+     *
      * @param beforeName 공백 이전 이름
-     * @param afterName 공백 이후 이름
+     * @param afterName  공백 이후 이름
      */
     static void printSpaceBetweenNames(String beforeName, String afterName) {
         int beforeNameLength = beforeName.length() == 0 ? 0 : (beforeName.length() + 1) / 2 - 1;
         int afterNameLength = afterName.length() / 2;
         int spaceCount = SPACE.length() - beforeNameLength - afterNameLength;
 
-        for (int i = 0;  i < spaceCount; i++)
-            System.out.print(" ");
+        Stream.generate(() -> " ")
+                .limit(spaceCount)
+                .forEach(System.out::print);
     }
 
     /**
      * 사다리타기의 맨 윗줄 이름을 출력하는 메서드
+     *
      * @param names 출력할 이름 리스트
      */
     static void printName(ArrayList<String> names) {
         printSpaceBetweenNames("", names.get(0));
         System.out.printf("%s", names.get(0));
-        for (int i = 1; i < names.size(); i++) {
-            printSpaceBetweenNames(names.get(i - 1), names.get(i));
-            System.out.printf("%s", names.get(i));
-        }
+        IntStream.range(1, names.size())
+                .forEachOrdered(i -> {
+                    printSpaceBetweenNames(names.get(i - 1), names.get(i));
+                    System.out.printf("%s", names.get(i));
+                });
         System.out.println();
     }
 }
