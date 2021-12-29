090d2d34cdb601614d12a6abe9be6e7d10905984
pride.chicken <pride.chicken@kakaocorp.com>
2021-12-29 18:45:36 +0900

    [refactor] Line 클래스 분리에 의해 변경
    - 기존과 다르게 Line 클래스를 따로 생성했기 때문에 이에 맞춰 변경함

--- src/Ladder.java
+++ src/Ladder.java
@@ -5,21 +5,21 @@ public class Ladder {
     // 인스턴스 변수
     private final int column;
     private final int row;
-    private final List<List<Character>> ladder;
+    private final List<Line> ladder;
 
     // 생성자
-    public Ladder(int[] inputNumber) {
-        this.column = inputNumber[1];
-        this.row = 2 * inputNumber[0] - 1;
+    public Ladder(int row, int column) {
+        this.column = column;
+        this.row = 2 * row - 1;
         this.ladder = getLadder();
     }
 
     // 메소드
-    private List<List<Character>> getLadder() {
-        List<List<Character>> createdLadder = new ArrayList<>();
+    private List<Line> getLadder() {
+        List<Line> createdLadder = new ArrayList<>();
 
         for (int i = 0; i < column; i++) {
-            List<Character> line = IntStream.range(0, row).mapToObj(this::getLadderElement).collect(Collectors.toList());
+            Line line = new Line(row);
             createdLadder.add(line);
         }
         return createdLadder;
