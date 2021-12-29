59cac92003c8b11f89e75eb88b96c2c715607d20
jh Lim <70519147+jhwlim@users.noreply.github.com>
2021-12-29 22:07:20 +0900

    사다리 게임 2단계 피드백 반영 및 3단계 구현 (#346)
    * docs : 3단계 요구사항 정리
    
    * add : [2단계] PR 피드백 반영 & [3단계] 요구사항 반영
    
    - 사다리 게임에 참여하는 사람 이름 입력 기능 및 유효성 검사 추가
    - 사다리 상태 출력시 사람 이름도 함께 출력되도록 수정
    - 사람 이름 입력 글자 수 제한(최대 5글자)에 따라 사다리 폭 수정
    - 사다리 가로 라인이 겹치지 않도록 로직 수정
    - 리팩토링 : 클래스 세분화, 접근제어자 수정, 변수 및 메서드 명 수정

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,13 @@
-import java.util.Scanner;
-
 public class Main {
-    public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        LadderGame game = null;
-        try { // 사다리 게임에 참여할 사람의 수와 사다리 높이를 시스템에서 입력 받기
-            int numberOfParticipant = inputNumberOfParticipant();
-            int height = inputLadderHeight();
-            game = new LadderGame(numberOfParticipant, height);
+        LadderGame game = new LadderGame();
+        try {
+            game.start();
+            game.printParticipantsAndLadder();
         } catch (Exception e) {
             System.err.println(e.getMessage());
-            return;
         }
-        System.out.println(game.getLadderStatus());
     }
 
-    /**
-     * 사다리 게임에 참여할 사람의 수 입력 받기
-     */
-    private static int inputNumberOfParticipant() throws Exception {
-        System.out.println("참여할 사람은 몇 명인가요?");
-        return inputPositiveNumber();
-    }
-
-    /**
-     * 사다리 높이 입력 받기
-     */
-    private static int inputLadderHeight() throws Exception {
-        System.out.println("최대 사다리 높이는 몇 개인가요?");
-        return inputPositiveNumber();
-    }
-
-    /**
-     * 양수 입력 받기
-     */
-    private static int inputPositiveNumber() throws Exception {
-        int number = 0;
-        try {
-            number = scanner.nextInt();
-        } catch (Exception e) {
-            throw new Exception("잘못된 입력 값입니다.");
-        }
-        if (number <= 0) {
-            throw new Exception("0과 같거나 작은 수를 입력하였습니다.");
-        }
-        return number;
-    }
 }
