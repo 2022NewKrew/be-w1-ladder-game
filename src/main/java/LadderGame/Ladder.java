6024a143bfa5b1da9b07742665a57670eef80bcf
Seonghun337 <77823678+Seonghun337@users.noreply.github.com>
2021-12-29 22:06:56 +0900

    implement step3 and modify from gt's 2nd feedback (#323)
    Co-authored-by: ethan <ethan.337@kakaocorp.com>

--- src/main/java/LadderGame/Ladder.java
+++ src/main/java/LadderGame/Ladder.java
@@ -1,82 +1,113 @@
 package LadderGame;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Ladder {
-    private static Random random = new Random();
     private static final String PILLAR = "|";
-    private static final String BRIDGE = "-";
-    private static final String BLANK = " ";
+    private static final String BRIDGE = "-----";
+    private static final String BLANK = "     ";
 
-    private int n;
-    private int m;
+    private int numOfPerson;
+    private int height;
     private ArrayList<LadderRow> ladder;
+    private List<String> personNames;
 
-    public Ladder(){
-        ladder = new ArrayList<LadderRow>();
-        random.setSeed(System.currentTimeMillis());
-    }
-
-    //
-    // n과 m을 입력받아 사다리를 생성합니다.
-    //
-    public void createLadder(){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("참여할 사람은 몇 명인가요?");
-        int n = sc.nextInt();
-        System.out.println("최대 사다리 높이는 몇 개인가요?");
-        int m = sc.nextInt();
-
-        if (n <= 0 || m <= 0)
+    public Ladder(List<String> personNames, int height) {
+        if (personNames.isEmpty() || height <= 0)
             throw new IllegalArgumentException();
 
-        this.n = n;
-        this.m = m;
+        this.personNames = personNames;
+        this.numOfPerson = personNames.size();
+        this.height = height;
 
-        for(int i = 0; i < m; i++){
-            ladder.add(new LadderRow(n));
+        ladder = new ArrayList<>();
+        for (int i = 0; i < height; i++) {
+            ladder.add(new Ladder.LadderRow(numOfPerson));
         }
     }
+
 
     //
     // Row를 한 줄씩 가져와 화면에 출력합니다.
     //
-    public void printLadder(){
+    public void printLadder() {
         StringBuilder builder = new StringBuilder();
-
-        for(int i = 0; i < m; i++){
-            builder.append(ladder.get(i));
+        for (String personName : personNames){
+            try {
+                builder.append(centerAligned(personName, 5));
+                builder.append(" ");
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+        builder.append("\n");
+        for (LadderRow row : ladder) {
+            builder.append("  "); //왼쪽 패딩 2칸 공백
+            builder.append(row);
             builder.append("\n");
         }
+
         System.out.println(builder.toString());
     }
 
     //
+    // 입력 문자를 size에 맞게 가운데 정렬합니다. 예시는 아래와 같습니다. (_:공백)
+    // 1글자,size=5 -> __a__
+    // 2글자,size=5 -> _aa__
+    // 3글자,size=5 -> _aaa_
+    // 4글자,size=5 -> aaaa_
+    private String centerAligned(String str, int size){
+
+        if(str.isEmpty() || str.isBlank()) {
+            return null;
+        }
+
+        int leftPad = (int)((size - str.length()) / 2);
+
+        if (leftPad == 0){
+            return String.format("%"+(-size)+"s", str);
+        }
+
+        return String.format(
+                "%" + (-size) + "s",
+                String.format("%" + (leftPad+str.length()) + "s", str)
+                );
+    }
+
+
+    //
     // 사다리의 한 행을 나타내는 이너클래스입니다.
     //
-    class LadderRow{
+    class LadderRow {
 
-        private ArrayList<Boolean> row;
+        private ArrayList<Boolean> points;
 
-        public LadderRow(int n){
-            row = new ArrayList<Boolean>(n-1);
-            for(int i=0; i<n-1; i++){
-                row.add(random.nextBoolean());
+        public LadderRow(int numOfPerson) {
+            Random random = new Random();
+            points = new ArrayList<Boolean>(numOfPerson - 1);
+
+            Boolean lastPoint = false;
+            for (int i = 0; i < numOfPerson - 1; i++) {
+                lastPoint = random.nextBoolean() && (!lastPoint);
+                points.add(lastPoint);
             }
         }
 
         //
-        // 외부 클래스의 지정된 문자를 가져와 현재 row 상태를 문자열로 변환합니다.
+        // 외부 클래스의 지정된 문자를 가져와 현재 row 상태를 문자열로 반환합니다.
         //
-        public String toString(){
+        public String toString() {
             StringBuilder builder = new StringBuilder();
+
             builder.append(PILLAR);
-            for(int i=0; i<n-1; i++){
-                builder.append(row.get(i) ? BRIDGE : BLANK);
+            for (Boolean point : points) {
+                builder.append(point ? BRIDGE : BLANK);
                 builder.append(PILLAR);
             }
+
             return builder.toString();
         }
 
