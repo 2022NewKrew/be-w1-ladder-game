5410778b00d5bf331d1f8a1267d969c164c98fab
benn.diagram <benn.diagram@kakaocorp.com>
2021-12-29 11:14:37 +0900

    리뷰 내용을 토대로 수정

--- ladderGame/src/com/company/Main.java
+++ ladderGame/src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
 
+import com.company.ladder.Ladder;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("참여한 사람은 몇명인가요?");
+        int attendantNum = scanner.nextInt();
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        int ladderHeight = scanner.nextInt();
+
+        Ladder ladder = new Ladder(attendantNum, ladderHeight);
+        ladder.makeLadder();
+        ladder.printLadder();
     }
 }
