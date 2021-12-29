26e02799031219adddef4363c8a669d86a4bce60
geatrigger-kim <geatrigger.kim@kakaocorp.com>
2021-12-29 13:22:21 +0900

    Apply feedback(static, naming)

--- src/main/java/com/gradle/geatrigger/Ladder.java
+++ src/main/java/com/gradle/geatrigger/Ladder.java
@@ -1,31 +1,33 @@
 package com.gradle.geatrigger;
 
 public class Ladder {
-    static int[][] ladder;
-    static int cnt, height;
+    int[][] ladder;
+    // width, height의 경우 이미 Ladder 인스턴스에 속해있어 ladder 생략
+    int peopleCount, width, height;
 
-    public Ladder(int cnt, int height) {
-        this.cnt = cnt;
+    public Ladder(int peopleCount, int height) {
+        this.peopleCount = peopleCount;
+        this.width = peopleCount - 1;
         this.height = height;
-        this.ladder = new int[height][cnt];
+        this.ladder = new int[this.height][this.width];
     }
 
     public void makeLadder() {
         for (int i = 0; i < height; i++) {
-            for (int j = 0; j < cnt - 1; j++) {
+            for (int j = 0; j < width; j++) {
                 ladder[i][j] = (int) Math.round(Math.random());
             }
-            ladder[i][cnt - 1] = 0;
         }
     }
 
     public String toString() {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < height; i++) {
-            for (int j = 0; j < cnt; j++) {
+            for (int j = 0; j < width; j++) {
                 sb.append(makeVerticals());
                 sb.append(makeHorizons(ladder[i][j]));
             }
+            sb.append(makeVerticals());
             sb.append("\n");
         }
         return sb.toString();
--- src/main/java/com/gradle/geatrigger/LadderGame.java
+++ src/main/java/com/gradle/geatrigger/LadderGame.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 
 public class LadderGame {
     public static void main(String[] args) {
-        int cnt, height;
+        int peopleCount, height;
         Scanner sc = new Scanner(System.in);
         System.out.println("참여할 사람은 몇 명인가요?");
-        cnt = sc.nextInt();
+        peopleCount = sc.nextInt();
         System.out.println("최대 사다리 높이는 몇 개인가요?");
         height = sc.nextInt();
 
-        Ladder ladder = new Ladder(cnt, height);
+        Ladder ladder = new Ladder(peopleCount, height);
 
         ladder.makeLadder();
         System.out.println(ladder.toString());
