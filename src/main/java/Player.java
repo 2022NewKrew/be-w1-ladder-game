fa258c972b987c50cad21c5d7dfff5aeabc39f1b
kusakina0608 <platina.kusakina@gmail.com>
2021-12-29 10:12:25 +0900

    Player 클래스에 Setter 추가

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -5,8 +5,12 @@ public class Player {
         this.name = name;
     }
 
+    public String getName() {
+        return name;
+    }
+
     @Override
     public String toString() {
-        return name;
+        return "{Player: " + name + '}';
     }
 }
