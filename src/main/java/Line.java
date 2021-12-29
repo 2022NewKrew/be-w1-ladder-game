90204d139bf5fb6fa641a85a79650fd4dd32a8a5
punsoo <sonu.sohn@kakaocorp.com@gmail.com>
2021-12-29 23:27:41 +0900

    feat: Line 클래스 구현

---
+++ src/main/java/Line.java
@@ -0,0 +1,20 @@
+package main.java;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Line {
+    private final List<Point> points ;
+
+    public Line(List<Point> points){
+        this.points = points;
+    }
+
+    public List<Point> points(){
+        return Collections.unmodifiableList(points);
+    }
+
+    public int move(int position){
+        return position + points.get(position).move();
+    }
+}
