e56e234dc8f8e76ab82334637d7a84e60ccbcfcd
charlie.p1 <charlie.p1@kakaocorp.com>
2021-12-29 13:25:11 +0900

    Move string constants in LadderState to LadderConverter

--- app/src/main/java/com/chanminkim/w1/LadderConverter.java
+++ app/src/main/java/com/chanminkim/w1/LadderConverter.java
@@ -1,13 +1,20 @@
 package com.chanminkim.w1;
 
 import java.util.Arrays;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class LadderConverter {
+    private static final Map<LadderState, String> STATE_STRING_MAP = Map.ofEntries(
+            Map.entry(LadderState.EMPTY, " "),
+            Map.entry(LadderState.VERTICAL, "|"),
+            Map.entry(LadderState.HORIZONTAL, "-")
+    );
+
     static String convertToString(Ladder ladder) {
         return Arrays.stream(ladder.getMap())
                 .map(line -> Arrays.stream(line)
-                        .map(LadderState::getValue)
+                        .map(STATE_STRING_MAP::get)
                         .collect(Collectors.joining()))
                 .collect(Collectors.joining("\n"));
     }
--- app/src/main/java/com/chanminkim/w1/LadderState.java
+++ app/src/main/java/com/chanminkim/w1/LadderState.java
@@ -1,17 +1,7 @@
 package com.chanminkim.w1;
 
 public enum LadderState {
-    EMPTY(" "),
-    VERTICAL("|"),
-    HORIZONTAL("-");
-
-    private final String value;
-
-    LadderState(String value) {
-        this.value = value;
-    }
-
-    public String getValue() {
-        return value;
-    }
+    EMPTY,
+    VERTICAL,
+    HORIZONTAL
 }
