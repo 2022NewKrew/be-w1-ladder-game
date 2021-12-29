59f79e3a47aecab09e6f077911171d35a2f6bf04
Gunyoung-Kim <gun025bba@naver.com>
2021-12-29 12:16:26 +0900

    가독성을 위한 ShapeMaker 리팩토링

--- src/main/java/com/gunyoung/one/boot/ShapeMaker.java
+++ src/main/java/com/gunyoung/one/boot/ShapeMaker.java
@@ -8,10 +8,14 @@ import java.util.List;
 public class ShapeMaker {
 
     private static final int BRIDGE_SIZE = 5;
-    private static final int BLANK_NUM_BEFORE_EACH_ROW = 2;
-    private static final String COLUMN_SHAPE = "|";
-    private static final String BRIDGE_SHAPE = "-----";
+    private static final int NUM_OF_BLANK_BEFORE_EACH_ROW = 2;
     private static final String BLANK = " ";
+    private static final String COLUMN_SHAPE = "|";
+    private static final String BRIDGE_SHAPE;
+
+    static {
+        BRIDGE_SHAPE = "-".repeat(BRIDGE_SIZE);
+    }
 
     private final Ladder ladder = Ladder.getInstance();
 
@@ -25,12 +29,12 @@ public class ShapeMaker {
     private void getShapeOfUsers(StringBuilder sb) {
         for (User user : ladder.getUsers()) {
             sb.append(
-                    getUserNameWithBlank(user)
+                    getFormattedUserName(user)
             );
         }
     }
 
-    private String getUserNameWithBlank(User user) {
+    private String getFormattedUserName(User user) {
         StringBuilder sb = new StringBuilder();
         String userName = user.getName();
         getBlanksBeforeName(sb, userName);
@@ -40,17 +44,18 @@ public class ShapeMaker {
     }
 
     private void getBlanksBeforeName(StringBuilder sb, String userName) {
-        int numOfBlankBeforeName = getNumOfBlankBeforeName(userName);
         sb.append(
                 getBlankWithRepeat(getNumOfBlankBeforeName(userName))
         );
     }
 
     private int getNumOfBlankBeforeName(String userName) {
-        if (userName.length() <= 2) {
+        int twoBlankThreshold = 2;
+        int oneBlankThreshold = 3;
+        if (userName.length() <= twoBlankThreshold) {
             return 2;
         }
-        if (userName.length() <= 3) {
+        if (userName.length() <= oneBlankThreshold) {
             return 1;
         }
         return 0;
@@ -76,7 +81,7 @@ public class ShapeMaker {
 
     private void getBlanksBeforeRowStart(StringBuilder sb) {
         sb.append(
-                getBlankWithRepeat(BLANK_NUM_BEFORE_EACH_ROW)
+                getBlankWithRepeat(NUM_OF_BLANK_BEFORE_EACH_ROW)
         );
     }
 
