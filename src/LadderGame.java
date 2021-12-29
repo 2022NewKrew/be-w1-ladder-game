55dcf72e33e1dc543bb9302e489e5b4beeafb094
champ.ion <champ.ion@kakaocorp.com>
2021-12-30 01:49:54 +0900

    Ladder Game STEP3 - Feedback(PR#355) - [Refactoring] Applied 'stream' to printer of Ladder Game information in User Interface

--- src/LadderGame.java
+++ src/LadderGame.java
@@ -4,9 +4,9 @@ import java.util.List;
 public class LadderGame {
     public static void main(String[] args) {
         UserInterface ui = new UserInterface();
-        List<String> userList = new ArrayList<>(ui.inputUserInfo());
+        List<String> userList = new ArrayList<>(ui.getUserInfoFromScanner());
         int numPeople = userList.size();
-        int ladderHeight = ui.inputLadderHeight();
+        int ladderHeight = ui.getLadderHeight();
 
         Ladder ladder = new Ladder(numPeople, ladderHeight);
         ui.printLadderGame(userList, ladder);
--- src/UserInterface.java
+++ src/UserInterface.java
@@ -8,7 +8,7 @@ public class UserInterface {
 
     Scanner scanner = new Scanner(System.in);
 
-    public List<String> inputUserInfo() {
+    public List<String> getUserInfoFromScanner() {
         try {
             System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
             String[] users = scanner.nextLine().split(SEPARATOR);
@@ -18,26 +18,26 @@ public class UserInterface {
             return Arrays.asList(users);
         } catch (IllegalArgumentException iae) {
             System.out.println("이름은 " + MAX_NUM_STRING + "글자를 넘어갈 수 없습니다.");
-            return inputUserInfo();
+            return getUserInfoFromScanner();
         }
     }
 
     private void checkUserNames(String[] users) {
-        if (Arrays.stream(users).allMatch(user -> user.length() <= MAX_NUM_STRING))
+        if (Arrays.stream(users).anyMatch(user -> user.length() > MAX_NUM_STRING))
             throw new IllegalArgumentException();
     }
 
-    public int inputLadderHeight() {
+    public int getLadderHeight() {
         try {
             System.out.println("최대 사다리 높이는 몇 개인가요?");
-            return inputPositiveInt();
+            return getPositiveIntFromScanner();
         } catch (IllegalArgumentException iae) {
             System.out.println("사다리 높이는 양의 정수여야 합니다.");
-            return inputLadderHeight();
+            return getLadderHeight();
         }
     }
 
-    private int inputPositiveInt() {
+    private int getPositiveIntFromScanner() {
         int value = scanner.nextInt();
         if (value <= 0) {
             throw new IllegalArgumentException();
@@ -46,18 +46,8 @@ public class UserInterface {
     }
 
     public void printLadderGame(List<String> userList, Ladder ladder) {
-        printUsers(userList);
-        printLadder(ladder);
-    }
-
-    private void printUsers(List<String> userList) {
-        for (String user : userList) {
-            System.out.printf("%" + MAX_NUM_STRING + "s ", user);
-        }
+        userList.forEach(user -> System.out.printf("%" + MAX_NUM_STRING + "s ", user));
         System.out.println();
-    }
-
-    private void printLadder(Ladder ladder) {
         System.out.println(ladder);
     }
 }
