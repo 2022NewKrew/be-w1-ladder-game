4ba3597e52de6d914ae7de4b53acd54a2ffaf9bc
tony.ahn <tony.ahn@kakaocorp.com>
2021-12-29 18:41:00 +0900

    Line클래스에 불필요한 메소드 제거

--- src/com/kakao/ladder/step3/Line.java
+++ src/com/kakao/ladder/step3/Line.java
@@ -9,19 +9,9 @@ public class Line {
 
     public Line() { }
 
-    // 전체 개수를 제공
-    public int getSize() {
-        return points.size();
-    }
-
     // 값을 추가
     public void addLine(boolean isBridge) {
         points.add(isBridge);
-    }
-
-    // 해당 index 값을 제공
-    public boolean getBridge(int index) {
-        return points.get(index);
     }
 
     // toString에 쓰이는 method. boolean 값에 맞게 bridge를 string으로 만들어준다.
@@ -34,9 +24,9 @@ public class Line {
     // Line을 String으로 변환해준다.
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < getSize(); i++) {
+        for(int i = 0; i < points.size(); i++) {
             sb.append('|');
-            sb.append(makeBridge(getBridge(i)));
+            sb.append(makeBridge(points.get(i)));
         }
         sb.append('|');
         return sb.toString();
