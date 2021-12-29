e59c7b63fb29686b52122914d35a05c16fd35fd3
charlie.p1 <charlie.p1@kakaocorp.com>
2021-12-29 18:40:25 +0900

    Add PIECE_WIDTH constant in LadderConverter

--- app/src/main/java/com/chanminkim/w1/view/LadderConverter.java
+++ app/src/main/java/com/chanminkim/w1/view/LadderConverter.java
@@ -7,10 +7,11 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class LadderConverter {
+    private static final Integer PIECE_WIDTH = 5;
     private static final Map<LadderState, String> STATE_STRING_MAP = Map.ofEntries(
-            Map.entry(LadderState.EMPTY, " "),
+            Map.entry(LadderState.EMPTY, " ".repeat(PIECE_WIDTH)),
             Map.entry(LadderState.VERTICAL, "|"),
-            Map.entry(LadderState.HORIZONTAL, "-")
+            Map.entry(LadderState.HORIZONTAL, "-".repeat(PIECE_WIDTH))
     );
 
     public static String convertToString(Ladder ladder) {
