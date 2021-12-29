2442c60733317c47575f9eb227fc912d999c3e5c
geatrigger-kim <geatrigger.kim@kakaocorp.com>
2021-12-29 18:41:07 +0900

    Write names on top of ladder

--- src/main/java/com/gradle/geatrigger/Ladder.java
+++ src/main/java/com/gradle/geatrigger/Ladder.java
@@ -1,15 +1,22 @@
 package com.gradle.geatrigger;
 
 public class Ladder {
+    final static int INTERVAL = 5;
+
     int[][] ladder;
     // width, height의 경우 이미 Ladder 인스턴스에 속해있어 ladder 생략
     int peopleCount, width, height;
+    String[] names;
 
-    public Ladder(int peopleCount, int height) {
+    StringBuilder sb;
+
+    public Ladder(int peopleCount, int height, String namesString) {
         this.peopleCount = peopleCount;
         this.width = peopleCount - 1;
         this.height = height;
         this.ladder = new int[this.height][this.width];
+        this.names = namesString.split(",");
+        sb = new StringBuilder();
     }
 
     public void makeLadder() {
@@ -21,16 +28,31 @@ public class Ladder {
     }
 
     public String toString() {
-        StringBuilder sb = new StringBuilder();
+        sb.setLength(0);
+        writeNames();
+        writeLadder();
+        return sb.toString();
+    }
+
+    private void writeLadder() {
         for (int i = 0; i < height; i++) {
+            sb.append(" ".repeat(INTERVAL / 2));
             for (int j = 0; j < width; j++) {
                 sb.append(makeVerticals());
-                sb.append(makeHorizons(ladder[i][j]));
+                sb.append(makeHorizons(ladder[i][j] * INTERVAL));
             }
             sb.append(makeVerticals());
             sb.append("\n");
         }
-        return sb.toString();
+    }
+
+    private void writeNames() {
+        for (String name : names) {
+            sb.append(" ".repeat(INTERVAL / 2 - name.length() / 2));
+            sb.append(name);
+            sb.append(" ".repeat((INTERVAL - INTERVAL / 2) - (name.length() - name.length() / 2) + 1));
+        }
+        sb.append("\n");
     }
 
     private String makeHorizons(int cnt) {
--- src/main/java/com/gradle/geatrigger/LadderGame.java
+++ src/main/java/com/gradle/geatrigger/LadderGame.java
@@ -5,13 +5,16 @@ import java.util.Scanner;
 public class LadderGame {
     public static void main(String[] args) {
         int peopleCount, height;
+        String namesString;
         Scanner sc = new Scanner(System.in);
+        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표로 구분하세요)");
+        namesString = sc.next();
         System.out.println("참여할 사람은 몇 명인가요?");
         peopleCount = sc.nextInt();
         System.out.println("최대 사다리 높이는 몇 개인가요?");
         height = sc.nextInt();
 
-        Ladder ladder = new Ladder(peopleCount, height);
+        Ladder ladder = new Ladder(peopleCount, height, namesString);
 
         ladder.makeLadder();
         System.out.println(ladder.toString());
