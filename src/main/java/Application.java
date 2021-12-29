4e2ae272827e962dada37e7ccdb854b646ad38af
sukstar76 <sukstar76@gmail.com>
2021-12-29 15:22:07 +0900

    참여자를 위한 인풋 추가
    참여자 정보를 받기 위해 인풋메소드 추가 및 분리하였스빈다.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,25 +1,43 @@
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Application {
-    public static int numOfPeople;
-    public static int heightOfLadder;
+    private static final Scanner SCANNER = new Scanner(System.in);
 
     public static void runApp() {
-        input();
+        List<Participant> participants = initializeParticipants();
+        Ladder ladder = initializeLadder(participants.size());
 
-        Ladder ladder = new Ladder(heightOfLadder, numOfPeople);
         ladder.makeRandomLadder();
         ladder.printLadder();
     }
 
-    public static void input() {
-        Scanner scanner = new Scanner(System.in);
+    public static List<Participant> initializeParticipants() {
+        String[] nameOfParticipants = inputParticipants();
 
-        System.out.println("참여할 사람은 몇 명인가요?");
-        numOfPeople = Integer.parseInt(scanner.nextLine());
+        return Arrays.stream(nameOfParticipants).map(name -> new Participant(name)).collect(Collectors.toList());
+    }
 
+    public static Ladder initializeLadder(int numOfPeople) {
+        int heightOfLadder = inputHeight();
+        Ladder ladder = new Ladder(heightOfLadder, numOfPeople);
+
+        return ladder;
+    }
+
+    public static int inputHeight() {
         System.out.println("최대 사다리 높이는 몇 개인가요?");
-        heightOfLadder = Integer.parseInt(scanner.nextLine());
+
+        return Integer.parseInt(SCANNER.nextLine());
+    }
+
+    public static String[] inputParticipants() {
+        System.out.println("참여할 사람 이름을 입력하세요.");
+        String inputOfParticipants = SCANNER.nextLine();
+
+        return inputOfParticipants.split(" ");
     }
 
     public static void main(String[] args) {
