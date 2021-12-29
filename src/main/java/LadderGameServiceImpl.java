357cb65a54b51ff13a7fd27de9f8526f24061892
Hongjeongmin <wjdals0471@naver.com>
2021-12-29 16:40:08 +0900

    사다리 정보 이중 for 분리

--- src/main/java/LadderGameServiceImpl.java
+++ src/main/java/LadderGameServiceImpl.java
@@ -2,7 +2,7 @@ import java.util.Random;
 
 public class LadderGameServiceImpl implements LadderGameService {
 
-    private static final Random randomInstance = new Random();
+    private final Random randomInstance = new Random();
 
     @Override
     public boolean[][] createLadderMap(int row, int col) {
@@ -10,12 +10,19 @@ public class LadderGameServiceImpl implements LadderGameService {
         boolean[][] ladderMap = new boolean[row][col];
 
         for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                ladderMap[i][j] = trueAndFalseGenerator();
-            }
+            ladderMap[i] = createFloorMap(col);
         }
 
         return ladderMap;
+    }
+
+    private boolean[] createFloorMap(int col) {
+        boolean[] map = new boolean[col];
+        for (int j = 0; j < col; j++) {
+            map[j] = trueAndFalseGenerator();
+        }
+
+        return map;
     }
 
     private boolean trueAndFalseGenerator() {
