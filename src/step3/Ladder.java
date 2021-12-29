54eb3af7256c8dbc28b5f2f41160e0317e69acca
tony.ahn <tony.ahn@kakaocorp.com>
2021-12-29 15:50:08 +0900

    사다리게임 3단계 수정

--- src/step3/Ladder.java
+++ src/step3/Ladder.java
@@ -1,83 +1,71 @@
-package step3;
+package com.kakao.ladder.step3;
 
+import step3.Line;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Ladder {
 
-    private ArrayList<String> ladder = new ArrayList<String>();;
-    private int peopleNum;
     private int height;
-    private ArrayList<String> peoples;
-    private StringBuilder sb = new StringBuilder();
-    private ArrayList<Line> lines = new ArrayList<Line>();
-    private Line line;
+    private List<String> peoples;
+    private List<Line> lines = new ArrayList<>();
 
     // 사람, 높이를 주지 않으면 자동으로 설정해준다.
     public Ladder() {
-        peoples  = new ArrayList<String>(Arrays.asList("A", "B"));
-        peopleNum = 2;
-        height = 5;
-        makeLadder();
+        this(Arrays.asList("A", "B"), 5);
     }
 
     // 사람, 높이를 주었을 때 그에 맞게 초기화해준다. 자동으로 사다리를 만든다.
-    public Ladder(ArrayList<String> p, int h) {
-        peoples = p;
-        peopleNum = p.size();
-        height = h;
+    public Ladder(List<String> peoples, int height) {
+        this.peoples = peoples;
+        this.height = height;
         makeLadder();
     }
 
-    // bridge를 line에 추가해주고 그에 맞는 String을 return해준다.
-    private String makeBridge(boolean canMake) {
+    // bridge를 line에 추가해주고 bridge를 만들었다면 그 다음번엔 bridge를 만들지 못하므로 false를 return해준다.
+    private boolean makeBridge(boolean canMake, Line line) {
         if(canMake) {
             line.addLine(true);
-            return "-----";
+            return false;
         }
         line.addLine(false);
-        return "     ";
+        return true;
     }
 
-    // 이전에 bridge가 존재하는 경우 "     " 를 추가하고 line에 false 정보를 추가해준다. 이외의 경우는 랜덤하게 설정한다.
-    private String getRandomBridge() {
-        if(line.getSize() != 0 && line.getLast()) {
-            return makeBridge(false);
+    // canMake가 false인 경우 bridge를 만들지 않고 종료하고 그렇지 않은 경우 랜덤하게 bridge를 만들어준다.
+    private boolean makeRandomBridge(boolean canMake, Line line) {
+        if(!canMake) {
+            return makeBridge(false, line);
         }
         double randNum;
         randNum = Math.random();
         if(randNum >= 0.5) {
-            return makeBridge(false);
+            return makeBridge(false, line);
         }
-        return makeBridge(true);
+        return makeBridge(true, line);
     }
 
-    // 사다리를 만들며 랜덤하게 "-----" 또는 "     "를 규칙에 어긋나지 않게 만들어준다.
-    private String makeLine() {
-        sb.setLength(0);
-        line = new Line();
-        for(int i=1;i<peopleNum;i++) {
-            sb.append('|');
-            sb.append(getRandomBridge());
+    // 현재 bridge를 만들수 있는 경우 canMake 값이 true이다. 초기에 가장 왼쪽 bridge는 항상 만들수 있으므로 true로 초기화한다.
+    private void makeLine() {
+        boolean canMake = true;
+        Line line = new Line();
+        for(int i = 1; i < peoples.size(); i++) {
+            canMake = makeRandomBridge(canMake, line);
         }
-        sb.append('|');
         lines.add(line);
-        return sb.toString();
     }
 
     // 사다리를 만들어준다.
     private void makeLadder() {
-        for(int i=0;i<height;i++) {
-            ladder.add(makeLine());
+        for(int i = 0; i < height; i++) {
+            makeLine();
         }
     }
 
     // 이름이 5자 이상인 경우 앞의 5자까지만 추가해준다. 길이에 맞춰서 다음 사람의 이름까지 적절한 공백을 준다.
     private void printPeople(String people) {
-        int endIdx = Math.min(people.length(), 5);
-        System.out.print(people.substring(0,endIdx));
-        for(int i=0;i<6-endIdx;i++)
-            System.out.print(" ");
+        System.out.printf("%-6.5s", people);
     }
 
     // 사람 리스트를 출력한다.
@@ -91,8 +79,8 @@ public class Ladder {
     //전체 사다리를 출력한다.
     public void printLadder() {
         printPeoples();
-        for(String s : ladder) {
-            System.out.println(s);
+        for(Line line : lines) {
+            System.out.println(line.toString());
         }
     }
 
