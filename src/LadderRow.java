f5ea74a000f5abd3bf3b913fa69fe49993d2f30d
champ.ion <champ.ion@kakaocorp.com>
2021-12-29 14:00:22 +0900

    Ladder Game STEP3 - [Feature] Added "RadderRow" class for using ArrayList of Collections

---
+++ src/LadderRow.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.StringJoiner;
+
+public class LadderRow {
+    private static final int FIRST_INDEX = 0;
+    private static final String PILLAR = "|";
+    private static final String EMPTY = "     ";
+    private static final String BRIDGE = "-----";
+
+    private final ArrayList<Boolean> row = new ArrayList<>();
+
+    public LadderRow(int width) {
+        final Random RANDOM = new Random();
+
+        for (int w = 0; w < width; w++) {
+            boolean bridge = (w == FIRST_INDEX || !row.get(preIndex(w))) && RANDOM.nextBoolean();
+            row.add(bridge);
+        }
+    }
+
+    private int preIndex(int idx) {
+        return --idx;
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner line = new StringJoiner(PILLAR, PILLAR, PILLAR);
+        for (Boolean bridge : row) {
+            line.add(bridge ? BRIDGE : EMPTY);
+        }
+        return line.toString();
+    }
+}
