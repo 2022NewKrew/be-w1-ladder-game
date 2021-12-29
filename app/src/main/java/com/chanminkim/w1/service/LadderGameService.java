fbd47dc9e5884e891c47c89798f3be19febd241e
charlie.p1 <charlie.p1@kakaocorp.com>
2021-12-29 18:47:17 +0900

    Add LadderService class
    - Move building ladder logic from LadderGameService to LadderService

--- app/src/main/java/com/chanminkim/w1/service/LadderGameService.java
+++ app/src/main/java/com/chanminkim/w1/service/LadderGameService.java
@@ -1,55 +1,21 @@
 package com.chanminkim.w1.service;
 
 import com.chanminkim.w1.controller.LadderGameDTO;
-import com.chanminkim.w1.model.*;
+import com.chanminkim.w1.model.Ladder;
+import com.chanminkim.w1.model.LadderGame;
+import com.chanminkim.w1.model.Player;
 
-import java.security.SecureRandom;
 import java.util.List;
-import java.util.Random;
 import java.util.stream.Collectors;
 
 public class LadderGameService {
-    private final Random random;
-
-    public LadderGameService() {
-        this(new SecureRandom());
-    }
-
-    public LadderGameService(Random random) {
-        this.random = random;
-    }
+    private final LadderService service = new LadderService();
 
     public LadderGame buildLadderGame(LadderGameDTO dto) {
-        Ladder ladder = buildLadder(dto.getPlayers().size(), dto.getHeightOfLadder());
+        Ladder ladder = service.buildLadder(dto.getPlayers().size(), dto.getHeightOfLadder());
         List<Player> players = dto.getPlayers().stream()
                 .map(Player::new)
                 .collect(Collectors.toUnmodifiableList());
         return new LadderGame(players, ladder);
-    }
-
-    public Ladder buildLadder(Integer numberOfPlayers, Integer heightOfLadder) {
-        Ladder ladder = new Ladder();
-        int widthOfLadder = numberOfPlayers * 2 - 1;
-        for (int i = 0; i < heightOfLadder; i++) {
-            ladder.appendLine(buildLine(widthOfLadder));
-        }
-        return ladder;
-    }
-
-    private LadderLine buildLine(Integer width) {
-        LadderLine line = new LadderLine();
-        for (int i = 0; i < width - 1; i += 2) {
-            line.appendState(LadderState.VERTICAL);
-            line.appendState(buildPiece(line.isOccurredBefore()));
-        }
-        line.appendState(LadderState.VERTICAL);
-        return line;
-    }
-
-    private LadderState buildPiece(boolean isOccurredBefore) {
-        if (isOccurredBefore) {
-            return LadderState.EMPTY;
-        }
-        return random.nextBoolean() ? LadderState.HORIZONTAL : LadderState.EMPTY;
     }
 }
