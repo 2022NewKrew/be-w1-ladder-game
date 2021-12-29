d08d665406b89c72017b3b7ceb7dfe476b9c9500
HYEONSEOK1 <gustir929@gmail.com>
2021-12-30 00:51:42 +0900

    - refactor : LadderRowView 와 마찬가지로 PlayerRow -> PlayerRowView로 변경

--- src/main/java/game/Ladder.java
+++ src/main/java/game/Ladder.java
@@ -4,18 +4,18 @@ import java.util.ArrayList;
 
 public class Ladder {
 
-    private final PlayerRow playerRow;
+    private final PlayerRowView playerRowView;
     private final ArrayList<LadderRow> ladderRowList = new ArrayList<>();
     public final static int typeSize = 5;
 
     public Ladder(ArrayList<String> playerList, int height) {
-        playerRow = new PlayerRow(playerList);
+        playerRowView = new PlayerRowView(playerList);
         for(int i = 0; i<height;i++ ) {
             ladderRowList.add(new LadderRow(playerList.size()));
         }
     }
     public void printLadder() {
-        System.out.println(playerRow.toString());
+        System.out.println(playerRowView.DrawElement());
         for(LadderRow ladderRow : ladderRowList)
         {
             System.out.println(LadderRowView.DrawElement(ladderRow.getElementList()));
--- src/main/java/game/PlayerRowView.java
+++ src/main/java/game/PlayerRowView.java
@@ -5,14 +5,14 @@ import java.util.ArrayList;
 import static game.Ladder.typeSize;
 
 // Player 의 네임을 저장하는 row class
-public class PlayerRow{
+public class PlayerRowView {
 
     private final ArrayList<String> playerList;
-    public PlayerRow(ArrayList<String> playerList) {
+    public PlayerRowView(ArrayList<String> playerList) {
         this.playerList = playerList;
     }
-    @Override
-    public String toString() {
+
+    public String DrawElement() {
         StringBuilder stringBuilder = new StringBuilder();
         String stringFormat = "%" + (typeSize +1) + "s";
         for(String player : playerList)
