a36c848fb52b3b96837b6e968a8c9cb37cc18b08
Daecheol Shin <60842206+cheol7826@users.noreply.github.com>
2021-12-29 22:07:09 +0900

    사다리게임 Step3 구현 (#336)
    * 사다리게임 Step1 구현 (#61)
    
    Co-authored-by: iron.shin <iron.shin@kakaocorp.com>
    
    * 사다리게임 Step2 구현
    
    * 사다리게임 Step3 구현
    
    * 코드리뷰 반영 리팩토링
    
    * view관련 로직 분리 및 리팩토링
    
    Co-authored-by: iron.shin <iron.shin@kakaocorp.com>

---
+++ src/LadderGameV3.java
@@ -0,0 +1,6 @@
+public class LadderGameV3 {
+    public static void main(String[] args){
+        LadderManager lm = new LadderManager();
+        lm.startLadder();
+    }
+}
---
+++ src/Line.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Line {
+    private final List<Boolean> line;
+
+    public Line(int numOfPerson){
+        line = new ArrayList<>();
+
+        for(int i=0; i<numOfPerson-1; i++) {
+            line.add(randomValue(i));
+        }
+    }
+
+    public List<Boolean> getLine() {
+        return line;
+    }
+
+    private boolean randomValue(int index){
+        if(collisionCheck(index))
+            return false;
+
+        return (int)(Math.random()*2) == 1;
+    }
+
+    private boolean collisionCheck(int index){
+        return index > 0 && line.get(index - 1);
+    }
+}
