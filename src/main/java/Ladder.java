acad94e5bfb03a5c0d5d40a7d16b2c155edad361
ben.ny <ben.ny@kakaocorp.com>
2021-12-29 13:27:16 +0900

    Refactor : LadderMaker를 제거하고 그 기능을 Ladder 클래스에 추가

--- src/main/java/Ladder.java
+++ src/main/java/Ladder.java
@@ -1,14 +1,34 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Ladder {
     private final LadderInfo info;
     private final ArrayList<ArrayList<Boolean>> connections;
+    private final Random random = new Random();
 
     public Ladder(LadderInfo info) {
         this.info = info;
-        LadderMaker maker = new LadderMaker(info);
-        connections = maker.makeConnections();
+        connections = makeConnections();
     }
+
+    public ArrayList<ArrayList<Boolean>> makeConnections() {
+        ArrayList<ArrayList<Boolean>> connections = new ArrayList<>();
+        int heightOfLadder = info.getHeightOfLadder();
+        for(int layer = 0; layer < heightOfLadder; layer++) {
+            connections.add(makeConnectionsOneLine());
+        }
+        return connections;
+    }
+
+    public ArrayList<Boolean> makeConnectionsOneLine() {
+        ArrayList<Boolean> connectionsOneLine = new ArrayList<>();
+        int numOfPeople = info.getNumOfPeople();
+        for(int person = 0; person < numOfPeople-1; person++) {
+            connectionsOneLine.add(random.nextBoolean());
+        }
+        return connectionsOneLine;
+    }
+
 
     public LadderInfo getInfo() {
         return info;
