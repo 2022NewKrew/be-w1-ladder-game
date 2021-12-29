9851320626589d9858f4ede984a55e3a4535586f
punsoo <sonu.sohn@kakaocorp.com@gmail.com>
2021-12-30 01:54:02 +0900

    feat: LineGenerator 구현

---
+++ src/main/java/LineGenerator.java
@@ -0,0 +1,30 @@
+package main.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LineGenerator {
+    private void generateFirstPoint(List<Point> points){
+        points.add(Point.randomPoint());
+    }
+
+    private void generateIntermediatePoint(List<Point> points, int numberOfPlayers){
+        Point prePoint = points.get(points.size()-1);
+        for(int i = 1; i < numberOfPlayers-1; i++){
+            points.add(Point.next(prePoint));
+        }
+    }
+
+    private void generateLastPoint(List<Point> points){
+        Point prePoint = points.get(points.size()-1);
+        points.add(Point.last(prePoint));
+    }
+
+    public Line generateLine(int numberOfPlayers){
+        List<Point> points = new ArrayList<>();
+        generateFirstPoint(points);
+        generateIntermediatePoint(points, numberOfPlayers);
+        generateLastPoint(points);
+        return new Line(points);
+    }
+}
