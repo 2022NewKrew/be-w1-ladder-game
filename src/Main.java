e0bf79f4f16584b99cbac9979b189597a61a39c0
eastclo <goldeastcloud@gmail.com>
2021-12-29 18:13:05 +0900

    3단계 구현 - fix inputParticipants와 inputLadderHeight의 예외처리 레퍼토리 통일, 불필요한 new ArrayList 삭제

--- src/Main.java
+++ src/Main.java
@@ -9,21 +9,21 @@ public class Main {
         List<String> participants = inputParticipants(scan);
         int ladderHeight = inputLadderHeight(scan);
 
-        LadderGame game = new LadderGame(new ArrayList<>(participants), new Ladder(participants.size(), ladderHeight), new StandardOutDisplayLadder());
+        LadderGame game = new LadderGame(participants, new Ladder(participants.size(), ladderHeight), new StandardOutDisplayLadder());
         game.displayLadder();
     }
 
     public static List<String> inputParticipants(Scanner scan) {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요");
-        List<String> participants = new ArrayList<>(Arrays.asList(scan.nextLine().split(",")));
+        List<String> participants = Arrays.asList(scan.nextLine().split(","));
 
-        if (participants.isEmpty() || participants.stream().anyMatch(s -> s.length() > MAX_NAME_LENGTH)) {
-            System.out.println("잘못 입력하셨습니다!");
-            System.out.println("이름은 " + MAX_NAME_LENGTH + "자 이하여야 합니다.\n");
-            return inputParticipants(scan);
+        if (!participants.isEmpty() && participants.stream().allMatch(s -> s.length() <= MAX_NAME_LENGTH)) {
+            return participants;
         }
 
-        return participants;
+        System.out.println("잘못 입력하셨습니다!");
+        System.out.println("이름은 " + MAX_NAME_LENGTH + "자 이하여야 합니다.\n");
+        return inputParticipants(scan);
     }
 
     public static int inputLadderHeight(Scanner scan) {
