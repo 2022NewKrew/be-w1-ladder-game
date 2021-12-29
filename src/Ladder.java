a63d876d544074a33d9af2d7e854b33ec1410406
green.day <green.day@kakaocorp.com>
2021-12-29 16:24:43 +0900

    refactor : width, height를 메소드에 인자로 전달하고 불필요한 인스턴스 변수 제거

--- src/Ladder.java
+++ src/Ladder.java
@@ -6,18 +6,16 @@ public class Ladder {
     private static final char HORIZONTAL_LINE = '-';
     private static final char EMPTY_LINE = ' ';
     private static final Random random = new Random();
-    private final int width;
-    private final int height;
     private char[][] ladderMap;
 
 
     public Ladder(int numOfPeople, int height) {
-        this.width = 2*numOfPeople -1;
-        this.height = height;
-        createLadder();
+        int width = 2 * numOfPeople - 1;
+
+        createLadder(height, width);
     }
 
-    private void createLadder() {
+    private void createLadder(int height, int width) {
         ladderMap = new char[height][width];
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
