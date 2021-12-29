0e5fa8fba93e6e310ab31c1efeb9204063e180cd
HyunJi / Rain <hyunjilim98@gmail.com>
2021-12-29 10:20:43 +0900

    사다리 게임 2단계 구현 (#199)
    * 사다리 게임 2단계 구현
    
    * 괄호 수정
    
    * 입력 클래스 분리

--- src/Application.java
+++ src/Application.java
@@ -1,19 +1,11 @@
 import ladder.Ladder;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class Application {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        System.out.println("참여할 사람은 몇 명인가요?");
-        int people = Integer.parseInt(br.readLine());
-        System.out.println("최대 사다리 높이는 몇 개인가요?");
-        int height = Integer.parseInt(br.readLine());
-
-        Ladder ladder = new Ladder(people, height);
-        ladder.printLadder();
+        ApplicationInput input = new ApplicationInput();
+        Ladder ladder = new Ladder(input.getPeople(), input.getHeight());
+        System.out.println(ladder);
     }
 }
