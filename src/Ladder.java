e46f53da069d6fbce538d204707eacf6e36ded12
green.day <green.day@kakaocorp.com>
2021-12-29 10:54:41 +0900

    refactor : LadderBuilder클래스 삭제, Ladder클래스에서 사다리 모양 만들도록 함

--- src/Ladder.java
+++ src/Ladder.java
@@ -3,23 +3,33 @@ import java.util.Random;
 public class Ladder {
     private int height;
     private int width;
-    private char[][] ladder;
+    private char[][] ladderMap;
 
-    public Ladder(int height, int width, char[][] ladder) {
+    public Ladder(int height, int numOfPeople) {
         this.height = height;
-        this.width = width;
-        this.ladder = ladder;
+        this.width = 2*numOfPeople -1;
+        createLadder();
     }
 
-    public int getHeight() {
-        return height;
+    private void createLadder() {
+        ladderMap = new char[height][width];
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (j % 2 == 0) {
+                    ladderMap[i][j] = '|';
+                } else {
+                    ladderMap[i][j] = getRandomLine();
+                }
+            }
+        }
     }
 
-    public int getWidth() {
-        return width;
+    private char getRandomLine() {
+        Random random = new Random();
+        return random.nextBoolean() ? '-' : ' ';
     }
 
     public char[][] getLadder() {
-        return ladder;
+        return ladderMap;
     }
 }
--- src/LadderMain.java
+++ src/LadderMain.java
@@ -1,20 +1,19 @@
 import java.util.Scanner;
 
 public class LadderMain {
-    public static int numPeople;
+    public static int numOfPeople;
     public static int height;
-    public static LadderBuilder ladderBuilder = new LadderBuilder();
 
     public static void main(String[] args) {
         getInput();
-        Ladder ladder = ladderBuilder.createLadder(height, numPeople);
+        Ladder ladder = new Ladder(height, numOfPeople);
         printLadder(ladder);
     }
 
     public static void getInput() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("참여할 사람은 몇 명인가요?");
-        numPeople = scanner.nextInt();
+        numOfPeople = scanner.nextInt();
         System.out.println("최대 사다리 높이는 몇 개인가요?");
         height = scanner.nextInt();
     }
