d6ff15b81da0a8715e3b116db32cb347158fa23b
frank.burger <frank.burger@kakaocorp.com>
2021-12-29 22:22:03 +0900

    refactor: improve naming of Ladder symbols

--- src/Ladder.java
+++ src/Ladder.java
@@ -8,7 +8,8 @@ public class Ladder {
     private static final String STEP = "-".repeat(STEP_WIDTH);
     private static final String NO_STEP = " ".repeat(STEP_WIDTH);
     private static final String BEAM = "|";
-    private static final String LEADING_SPACE = " ".repeat(STEP_WIDTH - 1);
+    private static final String PLAYER_NAME_LINE_LEADING_SPACE = " ".repeat(STEP_WIDTH / 2);
+    private static final String LADDER_LINE_LEADING_SPACE = " ".repeat(STEP_WIDTH - 1);
 
     private static final Random random = new Random();
 
@@ -26,7 +27,7 @@ public class Ladder {
     }
 
     private String makeRandomLine() {
-        StringBuilder line = new StringBuilder(LEADING_SPACE + BEAM);
+        StringBuilder line = new StringBuilder(LADDER_LINE_LEADING_SPACE + BEAM);
         boolean isLastBeamAvailable = true;
 
         for (int beamNo = 0; beamNo < width - 1; beamNo++) {
@@ -49,8 +50,8 @@ public class Ladder {
 
     private static String getPaddedPlayerName(Player player) {
         if (player.name.length() > STEP_WIDTH) {
-            // Should not happen
-            // Throws Exception?
+            // This should not happen.
+            // What to do here? Throws Exception?
         }
         int totalSpaceCount = STEP_WIDTH - player.name.length();
         int leftSpaceCount = totalSpaceCount / 2;
@@ -59,7 +60,7 @@ public class Ladder {
     }
 
     public String toPrettyString() {
-        String playersName = " ".repeat(STEP_WIDTH / 2) + String.join(" ", players.stream().map(player -> getPaddedPlayerName(player)).toList());
+        String playersName = PLAYER_NAME_LINE_LEADING_SPACE + String.join(" ", players.stream().map(player -> getPaddedPlayerName(player)).toList());
         String ladderShape = String.join(System.lineSeparator(), stateAsListOfString);
         return playersName + System.lineSeparator() + ladderShape;
     }
