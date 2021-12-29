d205eeeee55996d7091885e36da5685d9e6a5fb5
bb0711 <bb0711@kaist.ac.kr>
2021-12-29 16:29:59 +0900

    feat: added getPeopleNames() of IO class

--- src/IO.java
+++ src/IO.java
@@ -1,22 +1,40 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class IO {
-
+    private static final String qPeopleNames = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String qNumPeople = "참여할 사람은 몇 명인가요?";
     private static final String qMaxHeight = "최대 사다리 높이는 몇 개인가요?";
 
     public IO() {
     }
 
-    public static LadderConfig getInput(){
+    public LadderConfig getInput(){
         Scanner scanner = new Scanner(System.in);
+
+        String[] peopleNames = this.getPeopleNames(scanner);
+        int nPeople = this.getNumPeople(scanner);
+        int maxHeight = this.getMaxHeight(scanner);
+
+        LadderConfig ladderConfig = new LadderConfig(nPeople, maxHeight, peopleNames);
+        return ladderConfig;
+    }
+    public String[] getPeopleNames(Scanner scanner) {
+        System.out.println(qPeopleNames);
+        String strPeopleNames = scanner.next();
+        String[] peopleNames= strPeopleNames.split(",");
+
+        return peopleNames;
+    }
+    public int getNumPeople(Scanner scanner) {
         System.out.println(qNumPeople);
         int nPeople = scanner.nextInt();
+        return nPeople;
+    }
 
+    public int getMaxHeight(Scanner scanner) {
         System.out.println(qMaxHeight);
         int maxHeight = scanner.nextInt();
-
-        LadderConfig ladderConfig = new LadderConfig(nPeople, maxHeight);
-        return ladderConfig;
+        return maxHeight;
     }
 
     public static void printLadder(Ladder ladder) {
--- src/LadderConfig.java
+++ src/LadderConfig.java
@@ -1,10 +1,12 @@
 public class LadderConfig {
     private final int nPeople;
     private final int maxHeight;
+    private final String[] peopleNames;
 
-    public LadderConfig(int nPeople, int maxHeight) {
+    public LadderConfig(int nPeople, int maxHeight, String[] peopleNames) {
         this.nPeople = nPeople;
         this.maxHeight = maxHeight;
+        this.peopleNames = peopleNames;
     }
 
     public int getNumPeople(){
