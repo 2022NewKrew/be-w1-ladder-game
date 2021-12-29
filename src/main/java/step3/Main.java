f43dffad215e7a52063cf180455cd0d4f0062534
minkwon4 <minkwon4@naver.com>
2021-12-29 16:35:28 +0900

    프로젝트 구조 변경 및 사다리 게임 3단계 리팩토링

--- src/main/java/step3/Main.java
+++
@@ -1,9 +0,0 @@
-package step3;
-
-public class Main {
-	public static void main(String[] args) throws Exception {
-		LadderGame ladderGame = new LadderGame();
-		ladderGame.start();
-		ladderGame.printLadder();
-	}
-}
--- src/main/java/step3/Message.java
+++
@@ -1,9 +0,0 @@
-package step3;
-
-public class Message {
-	public static final String PLAYER_INPUT_MESSAGE = "참영할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
-
-	public static final String LADDER_HEIGHT_INPUT_MESSAGE = "최대 사다리 높이는 몇개인가요?";
-
-	public static final String LADDER_RESULT_MESSAGE = "실행결과";
-}
