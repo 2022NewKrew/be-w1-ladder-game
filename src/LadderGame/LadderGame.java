54eb61214abbd2984ece8485fc222a65710ee041
yuchanleeme <yuchanlee.me@gmail.com>
2021-12-29 11:38:42 +0900

    feat: 참여할 사람 이름 입력 받기
    - 참여할 사람 이름 입력 받는 메소드로 변경
    - 입력 부분 2개의 메소드로 분리

--- src/LadderGame/LadderGame.java
+++ src/LadderGame/LadderGame.java
@@ -1,24 +1,43 @@
 package LadderGame;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class LadderGame {
-    private int peopleNum, ladderHeight;
+    private List<String> playerNames;
+    private int ladderHeight, playerNum;
+    private final Scanner inputScanner;
+
+    public LadderGame() {
+        this.inputScanner =  new Scanner(System.in);
+    }
 
     private void inputData() {
-        Scanner inputScanner = new Scanner(System.in);
-        System.out.println("참여할 사람은 몇 명인가요?");
-        peopleNum = inputScanner.nextInt();
+        inputPlayerNames();
+        inputLadderHeight();
+        inputScanner.close();
+    }
 
+    private void inputPlayerNames(){
+        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉽표(,)로 구분하세요)");
+        String allPlayerNames = inputScanner.nextLine();
+        playerNames = Arrays.asList(allPlayerNames.split(","));
+        playerNum = playerNames.size();
+    }
+
+    private void inputLadderHeight(){
         System.out.println("최대 사다리 높이는 몇 개인가요?");
         ladderHeight = inputScanner.nextInt();
-
-        inputScanner.close();
     }
 
     public void startGame(){
         inputData();
-        Ladder ladder = new Ladder(peopleNum, ladderHeight);
+        for(String name: playerNames){
+            System.out.printf("%-6s", name);
+        }
+        Ladder ladder = new Ladder(playerNum, ladderHeight);
+        System.out.println();
         ladder.printLadder();
     }
 }
