43f9a70e18e89c9980b4d54c8bc952b5802208fc
Gunyoung Kim <60494603+Gunyoung-Kim@users.noreply.github.com>
2021-12-29 10:46:58 +0900

    Step2 - Gallix.kim  (#201)
    * 메소드 명 변경: GameStarter의 일부 메소드 직관적인 이름으로 변경
    
    * Extract Class: 표준 입출력을 담당하는 View class 추가
    GameStarter, Ladder 에서 표준 입출력 책임 분리
    
    * Typo 수정: NonInitializationException 에 대한 typo 수정
    
    * Class 추가 및 삭제: precondition 패키지 추가, NonInitializationException 삭제
    
    * Class 추가: Bridge, BridgeMakeStrategy
    Bridge: Ladder 에서 다리 역할을 하는 클래스
    BridgeMakeStrategy: Ladder 에 다리를 생성을 담당하는 인터페이스, 구현체 : RandomBridgeStrategy
    
    * getShapeOfLadderForEachRow for 문 for-each 문으로 대체
    
    * Bridge 생성자에 지역변수 선언 추가 for 직관성
    
    * todo 주석 작성
    
    * 모든 파일 포매팅 적용

--- src/main/java/com/gunyoung/one/boot/GameStarter.java
+++ src/main/java/com/gunyoung/one/boot/GameStarter.java
@@ -1,6 +1,7 @@
 package com.gunyoung.one.boot;
 
-import java.util.Scanner;
+import com.gunyoung.one.ladder.Ladder;
+import com.gunyoung.one.view.View;
 
 public class GameStarter {
 
@@ -8,41 +9,43 @@ public class GameStarter {
      * for Singleton
      */
     private static GameStarter INSTANCE;
-    private GameStarter() {}
+
+    private GameStarter() {
+    }
 
     public static GameStarter getInstance() {
-        if(INSTANCE == null)
+        if (INSTANCE == null)
             INSTANCE = new GameStarter();
         return INSTANCE;
     }
 
-    private final Scanner scanner = new Scanner(System.in);
+    private final View view = new View();
 
     /**
      * 게임을 시작한다
      */
     public void start() {
-        setDataForGame();
+        inputDataForLadder();
         drawLadder();
     }
 
-    private void setDataForGame() {
-        int numOfUser = takeNumberOfUser();
-        int ladderHeight = takeLadderHeight();
+    // todo : User class 추가 시 변경
+    private void inputDataForLadder() {
+        int numOfUser = inputNumberOfUser();
+        int ladderHeight = inputLadderHeight();
         Ladder.init(numOfUser, ladderHeight);
     }
 
-    private int takeNumberOfUser() {
-        System.out.println("참여할 사람은 몇 명인가요?");
-        return scanner.nextInt();
+    private int inputNumberOfUser() {
+        return view.inputIntWithMessage("참여할 사람은 몇 명인가요?");
     }
 
-    private int takeLadderHeight() {
-        System.out.println("최대 사다리 높이는 몇 개인가요?");
-        return scanner.nextInt();
+    private int inputLadderHeight() {
+        return view.inputIntWithMessage("최대 사다리 높이는 몇 개인가요?");
     }
 
     private void drawLadder() {
-        Ladder.getInstance().drawLadder();
+        String ladderFeature = Ladder.getInstance().getShapeOfLadder();
+        view.output(ladderFeature);
     }
 }
---
+++ src/main/java/com/gunyoung/one/view/View.java
@@ -0,0 +1,20 @@
+package com.gunyoung.one.view;
+
+import java.util.Scanner;
+
+public class View {
+    private final Scanner scanner;
+
+    public View() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public int inputIntWithMessage(String message) {
+        output(message);
+        return scanner.nextInt();
+    }
+
+    public void output(String str) {
+        System.out.println(str);
+    }
+}
