a848b26ad771fc7725713f61bed7c33241c80eae
eastclo <goldeastcloud@gmail.com>
2021-12-29 11:48:20 +0900

    3단계 구현 - Line 상수 형태 변경, 사다리 가로 라인이 겹치지 않게하는 로직 추가

--- src/Line.java
+++ src/Line.java
@@ -3,9 +3,9 @@ import java.util.List;
 import java.util.Random;
 
 public class Line {
-    private static final Character STRIPE = '|';
-    private static final Character BRIDGE = '-';
-    private static final Character SPACE = ' ';
+    private static final String STRIPE = "|";
+    private static final String BRIDGE = "-----";
+    private static final String SPACE = "     ";
 
     private final List<Boolean> line;
 
@@ -16,9 +16,16 @@ public class Line {
 
     private void drawLines(int width) {
         Random random = new Random();
+        //TODO:스트림 변경 가능?
         for(int i = 0; i < width - 1; i++) {
-            line.add(random.nextBoolean());
+            line.add(!isPrevBridge(i) && random.nextBoolean());
         }
+    }
+
+    private boolean isPrevBridge(int idx) {
+        if(idx == 0)
+            return false;
+        return line.get(idx-1);
     }
 
     @Override
