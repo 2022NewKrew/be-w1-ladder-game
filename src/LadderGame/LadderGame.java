33b146e5c7805442d8fb6c0d21c6358eb96ab527
yuchanleeme <yuchanlee.me@gmail.com>
2021-12-29 17:44:45 +0900

    feat: try-with-resources 적용
    - input 부분 try-with-resources 적용

--- src/LadderGame/LadderGame.java
+++ src/LadderGame/LadderGame.java
@@ -7,37 +7,40 @@ import java.util.Scanner;
 public class LadderGame {
     private List<String> playerNames;
     private int ladderHeight, playerNum;
-    private final Scanner inputScanner;
-
-    public LadderGame() {
-        this.inputScanner =  new Scanner(System.in);
-    }
 
     private void inputData() {
-        inputPlayerNames();
-        inputLadderHeight();
-        inputScanner.close();
+        try(Scanner inputScanner = new Scanner(System.in)){
+            inputPlayerNames(inputScanner);
+            inputLadderHeight(inputScanner);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
-    private void inputPlayerNames(){
+    private void inputPlayerNames(Scanner inputScanner){
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉽표(,)로 구분하세요)");
         String allPlayerNames = inputScanner.nextLine();
         playerNames = Arrays.asList(allPlayerNames.split(","));
         playerNum = playerNames.size();
     }
 
-    private void inputLadderHeight(){
+    private void inputLadderHeight(Scanner inputScanner){
         System.out.println("최대 사다리 높이는 몇 개인가요?");
         ladderHeight = inputScanner.nextInt();
     }
 
     public void startGame(){
         inputData();
+        printPlayerNames();
+
+        Ladder ladder = new Ladder(playerNum, ladderHeight);
+        ladder.printLadder();
+    }
+
+    private void printPlayerNames(){
         for(String name: playerNames){
             System.out.printf("%-6s", name);
         }
-        Ladder ladder = new Ladder(playerNum, ladderHeight);
         System.out.println();
-        ladder.printLadder();
     }
 }
