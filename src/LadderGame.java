ace52061977f98621cc55d32dfeb3d50d3cfd61d
onewns <wonjun9090@naver.com>
2021-12-29 17:30:14 +0900

    step3 참가자 이름을 기준으로 인풋받기

--- src/LadderGame.java
+++ src/LadderGame.java
@@ -6,16 +6,18 @@ public class LadderGame {
     public static void main(String[] args) {
 
         // 변수들을 먼저 정해놓고 시작
-        int row, col;
+        int height;
+        List<String> names;
+        Scanner sc = new Scanner(System.in);
 
         // 사용자의 입력을 받고 그에 따라 사다리의 가로 세로를 결정
-        Scanner sc = new Scanner(System.in);
-        System.out.println("참여할 사람은 몇 명인가요?");
-        col = sc.nextInt();
-        System.out.println("최대 사다리 높이는 몇 개인가요?");
-        row = sc.nextInt();
+        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+        names = List.of(sc.nextLine().split(","));
 
-        Ladder ladder = new Ladder(col, row);
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        height = sc.nextInt();
+
+        Ladder ladder = new Ladder(names, height);
         ladder.printLadder();
  }
 }
