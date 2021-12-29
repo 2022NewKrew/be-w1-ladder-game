3f7e8f0123b5a75e7bfcb8ba5c9362c3f9011853
benn.diagram <benn.diagram@kakaocorp.com>
2021-12-29 10:09:57 +0900

    실행 오류를 해결 못해서, 프로젝트를 다시 만듬

--- ladderGame/src/com/company/Main.java
+++ ladderGame/src/com/company/Main.java
@@ -1,22 +1,8 @@
 package com.company;
 
-import com.company.ladder.Ladder;
-
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("참여할 사람은 몇명인가요?");
-        int attendant_num = scanner.nextInt();
-
-        System.out.println("최대 사다리 높이는 몇 개인가요?");
-        int ladder_num = scanner.nextInt();
-
-        Ladder ladder = new Ladder(attendant_num,ladder_num);
-        ladder.makeRandomLadder();
-        ladder.print();
+	// write your code here
     }
 }
