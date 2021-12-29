0cbdc9947b9b218b4e0a2bde1e985849ea9ccdd5
geatrigger <53654547+geatrigger@users.noreply.github.com>
2021-12-29 22:08:13 +0900

    Step3 (#398)
    * Apply feedback(static, naming)
    
    * Write names on top of ladder
    
    * Change Array to ListArray and decrease depth
    
    Co-authored-by: geatrigger-kim <geatrigger.kim@kakaocorp.com>

--- src/main/java/com/gradle/geatrigger/Ladder.java
+++ src/main/java/com/gradle/geatrigger/Ladder.java
@@ -1,43 +1,52 @@
 package com.gradle.geatrigger;
 
-public class Ladder {
-    static int[][] ladder;
-    static int cnt, height;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-    public Ladder(int cnt, int height) {
-        this.cnt = cnt;
+public class Ladder {
+    ArrayList<Line> ladder;
+    // width, height의 경우 이미 Ladder 인스턴스에 속해있어 ladder 생략
+    int peopleCount, width, height;
+    ArrayList<String> names;
+
+    StringBuilder sb;
+
+    public Ladder(int peopleCount, int height, String namesString) {
+        this.peopleCount = peopleCount;
+        this.width = peopleCount - 1;
         this.height = height;
-        this.ladder = new int[height][cnt];
+        this.ladder = new ArrayList<Line>();
+        this.names = new ArrayList<String>(Arrays.asList(namesString.split(",")));
+        sb = new StringBuilder();
     }
 
     public void makeLadder() {
         for (int i = 0; i < height; i++) {
-            for (int j = 0; j < cnt - 1; j++) {
-                ladder[i][j] = (int) Math.round(Math.random());
-            }
-            ladder[i][cnt - 1] = 0;
+            Line line = new Line(width);
+            ladder.add(line);
         }
     }
 
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < cnt; j++) {
-                sb.append(makeVerticals());
-                sb.append(makeHorizons(ladder[i][j]));
-            }
-            sb.append("\n");
-        }
+        sb.setLength(0);
+        writeNames();
+        writeLadder();
         return sb.toString();
     }
 
-    private String makeHorizons(int cnt) {
-        if (cnt == 0)
-            return " ";
-        return "-".repeat(cnt);
+    private void writeLadder() {
+        for (int i = 0; i < height; i++) {
+            String lineString = ladder.get(i).writeLine();
+            sb.append(lineString);
+        }
     }
 
-    private String makeVerticals() {
-        return "|";
+    private void writeNames() {
+        for (String name : names) {
+            sb.append(" ".repeat(LadderConstant.INTERVAL / 2 - name.length() / 2));
+            sb.append(name);
+            sb.append(" ".repeat((LadderConstant.INTERVAL - LadderConstant.INTERVAL / 2) - (name.length() - name.length() / 2) + 1));
+        }
+        sb.append("\n");
     }
 }
---
+++ src/main/java/com/gradle/geatrigger/LadderConstant.java
@@ -0,0 +1,5 @@
+package com.gradle.geatrigger;
+
+public class LadderConstant {
+    final static int INTERVAL = 5;
+}
