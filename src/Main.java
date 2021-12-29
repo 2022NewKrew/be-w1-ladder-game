1ccfd3ddb8198d33aa410234c3e6e7abae6b6eb5
eastclo <goldeastcloud@gmail.com>
2021-12-29 17:09:23 +0900

    3단계 구현 - fix Main try-catch 구문을 없애 경량화, String배열대신 컬렉션 사용

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static int MAX_NAME_LENGTH = 5;
@@ -9,48 +6,36 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        String[] participants = inputParticipants(scan);
+        List<String> participants = inputParticipants(scan);
         int ladderHeight = inputLadderHeight(scan);
 
-        LadderGame game = new LadderGame(new ArrayList<>(Arrays.asList(participants)), new Ladder(participants.length, ladderHeight), new StandardOutDisplayLadder());
+        LadderGame game = new LadderGame(new ArrayList<>(participants), new Ladder(participants.size(), ladderHeight), new StandardOutDisplayLadder());
         game.displayLadder();
     }
 
-    public static String[] inputParticipants(Scanner scan) {
-        try {
-            System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요");
-            String[] participants = scan.nextLine().split(",");
+    public static List<String> inputParticipants(Scanner scan) {
+        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요");
+        List<String> participants = new ArrayList<>(Arrays.asList(scan.nextLine().split(",")));
 
-            if (Arrays.stream(participants).findAny().isEmpty() ||
-                   Arrays.stream(participants).anyMatch(s -> s.length() > MAX_NAME_LENGTH)) {
-                throw new InputMismatchException();
-            }
-
-            return participants;
-        } catch (InputMismatchException e) {
+        if (participants.isEmpty() || participants.stream().anyMatch(s -> s.length() > MAX_NAME_LENGTH)) {
             System.out.println("잘못 입력하셨습니다!");
             System.out.println("이름은 " + MAX_NAME_LENGTH + "자 이하여야 합니다.\n");
-
             return inputParticipants(scan);
         }
+
+        return participants;
     }
 
     public static int inputLadderHeight(Scanner scan) {
-        try {
-            System.out.println("최대 사다리 높이는 몇 개 인가요?");
-            int height = scan.nextInt();
+        System.out.println("최대 사다리 높이는 몇 개 인가요?");
+        String height = scan.nextLine();
 
-            if(height <= 0) {
-                throw new InputMismatchException();
-            }
-
-            return height;
-        } catch (InputMismatchException e) {
-            System.out.println("잘못 입력하셨습니다!");
-            System.out.println("양의 정수만 입력해주세요\n");
-            scan.nextLine();
-
-            return inputLadderHeight(scan);
+        if(height.chars().allMatch(Character::isDigit) && Integer.parseInt(height) > 0) {
+            return Integer.parseInt(height);
         }
+
+        System.out.println("잘못 입력하셨습니다!");
+        System.out.println("양의 정수만 입력해주세요\n");
+        return inputLadderHeight(scan);
     }
 }
