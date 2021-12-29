88a602b958512ddf1b8914597703e508affb0aca
jake.0208 <jake.0208@kakaocorp.com>
2021-12-29 13:22:34 +0900

    refactor: getInputNumber메소드 재귀 요소 제거

--- src/main/java/InputManager.java
+++ src/main/java/InputManager.java
@@ -5,22 +5,23 @@ public class InputManager {
 
     public StartInfo getStartInfo(){
         System.out.println("참여할 사람은 몇 명인가요?");
-        int numPeople = inputNumber();
+        int numPeople = getInputNumber();
 
         System.out.println("최대 사다리 높이는 몇 개인가요?");
-        int maxHeight = inputNumber();
+        int maxHeight = getInputNumber();
 
         return new StartInfo(numPeople, maxHeight);
     }
-    private int inputNumber(){
-        try{
-            int value = Integer.parseInt(sc.nextLine());
-            checkIntValue(value);
-            return value;
-        }
-        catch(NumberFormatException e){
-            System.out.println("양의 정수를 입력해주세요.");
-            return inputNumber();
+    private int getInputNumber(){
+        while(true){
+            try{
+                int value = Integer.parseInt(sc.nextLine());
+                checkIntValue(value);
+                return value;
+            }
+            catch(NumberFormatException e){
+                System.out.println("양의 정수를 입력해주세요.");
+            }
         }
     }
     private int checkIntValue(int value){
