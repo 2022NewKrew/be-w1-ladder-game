be27e1745dfe96c360690d872aa62b951aeb1234
roger.lee <2wjdwo97@gmail.com>
2021-12-29 13:03:10 +0900

    Create a class for managing system input
    create exception class
    move user input function from main to PositiveScanner class

--- ladder/src/main/java/LadderMain.java
+++ ladder/src/main/java/LadderMain.java
@@ -2,12 +2,8 @@ import java.util.Scanner;
 
 public class LadderMain {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("참여할 사람은 몇명인가요?");
-        int numOfParticipants = sc.nextInt();
-
-        System.out.println("최대 사다리 높이는 몇 개인가요?");
-        int numOfLadderHeight = sc.nextInt();
+        int numOfParticipants = PositiveIntScanner.getPositiveInt("참여할 사람은 몇명인가요?");
+        int numOfLadderHeight = PositiveIntScanner.getPositiveInt("최대 사다리 높이는 몇 개인가요?");
 
         Ladder ladder = new Ladder(numOfParticipants, numOfLadderHeight);
         LadderPrinter.print(ladder);
