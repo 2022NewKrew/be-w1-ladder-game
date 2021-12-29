803c713c6237066d9e7b0f952d0ecfd6d199afc1
jb.jang <jb.jang@kakaocorp.com>
2021-12-29 17:33:11 +0900

    feat: 게임 참여 사람 이름을 받도록 변경
    요구사항에 맞게 참여하는 사람 이름을 받도록 코드가 변경되었습니다.
    - 입력을 담당하는 클래스 Input 이 추가되었습니다.
    - Ladder 클래스의 setLadderInfo, setLineList 메소드가 생성자로
      통합되었습니다.
    - Ladder 클래스의 생성자가 인자로 playerNames 와 ladderHeight 을
      받습니다.
    - LadderGame 클래스가 Ladder 객체를 생성할 때 Input 클래스의
      getPlayerNames, getLadderHeight 메소드를 호출해 결과값을 인자로
      넣어줍니다.

--- src/main/java/LadderGame.java
+++ src/main/java/LadderGame.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class LadderGame {
     public static void main(String[] args) {
-        Ladder ld = new Ladder();
+        Ladder ld = new Ladder(Input.getPlayerNames(), Input.getLadderHeight());
         ld.printLadder();
     }
 }
