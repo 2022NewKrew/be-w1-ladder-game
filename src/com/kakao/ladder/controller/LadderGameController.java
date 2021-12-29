68d170498b1934e252fd05f41b4775f85d5aa5dc
chanSeong95 <62419145+chanSeong95@users.noreply.github.com>
2021-12-29 22:07:28 +0900

    사다리 타기 step3 구현 (#353)
    * step5 까지의 기능은 구현하였고 MVC 패턴을 적용해 보았습니다
    
    * 종료 조건 및 전체 출력 문자열 상수화
    
    Co-authored-by: brody.moon <brody.moon@kakaocorp.com>

---
+++ src/com/kakao/ladder/controller/LadderGameController.java
@@ -0,0 +1,69 @@
+package com.kakao.ladder.controller;
+
+import com.kakao.ladder.model.Ladder;
+import com.kakao.ladder.view.UserInput;
+
+/**
+ * author    : brody.moon
+ * version   : 1.0
+ * 사다리와 입력받는 함수를 연결해주는 컨트롤러입니다. MVC 패턴으로 짜보려고 도입해보았는데 많이 부족한 것 같습니다.
+ */
+public class LadderGameController {
+    /**
+     * ladder   컨트롤러에 연결된 사다리 객체입니다.
+     */
+    public static final String END_CONDITION = "춘식이";
+    public static final String ALL_PRINT_CONDITION = "all";
+    private final Ladder ladder;
+
+    public LadderGameController() {
+        ladder = new Ladder(UserInput.getInputInofs());
+    }
+
+    /**
+     * 사다리 모양을 보여주고 결과에 대해 알려주는 로직을 컨트롤 하는 메서드입니다.
+     */
+    public void start() {
+        printLadderImage();
+
+        UserInput.bufferClear();
+
+        resultCheck();
+    }
+
+    /**
+     * 사다리 클래스에서 현재 설정된 이름들을 받아와서 UserInput 클래스로 넘겨주고 정상 입력된 문자열을 받아서 처리해주는 메서드입니다.
+     */
+    private void resultCheck() {
+        String checkName = "";
+        while (!checkName.equals(END_CONDITION)) {
+            checkName = UserInput.showResult(ladder.getNames());
+
+            isAllOrName(checkName);
+        }
+    }
+
+    /**
+     * 입력된 문자열이 all 인지 춘식이 인지 특정 이름인지에 따라 따로 처리해주는 메서드입니다.
+     *
+     * @param name 입력된 문자열 ( 유저 이름 or all or 춘식이 )
+     */
+    private void isAllOrName(String name) {
+        if (name.equals(END_CONDITION))
+            return;
+
+        if (name.equals(ALL_PRINT_CONDITION)) {
+            ladder.printAllResult();
+            return;
+        }
+
+        ladder.printResult(name);
+    }
+
+    /**
+     * 사다리 클래스의 printLadder 메서드를 최대한 숨기기 위해 따로 메서드를 만들었습니다.
+     */
+    public void printLadderImage() {
+        ladder.printLadder();
+    }
+}
