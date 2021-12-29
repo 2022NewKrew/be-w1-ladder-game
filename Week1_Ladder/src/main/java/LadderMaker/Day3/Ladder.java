333ca96d5eee63bd5f4eda4afca6a9a59fe44c8a
eroica.b <eroica.b@kakaocorp.com>
2021-12-29 13:11:55 +0900

    use Line as hint

--- Week1_Ladder/src/main/java/LadderMaker/Day3/Ladder.java
+++ Week1_Ladder/src/main/java/LadderMaker/Day3/Ladder.java
@@ -6,7 +6,7 @@ import java.util.stream.Stream;
 public class Ladder {
     private final int heightOfLadder;
     private final int numOfPeople;
-    private final ArrayList<ArrayList<Boolean>> connectedInfo;
+    private final ArrayList<Line> connectedInfo;
     private final ArrayList<String> nameOfPeople;
 
     public Ladder(int heightOfLadder, ArrayList<String> nameOfPeople){
@@ -15,19 +15,14 @@ public class Ladder {
         this.numOfPeople = nameOfPeople.size();
 
         connectedInfo = new ArrayList<>(heightOfLadder);
-        makeSubArrayList(connectedInfo);
     }
 
-    public <T> void makeSubArrayList(ArrayList<ArrayList<T>> arrayList){
-        for(int i = 0; i < heightOfLadder; i++){ arrayList.add(new ArrayList<>(numOfPeople)); }
-    }
-
-    public void addConnectedInfo(int row, Boolean data) { connectedInfo.get(row).add(data); }
+    public void addConnectedInfo(Line line) { connectedInfo.add(line); }
     //public void setConnectedInfo(int row, int column, Boolean data){ connectedInfo.get(row).set(column, data); }
-    public boolean getConnectedInfo(int row, int column){ return connectedInfo.get(row).get(column); }
+    //public boolean getConnectedInfo(int row, int column){ return connectedInfo.get(row).getElement(column); }
     //public String getNameOfPeople(int idx){ return nameOfPeople.get(idx); }
     public int getNumOfPeople() { return numOfPeople; }
     public int getHeightOfLadder(){ return heightOfLadder; }
     public Stream<String> getNameStream(){ return nameOfPeople.stream(); }
-    public Stream<Boolean> getConnectedStream(int row) { return connectedInfo.get(row).stream(); }
+    public Stream<Boolean> getConnectedStream(int row) { return connectedInfo.get(row).getLineStream(); }
 }
--- Week1_Ladder/src/main/java/LadderMaker/Day3/LadderMaker.java
+++ Week1_Ladder/src/main/java/LadderMaker/Day3/LadderMaker.java
@@ -8,17 +8,17 @@ public class LadderMaker {
 
     public void makeLadder(Ladder ladder){
         for(int row = 0; row < ladder.getHeightOfLadder(); row++) {
-            makeRow(ladder, row);
+            makeRow(ladder);
         }
     }
 
-    public void makeRow(Ladder ladder, int row){
-        ladder.addConnectedInfo(row, randomManager.getRandomBoolean());
+    public void makeRow(Ladder ladder){
+        Line line = new Line();
+        line.addElement(randomManager.getRandomBoolean());
 
         for(int column = 1; column < ladder.getNumOfPeople() - 1; column++){
-            ladder.addConnectedInfo(row,
-                    (!ladder.getConnectedInfo(row, column - 1) && randomManager.getRandomBoolean())
-            );
+            line.addElement(!line.getElement(column - 1) && randomManager.getRandomBoolean());
         }
+        ladder.addConnectedInfo(line);
     }
 }
