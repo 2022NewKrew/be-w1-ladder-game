f481fd24dcf84ef30278388f5ebd5fd499081628
MinboShim <80995486+tlaalsqh7414@users.noreply.github.com>
2021-12-29 10:47:20 +0900

    사다리게임 step2 구현 (#238)
    Co-authored-by: = <=>

--- app/src/main/java/ladderGame/LadderGame.java
+++ app/src/main/java/ladderGame/LadderGame.java
@@ -3,58 +3,25 @@
  */
 package ladderGame;
 
-import java.util.*;
+import java.util.Scanner;
 
 public class LadderGame {
-    static int numberOfUser; // 참여자 수
-    static int height;       // 사다리 높이
-    static List<List<String>> ladder = null;
-
     public static void main(String[] args) {
-        getInput();
-        makeRandomLadder();
-        printLadder();
+        Ladder ladder = new Ladder(getInput());
+        ladder.print();
     }
 
-    static void getInput() {
+    static int[] getInput() {
+        int[] args = new int[2];
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("참여할 사람은 몇 명인가요?");
-        numberOfUser = scanner.nextInt();
+        args[0] = scanner.nextInt();
 
         System.out.println("최대 사다리 높이는 몇 개인가요?");
-        height = scanner.nextInt();
+        args[1] = scanner.nextInt();
 
         scanner.close();
-    }
-
-    static void makeRandomLadder() { // 랜덤으로 사다리를 생성해서 ladder에 정보 저장
-        if(ladder == null) ladder = new ArrayList(height);
-        for(int i = 0; i < height; i++) {
-            makeRow();
-        }
-    }
-
-    static void makeRow() {
-        List<String> temp = new ArrayList(numberOfUser - 1);
-        for(int i = 0; i < numberOfUser - 1; i++) {
-            if(getRandomBool()) {
-                temp.add("-");
-            } else {
-                temp.add(" ");
-            }
-        }
-        ladder.add(temp);
-    }
-
-    static boolean getRandomBool() { return Math.random() < 0.5; }
-
-    static void printLadder() {
-        for(int i = 0; i < height; i++) {
-            for(int j = 0; j < numberOfUser - 1; j++) {
-                System.out.print("|");
-                System.out.print(ladder.get(i).get(j));
-            }
-            System.out.println("|");
-        }
+        return args;
     }
 }
