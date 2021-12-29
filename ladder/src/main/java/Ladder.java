aea90135c8e355358677b89b63e81a59ed807d56
2wjdwo97 <46587207+2wjdwo97@users.noreply.github.com>
2021-12-29 10:09:25 +0900

    Feature/step 2 (#183)
    * Implement ladder first step
    
    * *Modify two depth indent
    
    detach create row method
    
    * *Add duplicate check function
    
    change List<List<boolean>> to Boolean[][]
    Implement isValid() function
    
    * *Apply pr comment
    
    make Random instance an attribute of ladder class
    extract printRow function from printShape function
    change bar mapping function
    call build function when the constructor is called

--- ladder/src/main/java/Ladder.java
+++ ladder/src/main/java/Ladder.java
@@ -1,40 +1,51 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Ladder{
-    private final List<List<Boolean>> ladderShape = new ArrayList<>();
+    // 연결되는 곳 true, 아닌 곳 false 로 저장
+    private final Boolean[][] ladderShape;
     private final int numOfParticipants;
     private final int numOfLadderHeight;
+    private final Random random;
 
     public Ladder(int numOfParticipants, int numOfLadderHeight){
         this.numOfParticipants = numOfParticipants;
         this.numOfLadderHeight = numOfLadderHeight;
+
+        ladderShape = new Boolean[numOfLadderHeight][numOfParticipants - 1];
+        random = new Random();
+
+        build();
     }
 
-    public void build(){
-        Random random = new Random(System.currentTimeMillis());
-        List<Boolean> newRow;
+    private void build(){
+        for (int row = 0; row < numOfLadderHeight; row++)
+            createRow(row);
+    }
 
-        for (int row = 0; row < numOfLadderHeight; row++) {
-            newRow = new ArrayList<>();
-            for (int bar = 0; bar < numOfParticipants - 1; bar++)
-                newRow.add(random.nextBoolean());
-            ladderShape.add(newRow);
-        }
+    private void createRow(int row){
+        for (int bar = 0; bar < numOfParticipants - 1; bar++)
+            ladderShape[row][bar] = isAlreadyConnected(row, bar) && random.nextBoolean();
+    }
+
+    private boolean isAlreadyConnected(int row, int bar){
+        if (bar == 0)
+            return true;
+        return !ladderShape[row][bar - 1];
     }
 
     public void printShape(){
-        ladderShape.forEach(
-                row -> {
-                    row.stream()
-                            .map(this::mapBar)
-                            .forEach(bar -> System.out.print("|" + bar));
-                    System.out.println("|");
-                });
+        Arrays.stream(ladderShape).forEach(this::printRow);
+    }
+
+    private void printRow(Boolean[] row){
+        Arrays.stream(row)
+                .map(this::mapBar)
+                .forEach(System.out::print);
+        System.out.println("|");
     }
 
     private String mapBar(Boolean barExists){
-        return barExists? " ": "-";
+        return barExists? "|-": "| ";
     }
 }
