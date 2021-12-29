de2a7d7cbae556c30f18792a16dc6835b2d6bbef
minkwon4 <minkwon4@naver.com>
2021-12-29 13:30:22 +0900

    사다리게임 3단계 구현

---
+++ src/main/java/step3/LadderLineStyle.java
@@ -0,0 +1,18 @@
+package step3;
+
+public enum LadderLineStyle {
+	HORIZONTAL_LINE("-----"),
+	HORIZONTAL_EMPTY("     "),
+	VERTICAL_LINE("|"),
+	EMPTY("");
+
+	private String shape;
+
+	LadderLineStyle(String shape) {
+		this.shape = shape;
+	}
+
+	public String getShape() {
+		return shape;
+	}
+}
