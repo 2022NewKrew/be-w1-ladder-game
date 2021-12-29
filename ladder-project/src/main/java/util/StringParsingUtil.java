ad69321a1f4a7b7525936f5a03c5392d02117fc5
edward.baek <edward.baek@kakaocorp.com>
2021-12-29 18:21:36 +0900

    feat : 이름을 파싱해주는 유틸 클래스 생성

---
+++ ladder-project/src/main/java/util/StringParsingUtil.java
@@ -0,0 +1,16 @@
+package util;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public final class StringParsingUtil {
+
+    private StringParsingUtil() { }
+
+    public static List<String> parse(String stringList, String split) {
+        String[] list = stringList.split(split);
+        return Arrays.stream(list)
+                .collect(Collectors.toList());
+    }
+}
