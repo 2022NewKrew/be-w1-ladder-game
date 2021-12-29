60c1922fac5da30a18ec84ceead43312e62a4bea
trevi.kim <trevi.kim@kakaocorp.com>
2021-12-30 00:16:48 +0900

    - refactor : Blank 로직 String.format 으로 변경

--- src/main/java/game/PlayerRow.java
+++ src/main/java/game/PlayerRow.java
@@ -14,20 +14,12 @@ public class PlayerRow{
     @Override
     public String toString() {
         StringBuilder stringBuilder = new StringBuilder();
-        for(int i = 0; i<playerList.size();i++)
+        String stringFormat = "%" + (typeSize +1) + "s";
+        for(String player : playerList)
         {
-            int blankLength = getBlankLength(i);
-            stringBuilder.append(" ".repeat(blankLength));
-            stringBuilder.append(playerList.get(i));
+            stringBuilder.append(String.format(stringFormat,player));
         }
         return stringBuilder.toString();
     }
 
-    private int getBlankLength(int i) {
-        String element = (String) playerList.get(i);
-        int blankLength = typeSize - element.length()/2;
-        if(i> 0)
-            blankLength -= (((String) playerList.get(i-1)).length()-1)/2;
-        return blankLength;
-    }
 }
