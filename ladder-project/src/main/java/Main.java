47a4cb405084f26fcc43699952d5ad0fd61ddef4
edward.baek <edward.baek@kakaocorp.com>
2021-12-29 13:51:18 +0900

    refactor : 메인에 존재하던 view 로직을 제거하였다

--- ladder-project/src/main/java/Main.java
+++ ladder-project/src/main/java/Main.java
@@ -1,24 +1,14 @@
-import domain.LadderGame;
-import domain.Participant;
+import controller.LadderGame;
 
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
-            int participantNumber = inputFromScreen("참여할 사람은 몇 명인가요? (1 명 이상 입력해주세요)", sc);
-            int height = inputFromScreen("최대 사다리 높이는 몇 개인가요?", sc);
-
-            Participant participant = new Participant(participantNumber);
-            LadderGame game = new LadderGame(participant, height);
-
+            LadderGame game = new LadderGame(sc);
             game.play();
         }
     }
 
-    //이 메서드가 (입력/출력) 2가지 일을 한다고 봐야할지 하나의 일을 한다고 봐야할지 잘 모르겠습니다..
-    public static int inputFromScreen(String message, Scanner scanner) {
-        System.out.println(message);
-        return scanner.nextInt();
-    }
 }
