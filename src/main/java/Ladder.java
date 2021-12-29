3a8c6c1bd841b006465d2309cf74edb628fc37cd
wldusdhso <wldusdhso@kookmin.ac.kr>
2021-12-29 10:47:59 +0900

    step2 완료 (#289)
    Co-authored-by: meg.j <meg.j@kakaocorp.com>

--- src/main/java/Ladder.java
+++ src/main/java/Ladder.java
@@ -4,22 +4,24 @@ public class Ladder {
     private final int numOfPeople;
     private final int height;
     private final String[][] ladder;
+    private Random random = new Random();
 
     Ladder(int numOfPeople, int height) {
         this.numOfPeople = numOfPeople;
         this.height = height;
-        this.ladder = new String[height][numOfPeople*2-1];
+        this.ladder = new String[height][numOfPeople-1];
         setLadder();
     }
 
     private void setLadder() {
-        Random random = new Random();
         for(int i = 0; i < height; i++) {
-            ladder[i][0] = "|";
-            for(int j = 1; j < numOfPeople*2-1; j+=2) {
-                ladder[i][j] = random.nextBoolean() ? "-" : " ";
-                ladder[i][j+1] = "|";
-            }
+            setLadderLine(i);
+        }
+    }
+
+    private void setLadderLine(int row) {
+        for(int j = 0; j < numOfPeople-1; j++) {
+            ladder[row][j] = random.nextBoolean() ? "-" : " ";
         }
     }
 
