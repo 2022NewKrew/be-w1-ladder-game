058011fa3cdfb59d01d127ac555dc478c4c76ad9
jeff.42 <jeff.42@kakaocorp.com>
2021-12-29 10:47:46 +0900

    test

--- src/main/java/input/CommandLineInputController.java
+++ src/main/java/input/CommandLineInputController.java
@@ -19,7 +19,7 @@ public class CommandLineInputController implements InputController {
 
     private int typeParticipants(Scanner sc) {
         try {
-            System.out.println("참여할 사람은 몇 명인가요?");
+            System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
             int numberOfParticipants = sc.nextInt();
             checkPositiveInteger(numberOfParticipants);
             return numberOfParticipants;
