712aacbcaf208f73721e98b7a01cb475b992c949
punsoo <sonu.sohn@kakaocorp.com@gmail.com>
2021-12-30 02:58:32 +0900

    feat:LadderGenerator 클래스 구현

---
+++ src/main/java/LadderGenerator.java
@@ -0,0 +1,29 @@
+package main.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LadderGenerator {
+    private static final int MIN_HEIGHT = 1;
+    private static final String MESSAGE_MIN_HEIGHT = "사다리 높이는 1이상이어야 합니다.";
+    private LineGenerator lineGenerator;
+
+    public LadderGenerator(){
+        this.lineGenerator = new LineGenerator();
+    }
+
+    private void validate(int height) {
+        if (height < MIN_HEIGHT) {
+            throw new IllegalArgumentException(MESSAGE_MIN_HEIGHT);
+        }
+    }
+
+    public Ladder generateLadder(int numberOfPlayers, int height) {
+        validate(height);
+        List<Line> lines = new ArrayList<>();
+        for(int i=0; i<height; i++){
+            lines.add(lineGenerator.generateLine(numberOfPlayers));
+        }
+        return new Ladder(lines);
+    }
+}
