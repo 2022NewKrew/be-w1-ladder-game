8cab1b205635e8c78acc56bc1cdbb7949d8f3177
edward.baek <edward.baek@kakaocorp.com>
2021-12-29 18:18:09 +0900

    refactor : Ladder 클래스 리팩토링
    - 이차원 배열을 사용하던 부분을 이중리스트로 변경
    - 라인이 겹쳐서 생성되는 것을 방지하는 로직 추가

--- ladder-project/src/main/java/domain/Ladder.java
+++ ladder-project/src/main/java/domain/Ladder.java
@@ -2,27 +2,57 @@ package domain;
 
 import util.RandomUtil;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ladder {
 
     private final int line;
-    private final int[][] ladder;
+    private final List<List<Boolean>> ladder;
 
     public Ladder(int column, int height) {
-        this.ladder = new int[height][column - 1];
+        this.ladder = createLadderList(column-1, height);
         this.line = generateRandomLine();
         assert this.line > -1;
         initLadder();
     }
 
+    private List<List<Boolean>> createLadderList(int column, int height) {
+        List<List<Boolean>> ladderList = new ArrayList<>();
+        for(int i = 0; i < height; i++) {
+            ladderList.add(createLadderColumnList(column));
+        }
+        return ladderList;
+    }
+
+    private List<Boolean> createLadderColumnList(int column) {
+        List<Boolean> ladderColumnList = new ArrayList<>();
+        for(int i = 0; i < column; i++) {
+            ladderColumnList.add(false);
+        }
+        return ladderColumnList;
+    }
+
     private void initLadder() {
         for (int i = 0; i < line; i++) {
-            int rowPosition = RandomUtil.generateSpecificRange(ladder.length);
-            int columnPosition = RandomUtil.generateSpecificRange(ladder[0].length);
-            ladder[rowPosition][columnPosition] = 1;
+            int rowPosition = RandomUtil.generateSpecificRange(ladder.size());
+            int columnPosition = RandomUtil.generateSpecificRange(ladder.get(0).size());
+            boolean result = checkLinePositionValidate(rowPosition, columnPosition);
+            ladder.get(rowPosition).set(columnPosition, result);
         }
     }
 
-    public int[][] getLadder() {
+    private boolean checkLinePositionValidate(int rowPosition, int columnPosition) {
+        if(columnPosition - 1 >= 0 && ladder.get(rowPosition).get(columnPosition-1)) {
+            return false;
+        }
+        if(columnPosition + 1 < ladder.get(0).size() && ladder.get(rowPosition).get(columnPosition+1)) {
+            return false;
+        }
+        return true;
+    }
+
+    public List<List<Boolean>> getLadder() {
         return ladder;
     }
 
@@ -31,6 +61,6 @@ public class Ladder {
     }
 
     private int getLadderSize() {
-        return ladder.length * ladder[0].length;
+        return ladder.size() * ladder.get(0).size();
     }
 }
