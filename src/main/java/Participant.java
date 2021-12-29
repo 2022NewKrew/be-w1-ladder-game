1364666a916168ec57c244d36607abbb24701f31
sukstar76 <sukstar76@gmail.com>
2021-12-29 14:42:36 +0900

    Participant 추가
    참가자들을 추가하기위해 클래스를 만들었습니다.

---
+++ src/main/java/Participant.java
@@ -0,0 +1,11 @@
+public class Participant {
+    private final String name;
+
+    public Participant(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
