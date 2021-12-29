6804b2937accdb4e663a4774039fdfe0c036266a
DongHyun Kim <skeldtcan@daum.net>
2021-12-29 10:48:11 +0900

    Step3 (#302)
    * [refactor] 사다리 생성, 프린트 함수 이동
    
    * [refactor] 필요 없는 import 제거
    
    * [refactor] depth 축소

--- src/LadderGame/Ladder.java
+++ src/LadderGame/Ladder.java
@@ -1,5 +1,7 @@
 package LadderGame;
 
+import java.util.Random;
+
 public class Ladder {
     private int height;
     private int width;
@@ -9,6 +11,50 @@ public class Ladder {
         this.height = height;
         this.width = width;
         this.ladder = new char[height][width];
+    }
+
+    void makeLadder() {
+        Random rand = new Random();
+
+        for (int i=0; i<height; i++) {
+            makeRow(i);
+            ladder[i][0] = '|';
+            int bridge = rand.nextInt(width/2);
+        }
+    }
+
+    void makeRow(int i) {
+        Random rand = new Random();
+        int bridge = rand.nextInt(width/2);
+
+        ladder[i][0] = '|';
+        for (int j=1; j<width; j++) {
+            ladder[i][j] = makeBridge(bridge, j++);
+            ladder[i][j] = '|';
+        }
+    }
+
+    char makeBridge(int bridge, int j) {
+        if (bridge == j/2) {
+            return '-';
+        }
+        return ' ';
+    }
+
+    void printLadder(){
+        StringBuilder output = new StringBuilder();
+
+        for (int i=0; i<height; i++) {
+            setLadderRow(output, i);
+            output.append("\n");
+        }
+        System.out.println(output);
+    }
+
+    void setLadderRow(StringBuilder output, int i) {
+        for (int j=0; j<width; j++) {
+            output.append(ladder[i][j]);
+        }
     }
 
     public int getHeight() {
