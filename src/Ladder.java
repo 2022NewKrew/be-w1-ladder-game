9006515c6567025ed4a89fca359eecd7c8a5d5ec
vince.joe <seonghyeoncho96@gmail.com>
2021-12-29 15:37:03 +0900

    refactor: `ArrayList` -> `List`

--- src/Ladder.java
+++ src/Ladder.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Ladder {
-    private final ArrayList<LadderRow> rows = new ArrayList<>();
+    private final List<LadderRow> rows = new ArrayList<>();
 
     Ladder(int numOfPeople, int height) {
         for (int i = 0; i < height; ++i) {
