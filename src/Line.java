3ae971442cc9987da4e2818767c844cea28b2b58
bb0711 <bb0711@kaist.ac.kr>
2021-12-29 20:09:11 +0900

    fix: 한 줄에 최대 한개 사다리만 나오던 버그 수정 및 리팩토링

--- src/Line.java
+++ src/Line.java
@@ -9,18 +9,10 @@ public class Line {
 
     // step이 있을 수 있는 크기인 (numPeople - 1) 수 만큼 Line 길이 지정
     public Line (int numPeople) {
-        boolean check = false;
+        boolean prevStep = false;// if there was a step right before, prevStep = true
         for(int i=0; i<numPeople-1; i++){
-            // if there was a step right before, check = true
-            if (check) {
-                //if there were a step before, do not add any step.
-                this.buildStepAtLast(this.cells, stepNotExist);
-                continue;
-            }
-            check = this.buildStepsPerCell(this.cells);
-
+            prevStep = buildStep(prevStep, this.cells, stepNotExist);
         }
-
     }
     public ArrayList<Boolean> getCells(){
         return cells;
@@ -31,18 +23,23 @@ public class Line {
         float randomFloat = random.nextFloat();
 
         if (randomFloat <0.5) { // make each step by 50% probability
-            buildStepAtLast(cells, stepExist);
+            cells.add(stepExist);
             return true;
         }
-        buildStepAtLast(cells, stepNotExist);
+        cells.add(stepNotExist);;
         return false;
     }
 
-    // build step in the last position of line
-    public static void buildStepAtLast(ArrayList<Boolean> cells, boolean existOrNot) {
-        cells.add(existOrNot);
+
+    public boolean buildStep(boolean prevStep, ArrayList<Boolean> cells, boolean existOrNot) {
+        // build empty step if the previous step is exists in the same line.
+        if (prevStep){
+            cells.add(existOrNot);
+            return stepNotExist;
+        }
+        return this.buildStepsPerCell(this.cells);
+
     }
 
-    // 라인의 좌표 값에 step('-')이 있는지 유무를 판단하는 로직 추가
 
 }
