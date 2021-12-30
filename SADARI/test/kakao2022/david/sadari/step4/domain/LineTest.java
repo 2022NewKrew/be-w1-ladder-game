85a73443b8d2f4880e02767557a62d88fba40b1f
david.dh <david.dh@kakaocorp.com>
2021-12-30 18:37:59 +0900

    코드 재정렬

--- SADARI/test/kakao2022/david/sadari/step4/domain/LineTest.java
+++ SADARI/test/kakao2022/david/sadari/step4/domain/LineTest.java
@@ -12,18 +12,21 @@ class LineTest {
         line.isPrevLine = false;
         assertFalse(line.addPoint(false));
     }
+
     @Test
     void addPointFT() {
         Line line = new Line();
         line.isPrevLine = false;
         assertTrue(line.addPoint(true));
     }
+
     @Test
     void addPointTF() {
         Line line = new Line();
         line.isPrevLine = true;
         assertFalse(line.addPoint(false));
     }
+
     @Test
     void addPointTT() {
         Line line = new Line();
