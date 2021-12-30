9635258421783b36dcb9d992c20318845870a4c2
more.potens <more.potens@kakaocorp.com>
2021-12-30 16:27:43 +0900

    사다리 게임 4단계 구현 - 미사용 테스트 제거

--- src/test/java/ladder/domain/EntryTest.java
+++ src/test/java/ladder/domain/EntryTest.java
@@ -6,11 +6,6 @@ import org.junit.jupiter.api.Test;
 public class EntryTest {
 
     @Test
-    public void 선_그리기_테스트() {
-
-    }
-
-    @Test
     public void 참가자_이름_5자_테스트() {
         // given
         Entry testEntry = new Entry("Ladder", 5);
