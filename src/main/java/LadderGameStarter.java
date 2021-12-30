1d956b4b7c47f43eadf4a7b4e9d784e537abb3af
ben.ny <ben.ny@kakaocorp.com>
2021-12-30 13:59:48 +0900

    Refactor : stream 으로 코드 간결화

--- src/main/java/LadderGameStarter.java
+++ src/main/java/LadderGameStarter.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class LadderGameStarter {
@@ -19,15 +20,11 @@ public class LadderGameStarter {
     }
 
     public boolean isValid(String[] people){
-        for (String name : people){
-            if (name.length() > maxNameLength){
-                System.out.println("문자열의 길이는 " + maxNameLength + "이하여야 합니다.");
-                return false;
-            }
-            if (name.isEmpty()){
-                System.out.println("빈 문자열이 있습니다. 다시 입력해주세요.");
-                return false;
-            }
+        if(Arrays.stream(people).anyMatch(name -> (name.length() > maxNameLength) || name.isEmpty())){
+            System.out.println("아래의 조건을 충족하는 지 확인하고 다시 입력해주세요.");
+            System.out.println("- 문자열의 길이는 " + maxNameLength + "이하여야 합니다.");
+            System.out.println("- 쉼표 사이에 빈 문자열이 있으면 안됩니다.");
+            return false;
         }
         return true;
     }
