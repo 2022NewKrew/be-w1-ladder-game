a074f1569c03c2279505eef6e51c4159d872da4b
david.dh <david.dh@kakaocorp.com>
2021-12-30 18:36:52 +0900

    startGame 메소드 추가

--- SADARI/src/kakao2022/david/sadari/step4/LadderGame.java
+++ SADARI/src/kakao2022/david/sadari/step4/LadderGame.java
@@ -1,11 +1,21 @@
 package kakao2022.david.sadari.step4;
 
 import kakao2022.david.sadari.step4.domain.Ladder;
+import kakao2022.david.sadari.step4.view.LadderUI;
+
+import java.util.List;
 
 public class LadderGame {
 
+    public static void startGame() {
+        LadderUI ladderUI = new LadderUI();
+        List<String> peopleList = ladderUI.inputPeople();
+        int ladderHeight = ladderUI.inputLadderHeight();
+        Ladder ladder = new Ladder(peopleList, ladderHeight);
+        ladderUI.printLadder(ladder);
+    }
+
     public static void main(String[] args) {
-        Ladder ladder = new Ladder();
-        ladder.startGame();
+        LadderGame.startGame();
     }
 }
