017ffbecda6d74520d604bbe042d4c5eaf811630
vince.joe <seonghyeoncho96@gmail.com>
2021-12-30 12:43:47 +0900

    chore: gradle 프로젝트로 변경

--- src/main/java/vince/Ladder.java
+++ src/main/java/vince/Ladder.java
@@ -1,3 +1,5 @@
+package vince;
+
 import java.util.ArrayList;
 import java.util.List;
 
--- src/main/java/vince/Main.java
+++ src/main/java/vince/Main.java
@@ -1,3 +1,5 @@
+package vince;
+
 public class Main {
     public static void main(String[] args) {
         LadderGame game = new LadderGame();
