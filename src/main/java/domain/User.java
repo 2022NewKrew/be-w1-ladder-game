5c903f14b96c03ef78f10f04c58f845bbf79f469
binary.yun <binary.yun@kakaocorp.com>
2021-12-31 00:32:42 +0900

    feat: 유저의 현재 위치(행, 열)을 저장하는 필드 생성

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -2,11 +2,13 @@ package domain;
 
 public class User {
     private final String name;
-    private int location;
+    private int row;
+    private int col;
 
-    public User(String name, int location) {
+    public User(String name, int row, int col) {
         this.name = name;
-        this.location = location;
+        this.row = row;
+        this.col = col;
     }
 
     public String getName() {
