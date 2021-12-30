77f7fdee8dbaf9c59da97a67cf36c2e8f51b50b5
GoonLee <auteur.kor@gmail.com>
2021-12-30 11:55:54 +0900

    Apply code review: specify AAA pattern

--- src/test/java/domain/LadderFactoryTest.java
+++ src/test/java/domain/LadderFactoryTest.java
@@ -13,82 +13,100 @@ public class LadderFactoryTest {
     @Test
     @DisplayName("Ladder width check")
     void buildLadder_ValidWidth_HasOneLessHorizontalLines() {
+        // Arrange
         int width = 10;
         int height = 10;
 
         Ladder ladder = LadderFactory.buildLadder(width, height);
 
+        // Act
         int actualWidth = ladder.getLayers().get(0).getHorizontalLines().size();
 
+        // Assert
         assertThat(actualWidth).isEqualTo(width - 1);
     }
 
     @Test
     @DisplayName("Ladder height check")
     void buildLadder_ValidHeight_HasSameLayers() {
+        // Arrange
         int width = 10;
         int height = 10;
 
         Ladder ladder = LadderFactory.buildLadder(width, height);
 
+        // Act
         int actualWidth = ladder.getLayers().size();
 
+        // Assert
         assertThat(actualWidth).isEqualTo(height);
     }
 
     @Test
     @DisplayName("Divergence resolving check: TT")
     void resolveDivergence_TrueAndTrue_ReturnsFalse() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        // Arrange
         Method resolveDivergence = LadderFactory.class.getDeclaredMethod("resolveDivergence", boolean.class, boolean.class);
         resolveDivergence.setAccessible(true);
 
         boolean previous = true;
         boolean current = true;
 
+        // Act
         boolean resolvedBoolean = (Boolean) resolveDivergence.invoke(null, previous, current);
 
+        // Assert
         assertThat(resolvedBoolean).isFalse();
     }
 
     @Test
     @DisplayName("Divergence resolving check: TF")
     void resolveDivergence_TrueAndFalse_ReturnsFalse() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        // Arrange
         Method resolveDivergence = LadderFactory.class.getDeclaredMethod("resolveDivergence", boolean.class, boolean.class);
         resolveDivergence.setAccessible(true);
 
         boolean previous = true;
         boolean current = false;
 
+        // Act
         boolean resolvedBoolean = (Boolean) resolveDivergence.invoke(null, previous, current);
 
+        // Assert
         assertThat(resolvedBoolean).isFalse();
     }
 
     @Test
     @DisplayName("Divergence resolving check: FT")
     void resolveDivergence_FalseAndTrue_ReturnsTrue() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        // Arrange
         Method resolveDivergence = LadderFactory.class.getDeclaredMethod("resolveDivergence", boolean.class, boolean.class);
         resolveDivergence.setAccessible(true);
 
         boolean previous = false;
         boolean current = true;
 
+        // Act
         boolean resolvedBoolean = (Boolean) resolveDivergence.invoke(null, previous, current);
 
+        // Assert
         assertThat(resolvedBoolean).isTrue();
     }
 
     @Test
     @DisplayName("Divergence resolving check: FF")
     void resolveDivergence_FalseAndFalse_ReturnsFalse() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        // Arrange
         Method resolveDivergence = LadderFactory.class.getDeclaredMethod("resolveDivergence", boolean.class, boolean.class);
         resolveDivergence.setAccessible(true);
 
         boolean previous = false;
         boolean current = false;
 
+        // Act
         boolean resolvedBoolean = (Boolean) resolveDivergence.invoke(null, previous, current);
 
+        // Assert
         assertThat(resolvedBoolean).isFalse();
     }
 }
