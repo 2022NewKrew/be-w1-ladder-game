46b060bcb2c2990c35d318eeab75aa94c112fa45
jun.dev <jun.dev@kakaocorp.com>
2021-12-30 14:55:34 +0900

    test: 연속적인 사다리 선 제거 결과 테스트코드 추가

--- src/main/java/ladder/domain/LadderTest.java
+++ src/main/java/ladder/domain/LadderTest.java
@@ -17,4 +17,24 @@ public class LadderTest {
 
         assertTrue(ladderObject.getPeopleCount() == 5);
     }
+
+    @Test
+    void 연속적인_사다리_선_제거_테스트(){
+        Ladder ladderObject = new Ladder();
+        String nameAry = "a,ab,abc,abcd,abcde,a,b,c,d,e,a,b,c,d,e";
+        Long height = Long.valueOf(100000); //테스트를 위해 높은 height로 지정
+        ladderObject.setValue(nameAry, height);
+        ladderObject.initLine();
+        ladderObject.shuffle();
+
+
+        Line[] lineStatus = ladderObject.getLineStatus();
+
+        for(int i = 0 ; i < height ; i++){
+            for(int j = 0 ; j < lineStatus[i].value.size() - 1 ; j++){
+                assertTrue(lineStatus[i].value.get(j) + 1 != lineStatus[i].value.get(j+1));
+            }
+        }
+
+    }
 }
