dce825fe325370b92df831a11a592837b0fb1b65
GoonLee <auteur.kor@gmail.com>
2021-12-30 11:27:14 +0900

    Apply advice: use unmodifiable list

--- src/main/java/domain/Ladder.java
+++ src/main/java/domain/Ladder.java
@@ -1,12 +1,13 @@
 package domain;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Ladder {
     private final List<Layer> layers;
 
     public Ladder(List<Layer> layers) {
-        this.layers = layers;
+        this.layers = Collections.unmodifiableList(layers);
     }
 
     public List<Layer> getLayers () {
--- src/main/java/domain/Layer.java
+++ src/main/java/domain/Layer.java
@@ -1,12 +1,13 @@
 package domain;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Layer {
     private final List<Boolean> horizontalLines;
 
     public Layer(List<Boolean> horizontalLines) {
-        this.horizontalLines = horizontalLines;
+        this.horizontalLines = Collections.unmodifiableList(horizontalLines);
     }
 
     public List<Boolean> getHorizontalLines() {
