bbd2945fdafb0fe9ffc7d7eb41752fae999eed0f
david.dh <david.dh@kakaocorp.com>
2021-12-30 18:37:47 +0900

    입출력 메소드 개선

--- SADARI/src/kakao2022/david/sadari/step4/view/LadderUI.java
+++ SADARI/src/kakao2022/david/sadari/step4/view/LadderUI.java
@@ -1,48 +1,34 @@
 package kakao2022.david.sadari.step4.view;
 
-import kakao2022.david.sadari.step4.domain.Line;
+import kakao2022.david.sadari.step4.domain.CheckName;
+import kakao2022.david.sadari.step4.domain.Ladder;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class LadderUI {
 
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static ArrayList<String> inputPeople() {
-        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
-        return (ArrayList<String>) Arrays.asList(scanner.next().split(","));
+    public List<String> inputPeople() {
+        boolean isDone = false;
+        List<String> peopleList = new ArrayList<>();
+        while (!isDone) {
+            System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+            peopleList = Arrays.asList(scanner.next().split(","));
+            isDone = CheckName.checkPeopleList(peopleList);
+        }
+        return peopleList;
     }
 
-    public static int inputLadderHeight() {
+    public int inputLadderHeight() {
         System.out.println("최대 사다리 높이는 몇 개인가요?");
         return scanner.nextInt();
     }
 
-    public static void printPeopleList(ArrayList<String> peopleList) {
-        for (String person : peopleList) {
-            System.out.printf("%-6s", person);
-        }
-        System.out.println();
-    }
-
-    public static void printLadder(ArrayList<Line> ladderBoard) {
-        for (Line line : ladderBoard) {
-            printLadderRow(line.getPoints());
-        }
-    }
-
-    private static void printLadderRow(ArrayList<Boolean> points) {
-        StringBuilder ladderLine = new StringBuilder("  |");
-        for (boolean point : points) {
-            ladderLine.append(getLadderRow(point));
-            ladderLine.append("|");
-        }
-        System.out.println(ladderLine);
-    }
-
-    private static String getLadderRow(boolean point) {
-        return point ? "-----" : "     ";
+    public void printLadder(Ladder ladder) {
+        System.out.println(ladder.getLadderInfo());
     }
 }
