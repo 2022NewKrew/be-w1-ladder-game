d09d896be7bf7726036b64f10daba5d387e2bc14
danny.an <danny.an@kakaocorp.com>
2021-12-30 16:26:22 +0900

    Step4 사다리 만들기 (Step3 Review 반영)

--- src/com/kakao/ui/GameInput.java
+++ src/com/kakao/ui/GameInput.java
@@ -1,16 +1,16 @@
 package com.kakao.ui;
 
-import com.kakao.exception.GameInputException;
+import com.kakao.exception.CheckGameInput;
 
 import java.util.*;
 
-public class GameInput {
+class GameInput {
 
-    private static final GameInputException exception = new GameInputException();
+    private static final CheckGameInput exception = new CheckGameInput();
 
     private Scanner sc = new Scanner(System.in);
 
-    public ArrayList<String> inputParticipants() {
+    ArrayList<String> inputParticipants() {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요.)");
         try {
             String participantsInput = sc.nextLine();
@@ -18,19 +18,19 @@ public class GameInput {
             String[] participants = participantsInput.split(",");
             return new ArrayList<>(Arrays.asList(participants));
         } catch (RuntimeException e) {
-            sc = new Scanner(System.in);
+            sc.next();
             return inputParticipants();
         }
     }
 
-    public int inputHeight() {
+    int inputHeight() {
         System.out.println("최대 사다리 높이는 몇 개인가요?");
         try {
             int height = sc.nextInt();
             exception.checkHeightException(height);
             return height;
         } catch (InputMismatchException e) {
-            sc = new Scanner(System.in);
+            sc.next();
             return inputHeight();
         }
     }
---
+++ test/com/kakao/domain/LineTest.java
@@ -0,0 +1,54 @@
+package com.kakao.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LineTest {
+
+    private final List<String> participants = new ArrayList<>();
+
+    LineTest() {
+        for (int i = 0; i < 3; i++) {
+            participants.add("test" + i);
+        }
+    }
+
+    @DisplayName("Step 만들기")
+    @RepeatedTest(10)
+    void testMakeStep(RepetitionInfo repetitionInfo) {
+        System.out.println(repetitionInfo.getCurrentRepetition() + " / " + repetitionInfo.getTotalRepetitions());
+
+        Line line = new Line(participants);
+        List<Step> stepLine = line.getStepLine();
+        Step lastStep = stepLine.get(stepLine.size()-1);
+        Step newStep = line.makeStep();
+
+        if (lastStep.isStep()) {
+            assertFalse(newStep.isStep());
+        }
+
+        System.out.println(lastStep.isStep() + " " + newStep.isStep() + "\n");
+
+//        # Reflection을 이용한 private method 테스트를 하려 했으나 실패하였습니다..
+//
+//        Method[] methods = Line.class.getDeclaredMethods();
+//        Method method = (Method) Arrays.stream(methods).filter(method1 ->
+//                method1.getName() == "makeStep"
+//        ).toArray()[0];
+//        method.setAccessible(true);
+//
+//        Step step = method.invoke(line);
+//        System.out.println(step);
+
+    }
+
+}
