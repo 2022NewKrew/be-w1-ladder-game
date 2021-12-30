af2840cf792836a30b16cb936caad6c4d4e50dd3
sukstar76 <sukstar76@gmail.com>
2021-12-30 17:25:23 +0900

    입력 검증 추가
    참가자 수 및 이름 제한 검증 추가

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,11 +4,17 @@ public class Application {
     private static final Scanner SCANNER = new Scanner(System.in);
 
     public static void runApp() {
-        String[] names = inputParticipants();
-        int height = inputHeight();
+        try {
+            String[] names = inputParticipants();
+            validateParticipants(names);
 
-        LadderGame ladderGame = new LadderGame(names, height);
-        ladderGame.printInformation();
+            int height = inputHeight();
+
+            LadderGame ladderGame = new LadderGame(names, height);
+            ladderGame.printInformation();
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public static int inputHeight() {
@@ -22,6 +28,18 @@ public class Application {
         String inputOfParticipants = SCANNER.nextLine();
 
         return inputOfParticipants.split(",");
+    }
+
+    public static void validateParticipants(String[] names) {
+        if (names.length <= 1) {
+            throw new RuntimeException("참여자의 수는 2명 이상이여야 합니다.");
+        }
+
+        for (int i = 0; i < names.length; i++) {
+            if (names[i].length() >= 6) {
+                throw new RuntimeException("참여자의 이름 길이는 5자 이하여야 합니다.");
+            }
+        }
     }
 
     public static void main(String[] args) {
