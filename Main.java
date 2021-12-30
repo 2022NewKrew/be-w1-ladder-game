4310ff360198005ed55d0336407ca5335a2fa28e
Euidong Lee <54145246+euidonglee@users.noreply.github.com>
2021-12-30 22:07:43 +0900

    Step4 (#497)
    * 4단계 수행
    
    * 4단계-수정
    
    Co-authored-by: ed.ever <ed.ever@kakaocorp.com>

--- Main.java
+++ Main.java
@@ -1,13 +1,14 @@
-import java.util.Scanner;
+package main;
+import main.domain.LadderGame;
+import main.domain.LadderGameInput;
+import main.view.UI;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
-        String[] names = sc.nextLine().split(",");
-        System.out.println("최대 사다리 높이는 몇 개인가요?");
-        int heightOfLadder = sc.nextInt();
+        LadderGameInput userInput = UI.getLadderGameInfo();
+        String[] names = userInput.getNames();
+        int heightOfLadder = userInput.getHeightOfLadder();
         LadderGame ladderGame = new LadderGame(names, heightOfLadder);
-        ladderGame.printLadder();
+        UI.printLadder(ladderGame);
     }
 }
