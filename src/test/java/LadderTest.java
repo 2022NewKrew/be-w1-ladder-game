b4b0db8c09028cdad7bddf2be2e40d246d53b6d8
jake.0208 <jake.0208@kakaocorp.com>
2021-12-30 21:29:01 +0900

    Test: Ladder클래스와 Line클래스에 대한 테스트 코드 작성

---
+++ src/test/java/LadderTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.lang.reflect.Field;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LadderTest {
+    @Test
+    void testToString() {
+        Ladder ladder = new Ladder(new String[]{"ben","jake","creed","kina","pug"}, 5, 5);
+        assertNotNull(ladder);
+    }
+
+    @Test
+    void testLadderContainsNull(){
+        Ladder ladder = new Ladder(new String[]{"ben","jake","creed","kina","pug"}, 5, 5);
+        try{
+            Field field = ladder.getClass().getDeclaredField("ladder");
+            field.setAccessible(true);
+
+            List<Line> testLadder = (List<Line>)field.get(ladder);
+            testLadder.stream()
+                    .forEach(Assertions::assertNotNull);
+
+        }catch(NoSuchFieldException | IllegalAccessException e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/test/java/LineTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.lang.reflect.Field;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LineTest {
+    @Test
+    void testToString() {
+        Line line = new Line(5);
+        assertNotNull(line.toString());
+    }
+
+    @Test
+    void testLineContainsNull(){
+        Line line = new Line(5);
+        try{
+            Field field = line.getClass().getDeclaredField("points");
+            field.setAccessible(true);
+
+            List<Boolean> testLadder = (List<Boolean>)field.get(line);
+            testLadder.stream()
+                    .forEach(Assertions::assertNotNull);
+
+        }catch(NoSuchFieldException | IllegalAccessException e){
+            e.printStackTrace();
+        }
+    }
+}
