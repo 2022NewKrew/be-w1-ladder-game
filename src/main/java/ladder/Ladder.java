7285dc5c504e77dfe217515ca6a0b31db0a01f6f
vince.joe <seonghyeoncho96@gmail.com>
2021-12-30 15:35:24 +0900

    chore: 패키지명 변경 (vince -> ladder)

--- src/main/java/ladder/Ladder.java
+++ src/main/java/ladder/Ladder.java
@@ -1,4 +1,4 @@
-package vince;
+package ladder;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/main/java/ladder/Main.java
+++ src/main/java/ladder/Main.java
@@ -1,4 +1,4 @@
-package vince;
+package ladder;
 
 public class Main {
     public static void main(String[] args) {
