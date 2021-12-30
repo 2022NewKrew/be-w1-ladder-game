c1773cb8da35ad99e6c63a74546b19738815a10a
pride.chicken <pride.chicken@kakaocorp.com>
2021-12-30 16:09:44 +0900

    [refactor] Ladder 구조 변경
    - 클래스 인스턴스 변수로 쓸 필요 없는 부분을 지역변수로 내려줌

--- src/LadderGame/domain/Ladder.java
+++ src/LadderGame/domain/Ladder.java
@@ -6,27 +6,22 @@ import java.util.List;
 public class Ladder {
     // 인스턴스 변수
     private final Name names;
-    private final List<Line> lines;
-    private final int column;
-    private final int row;
+    private final List<Line> lines = new ArrayList<>();
 
     // 생성자
     public Ladder(String names, int column) throws Exception {
         this.names = new Name(names);
-        this.column = column;
-        this.row = 2 * this.names.getNamesSize() - 1;
-        this.lines = createLines();
+        createLines(column);
     }
 
     // 메소드
-    private List<Line> createLines() throws Exception {
-        List<Line> createdLadder = new ArrayList<>();
+    private void createLines(int column) throws Exception {
+        int row = 2 * this.names.getNamesSize() - 1;
 
         for (int i = 0; i < column; i++) {
             Line line = new Line(row);
-            createdLadder.add(line);
+            lines.add(line);
         }
-        return createdLadder;
     }
 
     public Name getNames() {
