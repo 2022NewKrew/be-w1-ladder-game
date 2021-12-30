a58296dd77025f3cc56f8e311d4af35ab88e7e6b
jb.jang <jb.jang@kakaocorp.com>
2021-12-30 15:22:32 +0900

    refactor: Input 클래스 예외 처리
    참여자 이름과 최대 사다리 높이에 대한 입력값을 사용자가 이상하게 넣었을
    때에 예상치 못한 에러가 나지 않도록 예외처리를 추가했습니다.

--- src/main/java/Input.java
+++ src/main/java/Input.java
@@ -1,4 +1,3 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
@@ -12,11 +11,19 @@ public class Input {
 
     public static ArrayList<String> getPlayerNames() {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
-        return new ArrayList<>(Arrays.asList(sc.nextLine().split(SPLIT_REGEX)));
+        ArrayList<String> userInput = new ArrayList<>(Arrays.asList(sc.nextLine().split(SPLIT_REGEX)));
+        boolean isValidated = userInput.stream().allMatch(str -> str.length() < 6 && str.length() > 0);
+        if (isValidated) return userInput;
+        System.out.println("이름을 형식에 맞게 다시 입력하세요. (이름은 1~5자 사이로 입력하세요)");
+        return getPlayerNames();
     }
 
     public static int getLadderHeight() {
         System.out.println("최대 사다리 높이는 몇 개인가요?");
-        return sc.nextInt();
+        String userInput = sc.nextLine();
+        boolean isValidated = userInput.chars().allMatch( Character::isDigit );
+        if (isValidated) return Integer.parseInt(userInput);
+        System.out.println("최대 사디로 높이는 숫자로만 입력하세요");
+        return getLadderHeight();
     }
 }
