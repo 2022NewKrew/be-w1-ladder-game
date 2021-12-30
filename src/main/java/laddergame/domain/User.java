d1bc7d1d1cd43c097971e1d9f56af4158af36d44
JuyeoungJun <wjfhs01@gmail.com>
2021-12-30 10:57:46 +0900

    feat: User 생성 시 이름 길이 확인 로직 추가 (리뷰 반영)

--- src/main/java/laddergame/domain/User.java
+++ src/main/java/laddergame/domain/User.java
@@ -5,10 +5,17 @@ public class User {
     private final String name;
 
     public User(String name) {
+        validateUserNameLength(name);
         this.name = name;
     }
 
     public String getName() {
         return name;
+    }
+
+    private void validateUserNameLength(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("사람의 이름은 최대 5자입니다.");
+        }
     }
 }
