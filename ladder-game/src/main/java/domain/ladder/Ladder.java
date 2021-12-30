01505ed91fe1ed0d5d2f4a74a40a61d59c931dc2
kimdaeseon <kimdaeseon@khu.ac.kr>
2021-12-30 15:57:09 +0900

    Ladder의 generateLines를 Stream.generate를 사용하여 for문 제거

--- ladder-game/src/main/java/domain/ladder/Ladder.java
+++ ladder-game/src/main/java/domain/ladder/Ladder.java
@@ -7,37 +7,23 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Ladder {
+    private static final int MIN_LENGTH = 1;
+    private final List<LadderFrameLine> lines;
 
-
-    private final List<LadderFrameLine> ladderFrameLineList;
-
-    public Ladder(int numberOfPerson, int ladderHeight) {
-        checkNumberOfPerson(numberOfPerson);
-        checkLadderHeight(ladderHeight);
-        this.ladderFrameLineList = generateLadderFrameLineList(numberOfPerson - 1, ladderHeight);
+    public Ladder(int frameLength, int height) {
+        LadderPrecondition.checkFrameLength(frameLength, MIN_LENGTH);
+        LadderPrecondition.checkLadderHeight(height, MIN_LENGTH);
+        this.lines = generateLines(frameLength - 1, height);
     }
 
-    private List<LadderFrameLine> generateLadderFrameLineList(int ladderFrameLength, int ladderHeight) {
-        List<LadderFrameLine> ladderFrameLineList = new ArrayList<>();
-        for (int i = 0; i < ladderHeight; i++) {
-            ladderFrameLineList.add(new LadderFrameLine(ladderFrameLength));
-        }
-        return Collections.unmodifiableList(ladderFrameLineList);
+    private List<LadderFrameLine> generateLines(int frameLength, int height) {
+        return Stream.generate(()-> new LadderFrameLine(frameLength))
+                .limit(height)
+                .collect(Collectors.toUnmodifiableList());
     }
 
-    private void checkNumberOfPerson(int numberOfPerson) {
-        if (numberOfPerson < 1) {
-            throw new IllegalArgumentException("참여하는 사람의 수는 양의 정수여야 합니다.");
-        }
-    }
 
-    private void checkLadderHeight(int ladderHeight) {
-        if (ladderHeight < 1) {
-            throw new IllegalArgumentException("사다리의 높이는 양의 정수여야 합니다.");
-        }
-    }
-
-    public List<LadderFrameLine> getLadderFrameLineList() {
-        return ladderFrameLineList;
+    public List<LadderFrameLine> getLines() {
+        return lines;
     }
 }
