890df3f731352f7fc4e7ef39c3a5ec76e42bb646
binary.yun <binary.yun@kakaocorp.com>
2021-12-30 23:59:55 +0900

    refactor: Row 클래스의 네이밍 변경

---
+++ src/main/java/domain/Line.java
@@ -0,0 +1,26 @@
+package domain;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Line {
+    private final List<Boolean> bridges;
+
+    public Line(List<Boolean> bridges) {
+        this.bridges = bridges;
+    }
+
+    public List<Boolean> getBridges() {
+        return Collections.unmodifiableList(bridges);
+    }
+
+    public boolean existLeftBridge(int location) {
+        if (location == 0)
+            return false;
+        return bridges.get(location - 1);
+    }
+
+    public boolean existRightBridge(int location) {
+        return bridges.get(location);
+    }
+}
