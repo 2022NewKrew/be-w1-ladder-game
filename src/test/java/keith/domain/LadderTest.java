5ecb941c48abc6ce610e8d535c321a16c0d77402
keith.lee <keith.lee@kakaocorp.com>
2021-12-30 15:26:43 +0900

    4단계 구현 완료

--- src/test/java/keith/domain/LadderTest.java
+++ src/test/java/keith/domain/LadderTest.java
@@ -1,2 +1,35 @@
-package keith.domain;public class LadderTest {
+package keith.domain;
+
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class LadderTest {
+    static Ladder ladder;
+
+    @Test
+    void getConnectionsTest() {
+        List<String> names = new ArrayList<>();
+        for (int i = 0; i <= 100; i++) {
+            names.add("test");
+            ladder = new Ladder(names,i);
+            assertThat(ladder.getConnections().size()).isEqualTo(i);
+        }
+    }
+
+    @Test
+    void getParticipantsTest() {
+        List<String> names = new ArrayList<>();
+        ladder = new Ladder(names, 5);
+        assertThat(ladder.getParticipants().size()).isEqualTo(0);
+        for (int i = 1; i <= 100; i++) {
+            names.add("test");
+            ladder = new Ladder(names,5);
+            assertThat(ladder.getParticipants().size()).isEqualTo(i);
+        }
+    }
 }
--- src/test/java/keith/domain/LineTest.java
+++ src/test/java/keith/domain/LineTest.java
@@ -1,2 +1,22 @@
-package keith.domain;public class LineTest {
+package keith.domain;
+
+import org.junit.jupiter.api.*;
+import static org.assertj.core.api.Assertions.*;
+
+public class LineTest {
+    private static Line line;
+
+    @Test
+    void testZeroWidthLine() {
+        line = new Line(0);
+        assertThat(line.getPoints().size()).isEqualTo(2);
+    }
+
+    @Test
+    void testMultiWidthLine() {
+        for (int i = 1; i < 50; i++) {
+            line = new Line(i);
+            assertThat(line.getPoints().size()).isEqualTo(i + 1);
+        }
+    }
 }
