c46a6afcdc065f4a2695599f1f813962148a42db
david.dh <david.dh@kakaocorp.com>
2021-12-30 15:08:04 +0900

    파일 위치 변경

---
+++ SADARI/src/kakao2022/david/sadari/step4/LadderGame.java
@@ -0,0 +1,11 @@
+package kakao2022.david.sadari.step4;
+
+import kakao2022.david.sadari.step4.domain.Ladder;
+
+public class LadderGame {
+
+    public static void main(String[] args) {
+        Ladder ladder = new Ladder();
+        ladder.startGame();
+    }
+}
