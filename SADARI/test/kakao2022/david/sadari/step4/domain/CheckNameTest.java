746ba4ff6539132a2b84989c71ed2f7d1c5ac1ad
david.dh <david.dh@kakaocorp.com>
2021-12-30 18:31:43 +0900

    에러메세지 직접 입력하여 테스트

--- SADARI/test/kakao2022/david/sadari/step4/domain/CheckNameTest.java
+++ SADARI/test/kakao2022/david/sadari/step4/domain/CheckNameTest.java
@@ -1,6 +1,7 @@
 package kakao2022.david.sadari.step4.domain;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CheckNameTest {
@@ -9,6 +10,6 @@ class CheckNameTest {
     void checkNameLength() {
         NameLengthException exception = assertThrows(NameLengthException.class, () -> CheckName.checkNameLength("aaaaaa"));
         String errorMessage = exception.getMessage();
-        assertEquals(errorMessage, CheckName.NAME_ERROR_MESSAGE);
+        assertEquals(errorMessage, "이름은 최대 5글자 까지 부여할 수 있습니다.");
     }
 }
