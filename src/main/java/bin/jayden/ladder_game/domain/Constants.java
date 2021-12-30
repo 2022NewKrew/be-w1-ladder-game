3e09ce891942df42b1837d3f76746e54efbc0ee9
jayden-bin <jayden.bin@kakaocorp.com>
2021-12-30 20:22:54 +0900

    Code formatting
    - 전체파일 code formatting

--- src/main/java/bin/jayden/ladder_game/domain/Constants.java
+++ src/main/java/bin/jayden/ladder_game/domain/Constants.java
@@ -2,10 +2,10 @@ package bin.jayden.ladder_game.domain;
 
 public class Constants {
     public final static int MIN_PARTICIPANT_SIZE = 2;
-    public final static int MAX_PARTICIPANT_SIZE = Integer.MAX_VALUE-1;
+    public final static int MAX_PARTICIPANT_SIZE = Integer.MAX_VALUE - 1;
     public final static int MAX_NAME_SIZE = 5;
     public final static int MIN_NAME_SIZE = 1;
-    public final static int MAX_LADDER_HEIGHT = Integer.MAX_VALUE-1;
+    public final static int MAX_LADDER_HEIGHT = Integer.MAX_VALUE - 1;
     public final static int MIN_LADDER_HEIGHT = 1;
     public final static int PREFIX_SIZE = (MAX_NAME_SIZE + 1) / 2; //사디리를 출력할때 이름과 위치를 맞추기 위해 출력하는 빈칸 수
     public final static String PARTICIPANT_DELIMITER = ",";
