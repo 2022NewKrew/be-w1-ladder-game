7bd749bc83ce6924d8fcc544ac76dc2e4f5ce0d0
vince.joe <seonghyeoncho96@gmail.com>
2021-12-31 10:55:33 +0900

    refactor: `LadderPrinter` -> `LadderGamePrinter`

--- src/main/java/ladder/Main.java
+++ src/main/java/ladder/Main.java
@@ -1,12 +1,12 @@
 package ladder;
 
 import ladder.domain.LadderGame;
-import ladder.view.LadderPrinter;
+import ladder.view.LadderGamePrinter;
 
 public class Main {
     public static void main(String[] args) {
         LadderGame game = new LadderGame();
-        LadderPrinter printer = new LadderPrinter();
+        LadderGamePrinter printer = new LadderGamePrinter(" ", "-", "|");
         printer.printLadderGame(game);
     }
 }
