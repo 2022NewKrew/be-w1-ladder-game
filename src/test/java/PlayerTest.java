4efcf523cc5ced643e05a140f1d19a14a318b888
punsoo <sonu.sohn@kakaocorp.com@gmail.com>
2022-01-01 04:02:22 +0900

    test:PlayerTest 작성

---
+++ src/test/java/PlayerTest.java
@@ -0,0 +1,18 @@
+package test.java;
+
+import main.java.domain.Player;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+public class PlayerTest {
+    @ParameterizedTest(name = "이름 길이 제한 Test")
+    @ValueSource(strings = {"sooncheol", "kakaocommerce"})
+    public void 이름_길이_제한_Test(String input) {
+        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, () -> new Player(input));
+        assertEquals("이름은 최대 5글자입니다.",illegalArgumentException.getMessage());
+    }
+}
