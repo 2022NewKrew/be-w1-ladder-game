e7df4aa50511998f04ed24c63ff22696e069b9f6
jindo.min <jindo.min@kakaocorp.com>
2021-12-31 23:02:28 +0900

    [refactor] unmodifiableList로 변경

--- src/main/java/ladder/domain/Ladder.java
+++ src/main/java/ladder/domain/Ladder.java
@@ -1,5 +1,6 @@
 package ladder.domain;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Ladder {
@@ -8,7 +9,7 @@ public class Ladder {
     Ladder(List<Line> lines) {
         if (lines == null)
             throw new IllegalArgumentException();
-        this.lines = lines;
+        this.lines = Collections.unmodifiableList(lines);
     }
 
     public List<Line> getLadder() {
--- src/main/java/ladder/domain/Line.java
+++ src/main/java/ladder/domain/Line.java
@@ -1,5 +1,6 @@
 package ladder.domain;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Line {
@@ -8,7 +9,7 @@ public class Line {
     Line(List<Boolean> line) {
         if (line == null)
             throw new IllegalArgumentException();
-        this.line = line;
+        this.line = Collections.unmodifiableList(line);
     }
 
     public List<Boolean> getLine() {
