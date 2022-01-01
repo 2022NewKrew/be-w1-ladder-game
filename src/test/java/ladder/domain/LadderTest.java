cb2b992a8bfddf56222cf0f90f1837d14339f23d
jun.dev <jun.dev@kakaocorp.com>
2022-01-01 17:04:23 +0900

    refactor: 테스트코드 given, when, then으로 구성하도록 수정

--- src/test/java/ladder/domain/LadderTest.java
+++ src/test/java/ladder/domain/LadderTest.java
@@ -7,71 +7,80 @@ import org.junit.jupiter.api.parallel.Execution;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class LadderTest {
 
 
     @Test
-    void 콤마로_구분한_참여자수테스트() throws Exception {
-        //Ladder ladderObject = new Ladder();
-        String nameAry = "a,ab,abc,abcd,abcde";
+    void 이름에_공백이_포함되면_예외가_발생한다() throws Exception {
+        //given
+        String nameSample = "a,ab,abc,abcd,   abcde";
         Long height = Long.valueOf(10);
-        // ladderObject.setValue(nameAry, height);
 
-        // assertTrue(ladderObject.getPeopleCount() == 5);
+
+        //when
+        //Ladder ladderObject = new Ladder(nameSample, height); //이름에 공백이 들어간 객체생성시
+
+
+        //then
+        assertThatThrownBy(() -> {
+            Ladder ladderObject = new Ladder(nameSample, height);
+        }).isInstanceOf(Exception.class).hasMessageContaining("Space_Inside_Name");
     }
 
     @Test
     void 연속적인_사다리_선_제거_테스트() throws Exception {
-        //    Ladder ladderObject = new Ladder();
+
+        //given
         String nameAry = "a,ab,abc,abcd,abcde,a,b,c,d,e,a,b,c,d,e";
         Long height = Long.valueOf(100000); //테스트를 위해 높은 height로 지정
-        // ladderObject.setValue(nameAry, height);
-//        ladderObject.initLine();
-        //    ladderObject.shuffle();
 
 
-        //   List<Line> lineStatus = ladderObject.getLineStatus();
+        //when
+        Ladder ladderObject = new Ladder(nameAry, height); //사다리 구성
 
-//        for(int i = 0 ; i < height ; i++){
-//            for(int j = 0 ; j < lineStatus.get(i).value.size() - 1 ; j++){
-//                assertTrue(lineStatus.get(i).value.get(j) + 1 != lineStatus.get(i).value.get(j+1));
-//            }
-//        }
+
+
+        //then
+        List<Line> lineStatus = ladderObject.getLineStatus();
+        for(int i = 0 ; i < height ; i++){
+            for(int j = 0 ; j < lineStatus.get(i).value.size() - 1 ; j++){
+                assertTrue(lineStatus.get(i).value.get(j) + 1 != lineStatus.get(i).value.get(j+1)); //연속된 사다리가 제거된지 체크.
+            }
+        }
+
     }
 
     @Test
-    void 입력시_이름에_공백이_추가되는경우_예외가_발생한다() throws Exception{
-        // Ladder ladderObject = new Ladder();
-        String nameAry = "a,ab,abc,    abcd"; //공백발생 예외
+    void 콤마를_기준으로_참여자수를_나눈다() throws Exception{
+        //given
+        String nameAry = "a,ab,abc,abcd"; //공백발생 예외
         Long height = Long.valueOf(10);
 
-        boolean isException = false;
+        //when
+        Ladder ladderObject = new Ladder(nameAry, height);
 
-        try{
-            // ladderObject.setValue(nameAry, height);
-        } catch(Exception e){
-            isException = true;
-        }
 
-        assertTrue(isException);
+        //then
+        assertTrue(ladderObject.getPeopleCount() == 4);
     }
 
     @Test
     void 높이가_0_미만이면_예외가_발생한다() throws Exception{
-        // Ladder ladderObject = new Ladder();
+        //given
         String nameAry = "a,ab,abc,abcd";
         Long height = Long.valueOf(0); //높이가 0인 예외사항
 
-        boolean isException = false;
 
-        try{
-            //        ladderObject.setValue(nameAry, height);
-        } catch(Exception e){
-            isException = true;
-        }
+        //when
+        //객체생성시
 
-        assertTrue(isException);
+
+        //then
+        assertThatThrownBy(() -> {
+                Ladder ladderObject = new Ladder(nameAry, height);
+        }).isInstanceOf(Exception.class).hasMessageContaining("HeightBelowZero");
     }
 }
