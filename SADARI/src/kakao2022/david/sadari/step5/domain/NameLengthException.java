fa02bbf3ca26f4e4ce248a31268aaf3243f2c35b
david.dh <david.dh@kakaocorp.com>
2022-01-01 17:15:45 +0900

    변경 사항 없음

---
+++ SADARI/src/kakao2022/david/sadari/step5/domain/NameLengthException.java
@@ -0,0 +1,8 @@
+package kakao2022.david.sadari.step5.domain;
+
+public class NameLengthException extends Exception {
+
+    public NameLengthException(String message) {
+        super(message);
+    }
+}
