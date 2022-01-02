1b6ae8012f21594e19681a8519582d1c1a19d3c4
pride.chicken <pride.chicken@kakaocorp.com>
2022-01-02 16:45:52 +0900

    [feature] 결과 라인 출력
    - 결과 라인을 출력하는 함수 구현

--- src/LadderGame/view/LadderView.java
+++ src/LadderGame/view/LadderView.java
@@ -5,6 +5,8 @@ import LadderGame.domain.*;
 import java.util.stream.Collectors;
 
 public class LadderView {
+    private static final int MAX_NAME_SIZE = 5;
+    private static final int SPACE = 1;
 
     private final Ladder ladder;
     private final NameView nameView;
@@ -16,12 +18,24 @@ public class LadderView {
     }
 
     public void showLadder() {
-        System.out.println(this.nameView + getLaddersOutput());
+        System.out.println("사다리 결과");
+        System.out.println(nameView.showNames() + getLaddersOutput() + getResultOutput());
     }
 
     private String getLaddersOutput() {
         StringBuilder laddersOutput = new StringBuilder();
         ladder.getLines().stream().map(LineView::new).collect(Collectors.toList()).forEach(laddersOutput::append);
         return laddersOutput.toString();
+    }
+
+    private String getResultOutput(){
+        StringBuilder resultOutput = new StringBuilder();
+        for (String result : ladder.getResultList()) {
+            int padding = ((MAX_NAME_SIZE - result.length()) / 2) + SPACE;
+            int oddPadding = result.length()%2;
+            resultOutput.append(" ".repeat(padding)).append(result).append(" ".repeat(padding-oddPadding));
+        }
+        resultOutput.append("\n");
+        return resultOutput.toString();
     }
 }
