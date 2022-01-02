ecaecc0e74040ebc997aad6804c2a812c0d1ca86
vince.joe <seonghyeoncho96@gmail.com>
2022-01-02 23:12:49 +0900

    refactor: 출력 문자열 static 변수로 분리

--- src/main/java/ladder/Main.java
+++ src/main/java/ladder/Main.java
@@ -4,9 +4,13 @@ import ladder.domain.LadderGame;
 import ladder.view.LadderGamePrinter;
 
 public class Main {
+    private static final String EMPTY_STEP_STR = " ";
+    private static final String FILLED_STEP_STR = "-";
+    private static final String POLE_STR = "|";
+
     public static void main(String[] args) {
         LadderGame game = new LadderGame();
-        LadderGamePrinter printer = new LadderGamePrinter(" ", "-", "|");
+        LadderGamePrinter printer = new LadderGamePrinter(EMPTY_STEP_STR, FILLED_STEP_STR, POLE_STR);
         printer.printLadderGame(game);
     }
 }
