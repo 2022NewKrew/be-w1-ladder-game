6e35a875f90ed8b82a588628923d263e0a33c882
adj5672 <77478791+adj5672@users.noreply.github.com>
2022-01-03 04:01:22 +0900

    Step5 사다리 만들기 (#578)
    Co-authored-by: danny.an <danny.an@kakaocorp.com>

--- src/com/kakao/ui/GameInput.java
+++ src/com/kakao/ui/GameInput.java
@@ -1,38 +1,60 @@
 package com.kakao.ui;
 
-import com.kakao.exception.CheckGameInput;
+import com.kakao.exception.CheckGameIO;
 
 import java.util.*;
 
-class GameInput {
+public class GameInput {
 
-    private static final CheckGameInput exception = new CheckGameInput();
-
+    private static final CheckGameIO checkInput = new CheckGameIO();
     private Scanner sc = new Scanner(System.in);
+    private int participantCnt;
 
-    ArrayList<String> inputParticipants() {
-        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요.)");
+    public List<String> inputParticipants() {
+        System.out.println("\n참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요.)");
         try {
             String participantsInput = sc.nextLine();
-            exception.checkParticipantsException(participantsInput);
+            checkInput.checkParticipantsException(participantsInput);
             String[] participants = participantsInput.split(",");
-            return new ArrayList<>(Arrays.asList(participants));
+            participantCnt = participants.length;
+            return Arrays.asList(participants);
         } catch (RuntimeException e) {
-            sc.next();
+            sc = new Scanner(System.in);
             return inputParticipants();
         }
     }
 
-    int inputHeight() {
-        System.out.println("최대 사다리 높이는 몇 개인가요?");
+    public int inputHeight() {
+        System.out.println("\n최대 사다리 높이는 몇 개인가요?");
         try {
             int height = sc.nextInt();
-            exception.checkHeightException(height);
+            checkInput.checkHeightException(height);
+            sc.nextLine();
             return height;
         } catch (InputMismatchException e) {
-            sc.next();
+            System.out.println("\n사다리 높이는 1 이상의 정수값만 가능합니다.");
+            sc = new Scanner(System.in);
             return inputHeight();
         }
+    }
+
+    public List<String> inputResults() {
+        System.out.println("\n실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요.)");
+        try {
+            String resultsInput = sc.nextLine();
+            checkInput.checkResultsException(resultsInput, participantCnt);
+            String[] results = resultsInput.split(",");
+            return Arrays.asList(results);
+        } catch (RuntimeException e) {
+            sc = new Scanner(System.in);
+            return inputResults();
+        }
+    }
+
+    public String inputCheckParticipant() {
+        System.out.println("\n결과를 보고 싶은 사람은?");
+        String participant = sc.nextLine();
+        return participant;
     }
 
 }
--- src/com/kakao/ui/RunGame.java
+++ src/com/kakao/ui/RunGame.java
@@ -7,12 +7,25 @@ import java.util.*;
 public class RunGame {
 
     private static final GameInput gameInput = new GameInput();
+    private static GameOutput gameOutput;
+
+    public static void checkGameResult() {
+        String participant = gameInput.inputCheckParticipant();
+        if (gameOutput.checkParticipant(participant)) {
+            checkGameResult();
+        }
+    }
 
     public static void main(String[] args) {
-        ArrayList<String> participants = gameInput.inputParticipants();
+        List<String> participants = gameInput.inputParticipants();
+        List<String> results = gameInput.inputResults();
         int height = gameInput.inputHeight();
 
-        Ladder ladder = new Ladder(participants, height);
-        ladder.printLadder();
+        Ladder ladder = new Ladder(participants, height, results);
+        gameOutput = new GameOutput(ladder);
+
+        gameOutput.printLadder();
+        checkGameResult();
+        System.out.println("\n게임을 종료합니다.");
     }
 }
