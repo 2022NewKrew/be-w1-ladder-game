1276169e978e2682c272e0d2b81ae459db2bdd4d
mimseong <parkseongmin98@gmail.com>
2022-01-03 13:30:06 +0900

    [test] 성공하는 테스트 추가

--- src/test/java/ladder/domain/LadderTest.java
+++ src/test/java/ladder/domain/LadderTest.java
@@ -4,9 +4,24 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LadderTest {
     @Test
-    @DisplayName("Should not create ladders with invalid parameter (null)")
+    @DisplayName("[성공] Ladder 객체를 생성할 수 있다")
+    void canMakeLadder() {
+        List<Line> testLines = new ArrayList<>();
+        testLines.add(new Line(new ArrayList<>()));
+        testLines.add(new Line(new ArrayList<>()));
+
+        Ladder testLadder = new Ladder(testLines);
+
+        Assertions.assertEquals(testLines, testLadder.getLadder());
+    }
+
+    @Test
+    @DisplayName("[실패] Ladder 생성자에 null을 넣으면 IllegalArgumentException을 던져야 한다")
     void laddersWithInvalidParameter() {
         Assertions.assertThrows(IllegalArgumentException.class,
                 () -> new Ladder(null));
--- src/test/java/ladder/domain/LineTest.java
+++ src/test/java/ladder/domain/LineTest.java
@@ -4,9 +4,24 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LineTest {
     @Test
-    @DisplayName("Should not create lines with invalid parameter (null)")
+    @DisplayName("[성공] Line 객체를 생성할 수 있다")
+    void canMakeLine() {
+        List<Boolean> line = new ArrayList<>();
+        line.add(true);
+        line.add(false);
+
+        Line testLine = new Line(line);
+
+        Assertions.assertEquals(line, testLine.getLine());
+    }
+
+    @Test
+    @DisplayName("[실패] Line 생성자에 null을 넣으면 IllegalArgumentException을 던져야 한다")
     void linesWithInvalidParameter() {
         Assertions.assertThrows(IllegalArgumentException.class,
                 () -> new Line(null));
