df9ad47685aaf1980fc7c26712c5f17b29b2df09
unfunhy <unfunhy@gmail.com>
2022-01-03 10:49:51 +0900

    사다리게임 4단계 - 테스트 코드 작성

--- src/test/java/ladder/domain/GameControllerTest.java
+++ src/test/java/ladder/domain/GameControllerTest.java
@@ -1,10 +1,51 @@
 package ladder.domain;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("GameController Test")
 class GameControllerTest {
 
+    private static GameController gc;
+
+    @BeforeAll
+    static void initialize() {
+        gc = new GameController();
+    }
+
+    @DisplayName("사다리의 높이는 1 이상이어야 한다.")
     @Test
-    void startGame() {
+    void 최소_사다리_높이_테스트() {
+        try {
+            callPrivateValidationMethod("validateNumOfLine", 0);
+        } catch (Exception e) {
+            assertThat(e.getCause().getMessage()).isEqualTo("사다리 높이는 1 이상이어야 합니다.");
+            return;
+        }
+        fail();
+    }
+
+    @DisplayName("플레이어의 수는 2 이상이어야 한다.")
+    @Test
+    void 최소_플레이어_수_테스트() {
+        try {
+            callPrivateValidationMethod("validateNumOfPlayers", 1);
+        } catch (Exception e) {
+            assertThat(e.getCause().getMessage()).isEqualTo("플레이어가 두 명 이상이어야 합니다.");
+            return;
+        }
+        fail();
+    }
+
+    private void callPrivateValidationMethod(String methodName, int value) throws Exception {
+        Method method = gc.getClass().getDeclaredMethod(methodName, int.class);
+        method.setAccessible(true);
+        method.invoke(gc, value);
     }
 }
--- src/test/java/ladder/domain/LadderTest.java
+++ src/test/java/ladder/domain/LadderTest.java
@@ -1,25 +1,52 @@
 package ladder.domain;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Ladder Test")
 class LadderTest {
 
-    Ladder ladder;
+    static private Ladder ladder;
 
+    @BeforeAll
+    static void beforeAll() {
+        ladder = new Ladder();
+    }
+
+    @DisplayName("하나의 라인에서 사다리가 연속해서 두 개 이상 존재하지 않는다.")
     @Test
-    void setLadder() {
+    void 사다리_유효성_테스트() {
+        //given
         int numOfPlayers = 100;
         int numOfLine = 100;
 
-        ladder = new Ladder();
+        //when
         ladder.setLadder(numOfPlayers, numOfLine);
 
-        List<LadderLine> lines = ladder.getLadder();
+        //then
+        assertFalse(ladder.getLadder().stream() // Stream<LadderLine>
+                .map(LadderTest::isValidate) // Stream<Boolean>
+                .collect(Collectors.toList()) // List<Boolean>
+                .contains(false));
     }
 
-    @Test
-    void getLadder() {
+    static boolean isValidate(LadderLine line) {
+        List<Boolean> points = line.getLine();
+        for (int i = 1; i < points.size(); i++) {
+            if (abnormalCondition(points.get(i - 1), points.get(i)))
+                return false;
+        }
+        return true;
+    }
+
+    //prev point와 current point에 모두 bridge가 있다면 비정상 컨디션으로 분류
+    static boolean abnormalCondition(boolean prev, boolean current) {
+        return prev && current;
     }
 }
