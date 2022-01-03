5a7a7f07ea8a3846c73a2c7b3d35de06cf24fc0a
dfjung4254 <dfjung4254@gmail.com>
2022-01-03 12:25:08 +0900

    .gitignore 수정 및 out 디렉토리 적용

---
+++ ladder/src/main/java/com/kakao/leo/view/out/ConsoleWriteManager.java
@@ -0,0 +1,24 @@
+package com.kakao.leo.view.out;
+
+/**
+ * System.out 을 통해 String 객체를 출력하는 WriteManager 구현체
+ *
+ * @author leo.jung
+ * @since 1.0
+ */
+public class ConsoleWriteManager implements WriteManager {
+
+  private ConsoleWriteManager() {
+  }
+
+  public static ConsoleWriteManager create() {
+    return new ConsoleWriteManager();
+  }
+
+
+  @Override
+  public void draw(String str) {
+    System.out.println(str);
+  }
+
+}
