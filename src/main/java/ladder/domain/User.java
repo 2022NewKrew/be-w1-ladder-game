c8d7cce9183502ea1032059350876a991064e0c8
hoodie.b612 <hoodie.b612@kakaocorp.com>
2022-01-03 10:35:58 +0900

    Create User.id for identification

--- src/main/java/ladder/domain/User.java
+++ src/main/java/ladder/domain/User.java
@@ -1,12 +1,16 @@
 package ladder.domain;
 
+import java.util.UUID;
+
 public class User {
     static final int MAX_NAME_LEN = 5;
     private final String name;
+    private final UUID id;
 
 
     public User(String name) {
         validateNameLength(name);
+        this.id = UUID.randomUUID();
         this.name = name;
     }
 
@@ -14,6 +18,10 @@ public class User {
         if (name.length() > MAX_NAME_LEN) {
             throw new IllegalArgumentException(String.format("이름의 길이는 %d를 넘을 수 없습니다.", MAX_NAME_LEN));
         }
+    }
+
+    public UUID getId() {
+        return id;
     }
 
     @Override
