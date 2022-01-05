b227bce122d4ea2d1efd00c47ef66d5a1439c372
jeihyunsung <hyunsungjei@naver.com>
2022-01-06 01:15:59 +0900

    Step2 - depth minimize - remove 'else' - naming convention

--- src/main/java/LadderClass.java
+++ src/main/java/LadderClass.java
@@ -2,8 +2,8 @@ import java.util.Random;
 
 public class LadderClass {
 
-    char[][] ladderArray;
     static Random random = new Random();
+    char[][] ladderArray;
 
     LadderClass(int numberPeople, int ladderHeight){
         int printLadderColumns;
@@ -14,30 +14,33 @@ public class LadderClass {
         ladderArray = new char[printLadderRows][printLadderColumns];
     }
 
-    public void assignLadderArray(){
-
-        for (int row = 0; row < ladderArray.length; row++){
-            for (int col = 0; col< ladderArray[row].length; col++){
-                if (col%2 == 1){
-                    ladderArray[row][col] = assignRandomBridge(random);
-                }
-                else{
-                    ladderArray[row][col] = '|';
-                }
-            }
+    public void assignLadderArray() {
+        for (int row = 0; row < ladderArray.length; row++) {
+            assignLadderRow(row);
         }
     }
 
-    public char assignRandomBridge(Random random){
+    public void assignLadderRow(int row){
+        for (int col = 0; col < ladderArray[row].length; col++) {
+            assignLadderElement(row, col);
+        }
+    }
+
+    public void assignLadderElement(int row, int col){
+        if (col%2 == 1){
+            ladderArray[row][col] = assignRandomBridge();
+            return;
+        }
+        ladderArray[row][col] = '|';
+    }
+
+    public char assignRandomBridge(){
         return (random.nextBoolean()) ? '-' : ' ';
     }
 
     public void printLadder(){
         for (int row = 0; row < ladderArray.length; row++){
-            for (int col = 0; col< ladderArray[row].length; col++){
-                System.out.print(ladderArray[row][col]);
-            }
-            System.out.print("\n");
+            System.out.println(ladderArray[row]);
         }
     }
 
